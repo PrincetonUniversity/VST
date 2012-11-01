8ef9a8f002a6e5e94a5ee9a43ea6ad6af2b777aa
Gordon Stewart <jsseven@princeton.edu>
2012-11-01 01:03:33 +0000

    make the filesystem semantics slightly more realistic

--- veric/fs_extension.v
+++ veric/fs_extension.v
@@ -6,45 +6,104 @@ Require Import msl.base
 
 Set Implicit Arguments.
 
-Inductive f_mode: Type := RDONLY | WRONLY | RDWR.
+Inductive fmode: Type := RDONLY | WRONLY | RDWR.
 
-Definition f_writable (mode: f_mode) :=
+Definition fwritable (mode: fmode) :=
   match mode with
   | RDONLY => false
   | WRONLY => true
   | RDWR => true
   end.
 
-Inductive file: Type := mkfile: forall 
-  (mode: f_mode) (sz: nat) (cur: nat) (contents: forall (ofs: nat), memval), file.
+(* model of a simple "flat" filesystem *)
 
-Inductive fsys: Type := mkfsys: 
-  forall (fdtable: forall (fd: int), option file) (nopen: nat) (next_fd: int), fsys.
+Inductive file: Type := mkfile: forall (size: nat) (contents: forall (ofs: nat), memval), file.
+
+Definition fptr := nat.
+
+(*typedef struct {
+  int cur_dir; // i-node of the current working directory
+  int n_inodes; // allocated i-nodes
+  int n_open_files;
+  uint8_t map_open_files[BITMAP_SIZE(N_INODES)];
+  uint8_t map_inodes[BITMAP_SIZE(N_INODES)];
+  uint8_t map_blocks[BITMAP_SIZE(N_BLOCKS)];
+  uint32_t fd_map[MAX_OPEN_FILES]; // maps FDs to i-nodes
+  uint32_t fptr_map[MAX_OPEN_FILES]; // maps FDs to file pointers
+  file_mode_t fperm_map[MAX_OPEN_FILES]; // maps FDs to file permissions
+  uint8_t block_cache[CACHE_N_BLOCKS][BLOCK_SIZE]; // data block cache
+  uint32_t cache_map[CACHE_N_BLOCKS]; // maps cache block # to data block #
+  uint32_t cache_last_access_map[CACHE_N_BLOCKS]; // maps cache block # to approximate time since last access
+} fs_data;*)
+
+Inductive fs_data: Type := mkfsdata: forall 
+  (fdtable: forall (fd: int), option (fmode*fptr)) (*None = fd unused*)
+  (fcache: forall (fd: int), option file) (*models in-memory storage*)
+  (nfiles_open: int) (*number of files currently open*)
+  (fnames: forall (fd: int), option int) (*map from file descriptors to file names*), 
+  fs_data.
+
+Inductive fs: Type := mkfs: forall 
+  (fsdata: fs_data)
+  (fstore: forall (name: int), option file), (*models on-disk storage*)
+  fs.
 
 Section selectors.
-Variable (fs: fsys).
-Definition get_fdtable := match fs with mkfsys fdtable _ _ => fdtable end.
-Definition get_nopen := match fs with mkfsys _ nopen _ => nopen end.
-Definition get_next_fd := match fs with mkfsys _ _ fd => fd end.
+Variable (fs: fs).
+Definition get_fsdata := match fs with mkfs fsdata _ => fsdata end.
+Definition get_fstore := match fs with mkfs _ fstore => fstore end.
+
+Definition get_fdtable := match get_fsdata with mkfsdata fdtable _ _ _ => fdtable end.
+Definition get_fcache := match get_fsdata with mkfsdata _ fcache _ _ => fcache end.
+Definition get_nfiles_open := match get_fsdata with mkfsdata _ _ nfiles_open _ => nfiles_open end.
+Definition get_fnames := match get_fsdata with mkfsdata _ _ _ fnames => fnames end.
 
 Variable (fd: int).
-Definition get_file := get_fdtable fd.
+Definition get_file := get_fcache fd. 
+Definition get_fmode := 
+  match get_fdtable fd with 
+  | None => None 
+  | Some (md, cur) => Some md
+  end.
+Definition get_fptr := 
+  match get_fdtable fd with 
+  | None => None 
+  | Some (md, cur) => Some cur
+  end.
 
 Variable (f: file).
-Definition get_mode := match f with mkfile mode _ _ _ => mode end.
-Definition get_sz := match f with mkfile _ sz _ _ => sz end.
-Definition get_cur := fun f => match f with mkfile _ _ cur _ => cur end.
-Definition get_contents := fun f => match f with mkfile _ _ _ contents => contents end.
+Definition get_size := match f with mkfile sz _ => sz end.
+Definition get_contents := match f with mkfile _ contents => contents end.
 
 End selectors.
 
-Definition empty_fs := mkfsys (fun _:int => None).
+(*return the highest unallocated file descriptor, or None if none available*)
+Section alloc_fd.
+Variable (fs: fs).
+
+Fixpoint find_unused_fd (n: nat) (fdtable: int -> option file) :=
+  match n with 
+  | O => None
+  | S n' => match fdtable (Int.repr (Z_of_nat n)) with 
+            | None => Some (Int.repr (Z_of_nat n))
+            | Some _ => find_unused_fd n' fdtable
+            end
+  end.
+
+Definition MAX_FILE_DESCRIPTORS := (*(nat_of_Z Int.modulus)*) 10%nat.
+
+Definition alloc_fd := find_unused_fd MAX_FILE_DESCRIPTORS (get_fcache fs).
+
+End alloc_fd.
+
+Definition mount_fs (fstore: forall (name: int), option file) := 
+  mkfs (mkfsdata (fun _:int => None) (fun _:int => None) Int.zero (fun _:int => None)) fstore.
 
 Section FSExtension.
 Variables 
   (Z cT D: Type) 
   (csem: CoreSemantics genv cT mem D)
-  (client_sig: ext_sig mem fsys)
+  (client_sig: ext_sig mem fs)
 
   (after_at_external_excl: forall c ret c',
     after_external csem ret c = Some c' -> at_external csem c' = None)
@@ -57,7 +116,7 @@ Definition cores := fun _:nat => Some csem.
 
 Local Open Scope nat_scope.
 
-Inductive xT: Type := mkxT: forall (z: Z) (c: cT) (fs: fsys), xT.
+Inductive xT: Type := mkxT: forall (z: Z) (c: cT) (fs: fs), xT.
 
 Section selectors.
 Variable (s: xT).
@@ -77,17 +136,19 @@ Definition runnable := fun (ge: genv) (s: xT) =>
   end.
 Definition proj_zint := get_fs.
 Definition proj_zext := get_ext.
-Definition zmult := @prod Z fsys. 
+Definition zmult := @prod Z fs. 
 
+(*SYS_READ/WRITE_SIG: fd, buf, count*)
 Notation SYS_READ_SIG := (mksignature (AST.Tint::AST.Tint::AST.Tint::nil) (Some AST.Tint)).
 Notation SYS_WRITE_SIG := (mksignature (AST.Tint::AST.Tint::AST.Tint::nil) (Some AST.Tint)).
-Notation SYS_OPEN_SIG := (mksignature (AST.Tint::nil) (Some AST.Tint)).
+(*SYS_OPEN_SIG: name*)
+Notation SYS_OPEN_SIG := (mksignature (AST.Tint::AST.Tint::nil) (Some AST.Tint)).
 
 Notation SYS_READ := (EF_external 3%positive SYS_READ_SIG).
 Notation SYS_WRITE :=  (EF_external 4%positive SYS_WRITE_SIG).
 Notation SYS_OPEN := (EF_external 8%positive SYS_OPEN_SIG).
 
-Definition int2f_mode (i: int): option f_mode := 
+Definition int2fmode (i: int): option fmode := 
   if Int.eq i Int.zero then Some RDONLY 
   else if Int.eq i Int.one then Some WRONLY
        else if Int.eq i (Int.repr 3%Z) then Some RDWR
@@ -109,11 +170,11 @@ Definition val2oadr (v: val): option address :=
   | Vptr b ofs => Some (b, Int.intval ofs)
   end.
 
-Definition val2omode (mode: val): option f_mode :=
-  match val2oint mode with Some i => int2f_mode i | None => None end.
+Definition val2omode (mode: val): option fmode :=
+  match val2oint mode with Some i => int2fmode i | None => None end.
 
 Section read_write.
-Variable (f: file).
+Variable (f: file) (fptr: nat).
 
 Let contents := get_contents f.
 
@@ -124,9 +185,7 @@ Fixpoint read_file_aux (nbytes sz cur: nat): list memval :=
                  else contents cur :: read_file_aux nbytes' sz (S cur)
   end.
 
-Definition read_file (nbytes: nat): file*list memval :=
-  let bytes := read_file_aux nbytes (get_sz f) (get_cur f) in
-  (mkfile (get_mode f) (get_sz f) (get_cur f + length bytes) (get_contents f), bytes).
+Definition read_file (nbytes: nat): list memval := read_file_aux nbytes (get_size f) fptr.
 
 Fixpoint write_file_aux (bytes: list memval) (sz cur: nat): (nat -> memval)*nat :=
   match bytes with 
@@ -138,35 +197,79 @@ Fixpoint write_file_aux (bytes: list memval) (sz cur: nat): (nat -> memval)*nat 
   end.
 
 Definition write_file (bytes: list memval): file*nat :=
-  match write_file_aux bytes (get_sz f) (get_cur f) with (contents, nwritten) => 
-    (mkfile (get_mode f) (get_sz f + (get_cur f + nwritten - get_sz f)) 
-            (get_cur f + nwritten) (get_contents f), 
-     nwritten)
+  match write_file_aux bytes (get_size f) fptr with (contents, nwritten) => 
+    (mkfile (get_size f + (fptr + nwritten - get_size f)) (get_contents f), nwritten)
   end.
 
 End read_write.
 
-Section alloc_fd.
-Variable (fs: fsys).
+Section fs_read_write.
+Variables (fsys: fs) (fd: int).
 
-Fixpoint find_unused_fd (n: nat) (fdtable: int -> option file) :=
-  match n with 
-  | O => None
-  | S n' => match fdtable (Int.repr (Z_of_nat n)) with 
-            | None => Some (Int.repr (Z_of_nat n))
-            | Some _ => find_unused_fd n' fdtable
-            end
+Definition fs_read (nbytes: nat): option (list memval) := 
+  match get_file fsys fd, get_fptr fsys fd with
+  | Some f, Some cur => Some (read_file f cur nbytes)
+  | _, _ => None
   end.
 
-Definition alloc_fd := 
-   match get_file fs (get_next_fd fs) with
-   | None => Some (get_next_fd fs)
-   | Some _ => find_unused_fd (nat_of_Z Int.modulus) (get_fdtable fs)
-   end.
+Definition fs_write (bytes: list memval): option (nat(*nbytes written*)*fs) :=
+  match get_file fsys fd, get_fptr fsys fd, get_fmode fsys fd with
+  | Some file, Some cur, Some md => 
+    if fwritable md then 
+      match write_file file cur bytes with (new_file, nbytes_written) => 
+        Some (nbytes_written, 
+              mkfs (mkfsdata (fun i => if Int.eq fd i then Some (md, cur + nbytes_written)
+                                       else get_fdtable fsys i)
+                             (fun i => if Int.eq fd i then Some new_file
+                                       else get_fcache fsys i)
+                             (get_nfiles_open fsys)
+                             (get_fnames fsys))
+                   (get_fstore fsys))
+       end
+     else None
+  | _, _, _ => None
+  end.
 
-End alloc_fd.
+End fs_read_write.
+
+Definition new_file := mkfile O (fun _:nat => Undef).
+
+Section fs_open.
+Variable (fsys: fs) (unused_fd: int).
+
+Definition fs_open_existing (fname: int) (f: file) (md: fmode): fs :=
+  mkfs (mkfsdata (fun i => if Int.eq i unused_fd then Some (md, O) 
+                           else get_fdtable fsys i)
+                 (fun i => if Int.eq i unused_fd then Some f
+                           else get_fcache fsys i)
+                 (Int.add Int.one (get_nfiles_open fsys))
+                 (fun i => if Int.eq i unused_fd then Some fname 
+                           else get_fnames fsys i))
+       (get_fstore fsys).
+
+Definition fs_open_new (fname: int) (md: fmode): fs := 
+  mkfs (mkfsdata (fun i => if Int.eq i unused_fd then Some (md, O) 
+                           else get_fdtable fsys i)
+                 (fun i => if Int.eq i unused_fd then Some new_file
+                           else get_fcache fsys i)
+                 (Int.add Int.one (get_nfiles_open fsys))
+                 (fun i => if Int.eq i unused_fd then Some fname 
+                           else get_fnames fsys i))
+       (get_fstore fsys).
+
+Definition fs_open (fname: int) (md: fmode): option fs :=
+  match fwritable md, get_fstore fsys fname with
+  | false, Some f => Some (fs_open_existing fname f md)
+  | false, None => None
+  | true, Some f => Some (fs_open_existing fname f md)
+  | true, None => Some (fs_open_new fname md)
+  end.
+
+End fs_open.
 
 Definition handled: list AST.external_function := SYS_READ::SYS_WRITE::SYS_OPEN::nil.
+
+Definition is_open (fsys: fs) (fname: int) := exists fd, get_fnames fsys fd = Some fname.
 
 Inductive os_step: genv -> xT -> mem -> xT -> mem -> Prop :=
 | os_corestep: forall ge s m s' m',
@@ -174,54 +277,42 @@ Inductive os_step: genv -> xT -> mem -> xT -> mem -> Prop :=
   get_ext s=get_ext s' -> 
   corestep csem ge (get_core s) m (get_core s') m' -> 
   os_step ge s m s' m'
-| os_open: forall ge z s m c mode unused_fd fmode,
+| os_open: forall ge z s m c md0 fname0 md fname unused_fd fs',
   let fs := get_fs s in 
-  alloc_fd fs = Some unused_fd -> 
-  at_external csem (get_core s) = Some (SYS_OPEN, SYS_OPEN_SIG, mode::nil) ->
-  after_external csem (Some (Vint (get_next_fd fs))) (get_core s) = Some c -> 
-  val2omode mode = Some fmode -> 
-  let new_file := mkfile fmode O O (fun _:nat => Undef) in
-  os_step ge s m (mkxT z c 
-    (mkfsys (fun fd => if Int.eq fd unused_fd then Some new_file else get_fdtable fs fd)
-            (S (get_nopen fs))
-            (Int.add unused_fd Int.one))) m
-| os_read: forall ge z s m c fd fd' adr buf nbytes nbytes' file file' bytes m',
+  at_external csem (get_core s) = Some (SYS_OPEN, SYS_OPEN_SIG, fname0::md0::nil) ->
+  alloc_fd fs = Some unused_fd ->
+  val2omode md0 = Some md -> 
+  val2oint fname0 = Some fname -> 
+  ~is_open fs fname -> 
+  fs_open fs unused_fd fname md = Some fs' -> 
+  after_external csem (Some (Vint unused_fd)) (get_core s) = Some c -> 
+  os_step ge s m (mkxT z c fs') m
+| os_read: forall ge z s m c fd0 fd buf adr nbytes0 nbytes bytes m',
   let fs := get_fs s in
-  at_external csem (get_core s) = Some (SYS_READ, SYS_READ_SIG, fd::buf::nbytes::nil) ->
-  val2oint fd = Some fd' -> 
+  at_external csem (get_core s) = Some (SYS_READ, SYS_READ_SIG, fd0::buf::nbytes0::nil) ->
+  val2oint fd0 = Some fd -> 
   val2oadr buf = Some adr -> 
-  val2oint nbytes = Some nbytes' -> 
-  get_file fs fd' = Some file -> 
-  read_file file (nat_of_Z (Int.intval nbytes')) = (file', bytes) -> 
+  val2oint nbytes0 = Some nbytes -> 
+  fs_read fs fd (nat_of_Z (Int.intval nbytes)) = Some bytes -> 
   Mem.storebytes m (fst adr) (snd adr) bytes = Some m' -> 
   after_external csem (Some (Vint (Int.repr (Zlength bytes)))) (get_core s) = Some c -> 
-  os_step ge s m (mkxT z c
-    (mkfsys (fun fd => if Int.eq fd fd' then Some file' else get_file fs fd)
-            (get_nopen fs)
-            (get_next_fd fs))) m'
-| os_write: forall ge z s m c fd fd' adr buf nbytes nbytes' nwritten file file' bytes,
+  os_step ge s m (mkxT z c fs) m'
+| os_write: forall ge z s m c fd0 fd adr buf nbytes0 nbytes bytes fs' nbytes_written,
   let fs := get_fs s in
-  at_external csem (get_core s) = Some (SYS_WRITE, SYS_WRITE_SIG, fd::buf::nbytes::nil) ->
-  val2oint fd = Some fd' -> 
+  at_external csem (get_core s) = Some (SYS_WRITE, SYS_WRITE_SIG, fd0::buf::nbytes0::nil) ->
+  val2oint fd0 = Some fd -> 
   val2oadr buf = Some adr -> 
-  val2oint nbytes = Some nbytes' -> 
-  get_file fs fd' = Some file -> 
-  f_writable (get_mode file) = true -> 
-  Mem.loadbytes m (fst adr) (snd adr) (Int.intval nbytes') = Some bytes -> 
-  write_file file bytes = (file', nwritten) -> 
-  after_external csem (Some (Vint (Int.repr (Z_of_nat nwritten)))) (get_core s) = Some c -> 
-  os_step ge s m (mkxT z c
-    (mkfsys (fun fd => if Int.eq fd fd' then Some file' else get_file fs fd)
-            (get_nopen fs)
-            (get_next_fd fs))) m.
+  val2oint nbytes0 = Some nbytes -> 
+  Mem.loadbytes m (fst adr) (snd adr) (Int.intval nbytes) = Some bytes -> 
+  fs_write fs fd bytes = Some (nbytes_written, fs') -> 
+  after_external csem (Some (Vint (Int.repr (Z_of_nat nbytes_written)))) (get_core s) = Some c -> 
+  os_step ge s m (mkxT z c fs') m.
 
 Definition os_initial_mem := sim.initial_mem csem.
 
-Definition initial_fs: fsys := mkfsys (fun _:int => None) O Int.zero.
-
 Definition os_make_initial_core (ge: genv) (v: val) (args: list val): option xT :=
   match make_initial_core csem ge v args with
-  | Some c => Some (mkxT init_world c initial_fs)
+  | Some c => Some (mkxT init_world c (mount_fs (fun _: int => None)))
   | None => None
   end.
 
@@ -257,7 +348,7 @@ case_eq (at_external csem (get_core q)).
 intros [[ef sig] args] H1.
 inv H.
 apply corestep_not_at_external in H3; congruence.
-unfold os_at_external; rewrite H2; auto.
+unfold os_at_external; rewrite H0; auto.
 unfold os_at_external; rewrite H0; auto.
 unfold os_at_external; rewrite H0; auto.
 Qed.
@@ -276,22 +367,28 @@ Qed.
 
 Definition JuicyFSCoreSem := juicy_core_sem FSCoreSem.
 
+Definition isSome {A: Type} (o: option A) :=
+  match o with Some _ => true | None => false end.
+
 Definition Client_FSExtSpec :=
-  Build_external_specification juicy_mem AST.external_function (Z*fsys)
+  Build_external_specification juicy_mem AST.external_function (Z*fs)
   (fun ef: AST.external_function => unit (*placeholder for now*))
   (fun (ef: AST.external_function) u typs args zfs jm => 
-    match ef, zfs with
-      SYS_OPEN, (z, fs) => get_nopen fs < nat_of_Z Int.modulus 
-    | _, (_, _) => False (*TODO*)
+    match ef, zfs, args with
+      SYS_OPEN, (z, fs), mode::nil => 
+      nat_of_Z (Int.intval (get_nfiles_open fs)) < nat_of_Z Int.max_unsigned
+    | SYS_READ, (z, fs), fd::buf::nbytes => False (*TODO*)
+    | SYS_WRITE, (z, fs), fd::buf::nbytes => False (*TODO*)
+    | _, _, _ => False
     end)
   (fun (ef: AST.external_function) u ty retval zfs jm => 
     match ef, zfs with
-    | SYS_OPEN, (z, fs) => get_nopen fs < nat_of_Z Int.modulus 
-    | _, (_, _) => False (*TODO*)
+    | SYS_OPEN, (z, fs) => True (*TODO*)
+    | _, (_, _) => True (*TODO*)
     end).
 
 Program Definition Client_JuicyFSExtSpec := 
-  Build_juicy_ext_spec (Z*fsys) Client_FSExtSpec _ _.
+  Build_juicy_ext_spec (Z*fs) Client_FSExtSpec _ _.
 Next Obligation. hnf; intros a a' H; destruct e; auto. Qed.
 Next Obligation. hnf; intros a a' H; destruct e; auto. Qed.
 
