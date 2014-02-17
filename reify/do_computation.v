4a873bce04ccd725b1ee3cb061f8a76047f68e39
Mario Alvarez <mmcgil@princeton.edu>
2014-02-17 04:04:01 +0000

    Added a function, do_computation_equal, for computing on equalities, using the decision procedures built into the reified types.
    Also added decision procedures to many of the types that it seemed
    like it would be useful to have decision procedures for,
    in order to be able to test do_computation_equal.
    
    I still need to prove the correcness of do_computation_equal, but
    hopefully most of the heavy lifting has already been done in the
    correctness proof of do_computation.

--- reify/do_computation.v
+++ reify/do_computation.v
@@ -55,8 +55,9 @@ Fixpoint is_const (e : expr all_types) :=
       (* it and its arguments must be const *)
     | Func f es => andb (is_const_base f) (is_const_l es)
     | Const _ _ => true
-    | Equal _ l r => andb (is_const l) (is_const r)
+(*    | Equal _ l r => andb (is_const l) (is_const r) *)
     | _ => false
+
   end.             
 
 (* Simplify an expression, if we can.
@@ -94,5 +95,66 @@ Eval vm_compute in (do_computation nil (Func 13 [(Func 1 [])]) (tvType 11)).
 Eval vm_compute in (do_computation nil (Equal nat_tv (Func 1 []) (Func 1 [])) tvProp).
 Eval vm_compute in (do_computation nil (Equal nat_tv (Func 13 [(Func 1 [])]) (Func 1 [])) tvProp).
 Eval vm_compute in (do_computation nil (Equal nat_tv (Func 13 [(Func 1 [])]) (Func 13 [(Func 1 [])])) tvProp).
+
+Print expr.
+Print tvar.
+Check nth_error.
+
+Eval vm_compute in (nth_error [1; 2; 3; 4] 3).
+Print Impl.
+Print Sig.
+
+(* Computes an Expr that represents an equality (for types with decidable equality), 
+ * Appealing to the decision procedures contained in the type signature *)
+Check const_seqb.
+Print const_seqb.
+Locate const_seqb.
+
+
+Definition do_computation_equal (user_functions : list (signature all_types)) (e : expr all_types) : bool.
+remember e as exp; destruct e.
+(* We just return false in all cases except for Equal *)
+- apply false.
+- apply false.
+- apply false.
+(* Equal *)
+- destruct (do_computation user_functions e1 t).
+  (* lhs successfully computes *)
+  + destruct (do_computation user_functions e2 t).
+    (* rhs also successfully computes *)
+    { destruct t.
+      (* if t is tvProp, give up (no equality on Props!) *)
+      - apply false.
+      (* if t is an actual type, things get interesting.
+         we need to do some manipulation to get types to work out.
+         a naive approach would lead to Coq thinking that the implementation
+         pulled out of the types list, and the implementation from the tvarD
+         returned by do_computation don't match - but they do! *)
+      - unfold tvarD in *. unfold Impl_ in *.
+        destruct (nth_error all_types n).
+        (* if we found the type (we should have...) actually check equality *)
+        + apply (t.(Eqb) t0 t1).
+        (* otherwise, give up *)
+        + apply false.
+    }
+    (* rhs failed to compute *)
+    { apply false. }
+  (* lhs failed to compute *)
+  + apply false.
+(* the rest... *)
+- apply false.
+- apply false.
+Defined.
+
+
+(* some tests for do_compute_prop *)
+Eval vm_compute in (do_computation nil (Func 1 []) (tvType 11)).
+Eval vm_compute in (do_computation_equal nil (Equal (tvType 11) (Func 13 [(Func 1 [])]) (Func 13 [(Func 1 [])]))).
+
+Eval vm_compute in (do_computation nil (Func 13 [(Func 1 [])]) (tvType 11)).
+Eval vm_compute in (do_computation nil (Equal nat_tv (Func 1 []) (Func 1 [])) tvProp).
+Eval vm_compute in (do_computation nil (Equal nat_tv (Func 13 [(Func 1 [])]) (Func 1 [])) tvProp).
+Eval vm_compute in (do_computation nil (Equal nat_tv (Func 13 [(Func 1 [])]) (Func 13 [(Func 1 [])])) tvProp).
+
 
 End do_computation.
--- reify/types.v
+++ reify/types.v
@@ -26,6 +26,7 @@ Definition environ_type : Expr.type := no_eqb_type environ.
 
 Definition val_type : Expr.type := no_eqb_type val. 
 (*TODO: might want equality here*)
+(* but we might have to define it ourselves... *)
 
 
 Definition share_type : Expr.type := no_eqb_type share.
@@ -37,19 +38,110 @@ Definition ident_type :=
 
 Definition list_val_type : Expr.type := no_eqb_type (list val).
 
-Definition list_int_type := no_eqb_type (list int).
+Definition int_type : Expr.type.
+  refine ({| Expr.Impl := int
+             ; Expr.Eqb := Int.eq
+             ; Expr.Eqb_correct := _ |}).
+  (* have to reverse arguments to this lemma *)
+  intros. symmetry in H. apply binop_lemmas.int_eq_true in H.
+  assumption.
+Defined.
 
-Definition int_type := no_eqb_type int.
+(* Generic equality procedure for lists of things *)
+(* I'll do this later if it looks like it will actually be necessary *)
+(*Fixpoint lists_exact_zip (T1 T2 T3 : Type)
+         (f : T1 -> T2 -> T3) (l1 : list T1) (l2 : list T2) : option T3 := 
+match l1 with
+  | h1
+  | (cons h1 t1) =>
+    match l2 with
+      | (cons h2 t2) =>
+        (cons (f h1 h2) (lists_exact_zip T1 T2 T3 f t1 t2))
+      | nil => nil
+    end
+  | nil => nil
+end.
 
-Definition Z_type := no_eqb_type Z.
+Definition make_list_eqb (T : Type) (beq : (T -> T -> bool)) (l1 l2 : list T) : bool :=
+  let each_equal := lists_zip T T bool beq l1 l2 in
+  fold_right andb true each_equal.
+*)
+Fixpoint beq_list_int (l1 l2 : list int) : bool :=
+  match l1 with
+    | nil => 
+      match l2 with
+        | nil => true
+        | _ => false
+      end
+    | (cons h1 t1) =>
+      match l2 with
+        | nil => false
+        | (cons h2 t2) =>
+          (andb (Int.eq h1 h2) (beq_list_int t1 t2))
+      end
+  end.  
 
-Definition nat_type := no_eqb_type nat.
+Definition list_int_type : Expr.type.
+  refine ({| Expr.Impl := list int
+             ; Expr.Eqb := beq_list_int
+             ; Expr.Eqb_correct := _ |}).
+  intro x. induction x.
+  - intros.
+    destruct y.
+    + reflexivity.
+    + inversion H.
+  - intros.
+    destruct y.
+    + inversion H.
+    + inversion H.
+      symmetry in H1.
+      apply andb_true_eq in H1. inversion H1.
+      symmetry in H2.
+      apply IHx in H2. 
+      symmetry in H0.
+      apply int_eq_e in H0.
+      rewrite H0. rewrite H2. reflexivity.
+Defined.
 
-Definition positive_type := no_eqb_type positive.
+Definition Z_type :=
+  ({| Expr.Impl := Z
+      ; Expr.Eqb := Zeq_bool
+      ; Expr.Eqb_correct := Zeq_bool_eq |}).
 
-Definition bool_type := no_eqb_type bool.
+Definition nat_type := 
+  ({| Expr.Impl := nat
+      ; Expr.Eqb := beq_nat
+      ; Expr.Eqb_correct := beq_nat_true |}).
 
-Definition comparison_type := no_eqb_type comparison.
+Definition positive_type := 
+  ({| Expr.Impl := positive
+      ; Expr.Eqb := Pos.eqb
+      ; Expr.Eqb_correct := Peqb_true_eq |}).
+
+Definition bool_type := 
+  ({| Expr.Impl := bool
+      ; Expr.Eqb := eqb
+      ; Expr.Eqb_correct := eqb_prop |}).
+
+(* equality for comparisons; apparently doesn't exist? *)
+Definition eqb_comparison (c1 c2 : comparison) : bool :=
+  match (c1, c2) with
+    | (Ceq, Ceq) => true
+    | (Cne, Cne) => true
+    | (Clt, Clt) => true
+    | (Cle, Cle) => true
+    | (Cgt, Cgt) => true
+    | (Cge, Cge) => true
+    | _ => false
+  end.
+
+Definition comparison_type : Expr.type.
+  refine ({| Expr.Impl := comparison
+             ; Expr.Eqb := eqb_comparison
+             ; Expr.Eqb_correct := _ |}).
+  intros.
+  destruct x; destruct y; inversion H; reflexivity.
+Defined.
 
 Module Type unknown_types.
 Parameter (unknown_types : (list Expr.type)).
