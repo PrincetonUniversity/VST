c176f99c66cb8867ea4acc9ceb61ac00bad83b57
Gordon Stewart <jsseven@princeton.edu>
2014-04-09 16:31:49 +0000

    Prove corestep diagram of semantic vs. syntactic linking under assumptions on stacking monoid mentioned in previous commit messages. These assumptions will need to be validated for at least one language (e.g., C light), but they seem reasonable.

--- linking/stacking.v
+++ linking/stacking.v
@@ -49,20 +49,33 @@ Parameter tge_closed :
 Import Stacking_monoid.
 
 Record t : Type := mk 
-  { congr : forall d m d' m' c,
-            corestep mon.(sem) tge d m d' m' -> 
-            corestep_plus mon.(sem) tge (mon.(op) d c) m (mon.(op) d' c) m'
+  { step_congr : 
+      forall d m d' m' c,
+      corestep mon.(sem) tge d m d' m' -> 
+      corestep_plus mon.(sem) tge (mon.(op) d c) m (mon.(op) d' c) m'
 
-  ; call  : forall c ef dep_sig sig args d m id,
-            at_external mon.(sem) c = Some (ef,dep_sig,args) ->
-            ef = EF_external id sig -> 
-            initial_core mon.(sem) tge (Vptr id Int.zero) args = Some d -> 
-            corestep_plus mon.(sem) tge c m (mon.(op) d c) m 
+  ; atext_congr : 
+      forall c d ef dep_sig args,
+      at_external (sem mon) d = Some (ef,dep_sig,args) -> 
+      at_external (sem mon) (mon.(op) d c) = Some (ef,dep_sig,args)
 
-  ; ret   : forall c d v c' m,
-            halted mon.(sem) d = Some v -> 
-            after_external mon.(sem) (Some v) c = Some c' -> 
-            corestep_plus mon.(sem) tge (mon.(op) d c) m c' m }.
+  ; aftext_congr : 
+      forall c d rv d',
+      after_external (sem mon) rv d = Some d' ->
+      after_external (sem mon) rv (mon.(op) d c) = Some (mon.(op) d' c)
+
+  ; call : 
+      forall c ef dep_sig sig args d m id,
+      at_external mon.(sem) c = Some (ef,dep_sig,args) ->
+      ef = EF_external id sig -> 
+      initial_core mon.(sem) tge (Vptr id Int.zero) args = Some d -> 
+      corestep_plus mon.(sem) tge c m (mon.(op) d c) m 
+
+  ; ret : 
+      forall c d v c' m,
+      halted mon.(sem) d = Some v -> 
+      after_external mon.(sem) (Some v) c = Some c' -> 
+      corestep_plus mon.(sem) tge (mon.(op) d c) m c' m }.
 
 End Stacking_monoid_ok. End Stacking_monoid_ok.
 
@@ -171,14 +184,65 @@ move=> b l pf.
 have x: is_true (0 < size [:: b & l]) by [].
 rewrite (@fold_stack_consE a (b :: l) x); case=> <-.
 exists (op mon c' (projT1 (fold_stack_nonempty x))); split.
-apply: Stacking_monoid_ok.congr=> //.
+apply: Stacking_monoid_ok.step_congr=> //.
 by apply: mon_ok.
 move: (projT2 (fold_stack_nonempty x)).
 move: (@fold_stack_nonempty (b :: l)).
 pattern pf at 0.
 by case: (fold_stack _)=> // d pf'; case=> <-. }
 
-Admitted.
+{ move=> l m ef dep_sig args id d_ix d pf.
+move=> sz atext fnid fntbl init fld; rewrite /pushCore.
+move: (stack_push_wf l) pf.
+pattern (all (atExternal semantics) (CallStack.callStack (Linker.stack l))) 
+ at 0 1 2.
+move: sz atext fntbl fld; case: l=> /= fn_tbl; case=> /=; case=> // a.
+case=> // b l pf lt atext fntbl fld IH all_atext; rewrite fld.
+exists (op mon d c); split=> //.
+move: fnid; rewrite /LinkerSem.fun_id; case: ef atext=> // id' sig atext.
+case=> eq; subst id'.
+apply: (@Stacking_monoid_ok.call _ _ _ mon _ mon_ok
+        _ (EF_external id sig) dep_sig sig args d _ id)=> //.
+move: atext=> /=.
+have x: is_true (0 < size [:: b & l]) by [].
+move: fld; rewrite (@fold_stack_consE a (b :: l) x); case=> <-.
+by apply: (Stacking_monoid_ok.atext_congr mon_ok). }
+
+{ Opaque fold_stack.
+move=> l l'' m rv d' lt pop hlt aftext fld; case: l lt pop hlt fld.
+move=> fn_tbl; case=> /=; case=> // a; case=> // b; case=> /=.
++ move=> pf lt pop hlt fld.
+  Transparent fold_stack. simpl in fld. case: fld=> <-.
+  move: pop; case/popCoreE=> pf2 []inCtx l''_eq; rewrite /= in l''_eq.
+  move: aftext; rewrite l''_eq /= => aftext; exists d'; split=> //.
+  by apply: (@Stacking_monoid_ok.ret _ _ _ mon _ mon_ok _ _ rv _ _ hlt).
++ Opaque fold_stack. move=> q l pf lt pop hlt fld.
+  move: pop; case/popCoreE=> pf2 []inCtx l''_eq; rewrite /= in l''_eq.
+  move: aftext; rewrite l''_eq /= => aftext.
+  have x: is_true (0 < size [:: q & l]) by [].
+  rewrite (@fold_stack_consE (Core.upd b d') (q :: l) x).
+  exists (op mon d' (projT1 (fold_stack_nonempty x))); split=> //.
+  have y: is_true (0 < size [:: b, q & l]) by [].
+  move: (fld); rewrite (fold_stack_consE _ y); case=> <-.
+  apply: (@Stacking_monoid_ok.ret _ _ _ mon _ mon_ok _ _ rv _ _ hlt)=> //.
+  move: aftext.
+  move: (projT2 (fold_stack_nonempty y)). 
+  move: (@fold_stack_nonempty (b :: q :: l)).
+  pattern pf at 0; case e: (fold_stack _)=> //[bql] nempty; case=> <- => aftext.
+  have [ql fld_ql]: exists ql, fold_stack [:: q & l] = Some ql.
+  { by case: (fold_stack_nonempty x)=> ql eq; exists ql. }
+  move: (@Stacking_monoid_ok.aftext_congr _ _ _ mon _ mon_ok 
+         ql _ (Some rv) _ aftext).
+  have eq_bql: bql = op mon (Core.c b) ql.
+  { Transparent fold_stack. clear - e fld_ql. 
+    move: e fld_ql=> /=; case: l=> //; first by case=> <-; case=> <-.
+    move=> a l; case f: (fold_stack _)=> [x|//].
+    by case=> <-; case=> <-. }
+  rewrite eq_bql=> ->; f_equal.
+  Opaque fold_stack.
+  clear - fld_ql; case: (fold_stack_nonempty x)=> /=.
+  by rewrite fld_ql=> ?; case=> <-. }
+Qed.
 
 End Stacking_simulation.
 
