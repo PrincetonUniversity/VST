e683497c298f5aa43511a62132dc61d579472edd
Mario Alvarez <mmcgil@princeton.edu>
2014-04-09 17:28:14 +0000

    Finished proving Navarro Perez's list-segment lemmas.

--- reify/lseg_lemmas.v
+++ reify/lseg_lemmas.v
@@ -59,20 +59,19 @@ Proof.
   rewrite lseg_unroll. entailer.
   rewrite sepcon_comm.
   rewrite distrib_orp_sepcon. apply orp_left.
-  entailer.
-  unfold lseg_cons.
-  entailer.
-  rewrite sepcon_comm.
-  rewrite sepcon_assoc.
-  rewrite (sepcon_comm (list_cell ls sh h x)).
-  rewrite sepcon_assoc.
-  rewrite <- sepcon_assoc.
-  eapply derives_trans.
-  apply sepcon_derives.
-  apply field_at_conflict.
-  apply TT_right.
-  rewrite FF_sepcon.
-  apply FF_left.
+  - entailer.
+  - unfold lseg_cons.
+    entailer.
+    rewrite sepcon_comm.
+    rewrite sepcon_assoc.
+    rewrite (sepcon_comm (list_cell ls sh h x)).
+    rewrite sepcon_assoc.
+    rewrite <- sepcon_assoc.
+    eapply derives_trans.
+    + apply sepcon_derives; try (apply field_at_conflict).
+      apply TT_right.
+    + rewrite FF_sepcon.
+      apply FF_left.
 Qed.
 
 Definition NP_W4 := next_lseg_equal.
@@ -80,6 +79,15 @@ Definition NP_W4 := next_lseg_equal.
 Lemma neq_ptr_neq : forall x y, x <> y -> ptr_neq x y.
 Proof.
   intros; unfold ptr_neq; unfold not; intro peq; apply ptr_eq_e in peq; tauto.
+Qed.
+
+(* Useful in some of the lemmas that follow *)
+Lemma FF_elim : forall a b, a |-- FF -> a * b |-- FF.
+Proof.
+  intros.
+  rewrite <- FF_sepcon with (P := TT).
+  apply sepcon_derives. assumption.
+  apply TT_right.
 Qed.
 
 (* W5 *)
@@ -100,11 +108,6 @@ Proof.
     apply orp_left.
     + entailer.
     + unfold lseg_cons. entailer.
-      assert (forall a b, a |-- FF -> a * b |-- FF)%logic as FF_elim.
-      intros.
-      rewrite <- FF_sepcon with (P := TT).
-      eapply sepcon_derives. assumption.
-      eapply TT_right.
       eapply derives_trans.
       * apply FF_elim. rewrite sepcon_comm. rewrite <- sepcon_assoc.
         apply FF_elim. rewrite <- sepcon_assoc.
@@ -124,9 +127,9 @@ Definition NP_W5 := lseg_conflict.
 (* U1 *)
 Lemma first_field_at_lseg :
   forall T id sh ls h x z,
-    (field_at sh (tptr T) id z x * list_cell ls sh h x ===> 
-       (inj (x = z) * field_at sh (tptr T) id z x * list_cell ls sh h x) ||
-       (@lseg (tptr T) id ls sh (cons h nil) x z)).
+    star (field_at sh (tptr T) id z x) (list_cell ls sh h x) ===> 
+       star (star (inj (x = z)) (field_at sh (tptr T) id z x)) (list_cell ls sh h x) ||
+       (@lseg (tptr T) id ls sh (cons h nil) x z).
 Proof.
   intros.
   sep_solve.
@@ -156,65 +159,98 @@ Definition NP_U1 := first_field_at_lseg.
 (* U2 *)
 Lemma next_field_at_lseg :
   forall T id sh ls h contents x y z,
-    (field_at sh (tptr T) id y x * list_cell ls sh h x *
-     @lseg (tptr T) id ls sh contents y z) ===>
-    (inj (x = z) * field_at sh (tptr T) id y x * list_cell ls sh h x * @lseg (tptr T) id ls sh contents y z) ||
+    star (field_at sh (tptr T) id y x) (star (list_cell ls sh h x)
+     (@lseg (tptr T) id ls sh contents y z)) ===>
+    star (inj (x = z)) (star (field_at sh (tptr T) id y x)
+                       (star (list_cell ls sh h x)
+                               (@lseg (tptr T) id ls sh contents y z))) ||
     (@lseg (tptr T) id ls sh (cons h contents) x z).
 Proof.
-Admitted.
+intros.
+sep_solve.
+destruct (EqDec_val x z).
+- subst.
+  apply orp_right1.
+  entailer.
+- apply orp_right2.
+  rewrite lseg_unroll.
+  entailer.
+Qed.
 
 Definition NP_U2 := next_field_at_lseg.
+
 (* U3 *)
-(*
-Lemma lseg_nil_append : forall T id1 id2 sh ls1 ls2 c1 c2 x y,
-      @lseg T id1 ls1 sh c1 x y * @lseg T id2 ls2 sh c2 y nullval |--
-      @lseg T id1 ls1 sh (c1 ++ c2) x nullval.
-Check list_append.
-Lemma
- *)
-
-(*Proof.
+Lemma lseg_nil_append : forall T id sh ls c1 c2 x y,
+      star (@lseg T id ls sh c1 x y) (@lseg T id ls sh c2 y nullval) ===>
+      (@lseg T id ls sh (c1 ++ c2) x nullval).
+Proof.
 intros.
-eexists.
-rewrite lseg_neq.
-SearchAbout lseg_cons.
-unfold lseg_cons.
-SearchAbout field_at.
- normalize.
+sep_solve.
+pose (fun (v : val) => emp) as empred.
+assert ( lseg ls sh c1 x y * lseg ls sh c2 y nullval * (empred nullval)
+         |-- lseg ls sh (c1 ++ c2) x nullval * (empred nullval)) as lseg_fact'.
+- apply list_append.
+  intros.
+  unfold lseg_cell. entailer.
+- unfold empred in lseg_fact'.
+  autorewrite with norm in *; try (assumption); try (apply Cveric).
+Qed.
 
-SearchAbout (!!(_) && _).
+Definition NP_U3 := lseg_nil_append.
 
-apply neq_ptr_neq in H.
-erewrite lseg_neq.
-Focus 2.
-unfold ptr_neq. unfold not.
-erewrite lseg_unroll.
-eapply orp_right2.
-unfold lseg_cons. entailer.
-Check list_append.
-Admitted. *)
-
-(* U2 *)
-(*Lemma next_lseg_lseg : forall T id sh ls x z,
-      x <> z -> *)
-
-(*
-Check @lseg.
-Lemma first_mapsto_lseg : forall T ll sh ls x z,
-      exists contents,
-      ((prop (x =/= z)) && (mapsto sh T x z))%logic |-- @lseg T ll ls sh contents x z.
-
-Lemma empty_list_emp : forall T ll ls sh lem x,
-      is_pointer_or_null x ->
-      @lseg T ll ls sh lem x x |-- emp.
+(* U4 *)
+Lemma lseg_next_append : forall T id sh ls c1 c2 h x y z w,
+      star (@lseg T id ls sh c1 x y)
+           (star (@lseg T id ls sh c2 y z)
+                 (star (field_at sh (tptr T) id w z)
+                       (list_cell ls sh h z))) ===>
+      star (@lseg T id ls sh (c1 ++ c2) x z)
+           (star (field_at sh (tptr T) id w z)
+                 (list_cell ls sh h z)).
+Proof.
 intros.
-rewrite lseg_eq.
-SearchAbout (_ |-- emp * _).
-unfold lseg. unfold lseg'.
-SearchAbout lseg.
-Locate lseg_nil_eq.
-Check lseg.
-Check listspec.
-Check lseg.
-SearchAbout mapsto.
- *)
+unfold himp, star, inj.
+entailer.
+Qed.
+
+Definition NP_U4 := lseg_nil_append.
+
+(* U5 *)
+Lemma three_lseg_append : forall T id sh ls c1 c2 c3 x y z w,
+      star (@lseg T id ls sh c1 x y)
+           (star (@lseg T id ls sh c2 y z)
+                 (@lseg T id ls sh c3 z w)) ===>
+      star (@lseg T id ls sh (c1 ++ c2) x z) (@lseg T id ls sh c3 z w) ||
+      star (inj (z = w)) 
+           (star (@lseg T id ls sh c1 x y)
+           (star (@lseg T id ls sh c2 y z)
+                 (@lseg T id ls sh c3 z w))).
+Proof.
+intros.
+sep_solve.
+destruct (EqDec_val z w).
+- subst. apply orp_right2.
+  entailer.
+- apply orp_right1.
+  entailer.
+  rewrite (sepcon_comm (lseg ls sh c3 z w)).
+  pose (lpred := fun (z : val) => lseg ls sh c3 z w).
+  assert ( lseg ls sh c1 x y * lseg ls sh c2 y z * (lpred z)
+           |-- lseg ls sh (c1 ++ c2) x z * (lpred z)) as lseg_fact'.
+  + apply list_append.
+    intros.
+    unfold lseg_cell. unfold lpred.
+    entailer.
+    rewrite lseg_neq.
+    * unfold lseg_cons.
+      entailer.
+      apply FF_elim.
+      rewrite sepcon_comm. rewrite <- sepcon_assoc. apply FF_elim.
+      rewrite sepcon_comm. rewrite sepcon_assoc. rewrite sepcon_comm. apply FF_elim.
+      apply field_at_conflict.
+    * apply neq_ptr_neq; assumption.
+  + assumption.
+Qed.
+
+Definition NP_U5 := three_lseg_append.
+
