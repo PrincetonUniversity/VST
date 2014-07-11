bdc7ce56eec339cebaa1964cd6c4388fedfa5304
Qinxiang Cao <qinxiang@princeton.edu>
2014-07-11 17:37:04 +0000

    verif_sha_update2.v is done

--- sha/verif_sha_update2.v
+++ sha/verif_sha_update2.v
@@ -191,17 +191,15 @@ Proof.
 rewrite cVint_force_int_ZnthV
  by (rewrite initial_world.Zlength_map; omega).
  rewrite memory_block_array_tuchar by omega.
- rewrite (split_array_at (Zlength dd) _ _ (ZnthV tuchar (map Vint (map Int.repr dd))))
-    by omega.
+ rewrite split_offset_array_at with (contents :=ZnthV tuchar (map Vint (map Int.repr dd))) (lo := Zlength dd); [| omega | simpl; omega | reflexivity].
  rewrite (split_array_at (64-Zlength dd) _ _ (tuchars (map Int.repr data)))
     by omega.
+
  replace (offset_val (Int.repr (40 + Zlength dd)) c)
           with (offset_val (Int.repr (sizeof tuchar * Zlength dd)) (offset_val (Int.repr 40) c))
   by (change (sizeof tuchar) with 1%Z; rewrite Z.mul_1_l; rewrite offset_offset_val, add_repr; auto).
-rewrite <- offset_val_array_at_.
-  rewrite Z.add_0_r.
-  replace (Zlength dd + (64-Zlength dd))%Z with 64%Z by omega.
  cancel.
+ 
  after_call.
  replace_SEP 0%Z (`(array_at tuchar sh
            (cVint (force_int oo ZnthV tuchar (map Vint (map Int.repr data))))
@@ -213,12 +211,84 @@ rewrite <- offset_val_array_at_.
  entailer!.
  repeat flatten_sepcon_in_SEP.
  fold j k.
+(*
+  Espec : OracleKind
+  hashed : list int
+  dd : list Z
+  data : list Z
+  c : val
+  d : val
+  sh : share
+  len : nat
+  hi : int
+  lo : int
+  H : (Z.of_nat len <= Zlength data)%Z
+  H7 : ((Zlength hashed * 4 + Zlength dd) * 8)%Z = hilo hi lo
+  H3 : (Zlength dd < CBLOCKz)%Z
+  H3' : Forall isbyteZ dd
+  H4 : (LBLOCKz | Zlength hashed)
+  Hlen : (Z.of_nat len <= Int.max_unsigned)%Z
+  c' : name _c
+  data_ : name _data
+  len' : name _len
+  data' : name _data
+  p : name _p
+  n : name _n
+  fragment_ : name _fragment
+  j := (40 + Zlength dd)%Z : Z
+  k := (64 - Zlength dd)%Z : Z
+  H0 : (0 < k <= 64)%Z
+  H1 : (64 < Int.max_unsigned)%Z
+  DBYTES : Forall isbyteZ data
+  fragment : val
+  Delta := abbreviate : tycontext
+  POSTCONDITION := abbreviate : ret_assert
+  MORE_COMMANDS := abbreviate : statement
+  H2 : offset_in_range 64 (offset_val (Int.repr 40) c)
+  retval0 : val
+  ============================
+   semax Delta
+     (PROP  ()
+      LOCAL  (`(eq fragment) (eval_id _fragment);
+      `(typed_true tint)
+        (eval_expr
+           (Ebinop Oge (Etempvar _len tuint) (Etempvar _fragment tuint) tint));
+      `(eq (Vint (Int.repr k))) (eval_id _fragment);
+      `(eq (offset_val (Int.repr 40) c)) (eval_id _p);
+      `(eq (Vint (Int.repr (Zlength dd)))) (eval_id _n);
+      `(eq c) (eval_id _c); `(eq d) (eval_id _data);
+      `(eq (Vint (Int.repr (Z.of_nat len)))) (eval_id _len))
+      SEP 
+      (`(array_at tuchar sh
+           (cVint (force_int oo ZnthV tuchar (map Vint (map Int.repr data))))
+           0 (Int.unsigned (force_int fragment)) d);
+      `(array_at tuchar Tsh
+          (cVint (force_int oo ZnthV tuchar (map Vint (map Int.repr data))))
+          0 (Int.unsigned (force_int fragment)) (offset_val (Int.repr j) c));
+      `(array_at tuint Tsh (tuints (hash_blocks init_registers hashed)) 0 8 c);
+      `(sha256_length (hilo hi lo + Z.of_nat len * 8) c);
+      `(array_at tuchar Tsh (ZnthV tuchar (map Vint (map Int.repr dd))) 0
+          (Zlength dd) (offset_val (Int.repr 40) c));
+      `(field_at Tsh t_struct_SHA256state_st [_num]
+          (Vint (Int.repr (Zlength dd))) c);
+      `K_vector (eval_var _K256 (tarray tuint 64));
+      `(array_at tuchar sh (tuchars (map Int.repr data)) k (Zlength data) d)))
+     (Ssequence
+        (Scall None
+           (Evar _sha256_block_data_order
+              (Tfunction
+                 (Tcons (tptr t_struct_SHA256state_st)
+                    (Tcons (tptr tvoid) Tnil)) tvoid cc_default))
+           [Etempvar _c (tptr t_struct_SHA256state_st),
+           Etempvar _p (tptr tuchar)]) MORE_COMMANDS) POSTCONDITION
+
+*)
  gather_SEP 4%Z 1%Z.
  replace_SEP 0%Z (`(array_at tuchar Tsh (ZnthV tuchar (map Vint (map Int.repr (dd ++ data) ))) 0
         64 (offset_val (Int.repr 40) c))).
  entailer!.
- rewrite negb_true_iff in H2; 
- apply ltu_repr_false in H2; [ | omega..].
+ rewrite negb_true_iff in H5; 
+ apply ltu_repr_false in H5; [ | omega..].
  unfold j,k in *.
  rename c' into c.
  rewrite cVint_force_int_ZnthV
@@ -226,11 +296,8 @@ rewrite <- offset_val_array_at_.
  replace (offset_val (Int.repr (40 + Zlength dd)) c)
           with (offset_val (Int.repr (sizeof tuchar * Zlength dd)) (offset_val (Int.repr 40) c))
   by (change (sizeof tuchar) with 1%Z; rewrite Z.mul_1_l; rewrite offset_offset_val, add_repr; auto).
-  rewrite <- offset_val_array_at.
- replace (Zlength dd + (64 - Zlength dd))%Z with 64%Z by (clear; omega).
- rewrite Z.add_0_r.
- rewrite (split_array_at (Zlength dd) _ _ (ZnthV tuchar (map Vint (map Int.repr (dd ++ data)))))
-    by omega.
+ rewrite split_offset_array_at with (lo := Zlength dd) (contents := (ZnthV tuchar (map Vint (map Int.repr (dd ++ data))))); [| omega | simpl; omega | reflexivity].
+  normalize.
  apply sepcon_derives; apply derives_refl'; apply equal_f; apply array_at_ext; intros.
  unfold ZnthV. repeat rewrite if_false by omega.
  repeat rewrite map_app.
@@ -239,15 +306,18 @@ rewrite <- offset_val_array_at_.
  repeat rewrite map_app.
  unfold ZnthV. repeat rewrite if_false by omega.
  rewrite app_nth2; auto. f_equal.
- rewrite map_length. rewrite Z2Nat.inj_sub by omega.
- f_equal. rewrite Zlength_correct; rewrite Nat2Z.id. rewrite map_length; auto.
+ rewrite !map_length. rewrite Zlength_correct. 
+ rewrite Z2Nat.inj_add by omega.
+ rewrite Nat2Z.id. omega.
+ rewrite map_length; auto.
  repeat rewrite map_length. apply Nat2Z.inj_ge.
-  rewrite Z2Nat.id by omega. rewrite <- Zlength_correct; omega.
+ rewrite Z2Nat.id by omega. rewrite <- Zlength_correct; omega.
+
  forward_call (* sha256_block_data_order (c,p); *)
    (hashed, Zlist_to_intlist (dd++(firstn (Z.to_nat k) data)), c, (offset_val (Int.repr 40) c), Tsh).
  entailer.
  unfold j,k in *|-.
- rewrite negb_true_iff in H6; apply ltu_repr_false in H6; [ | omega..].
+ rewrite negb_true_iff in H8; apply ltu_repr_false in H8; [ | omega..].
  assert (length (dd ++ firstn (Z.to_nat k) data) = 64). {
   unfold k.
   rewrite app_length.
@@ -261,10 +331,10 @@ rewrite <- offset_val_array_at_.
   omega.
 }
  assert (length (Zlist_to_intlist (dd ++ firstn (Z.to_nat k) data)) = LBLOCK). {
-  apply length_Zlist_to_intlist. apply H8.
+  apply length_Zlist_to_intlist. apply H9.
 }
  apply andp_right; [apply prop_right |].
- rewrite Zlength_correct, H9. reflexivity.
+ rewrite Zlength_correct, H10. reflexivity.
  replace (data_block Tsh
       (intlist_to_Zlist (Zlist_to_intlist (dd ++ firstn (Z.to_nat k) data)))
       (offset_val (Int.repr 40) c))
@@ -278,7 +348,7 @@ rewrite <- offset_val_array_at_.
   with 64%Z
  by (rewrite Zlength_correct;
       change 64%Z with (Z.of_nat 64); symmetry; f_equal;
-       rewrite length_intlist_to_Zlist, H9; reflexivity).
+       rewrite length_intlist_to_Zlist, H10; reflexivity).
   apply equal_f; apply array_at_ext; intros.
  unfold tuchars, ZnthV. repeat rewrite if_false by omega.
  rewrite Zlist_to_intlist_to_Zlist.
@@ -310,7 +380,7 @@ rewrite <- offset_val_array_at_.
   rewrite app_length, Nat2Z.inj_add.
  repeat rewrite <- Zlength_correct.
   omega.
- rewrite H8; exists LBLOCK; reflexivity.
+ rewrite H9; exists LBLOCK; reflexivity.
  rewrite Forall_app; split; auto.
  apply Forall_firstn; auto.
  apply isbyte_intlist_to_Zlist.
@@ -325,10 +395,10 @@ entailer!.
  unfold data_block.
  entailer!.
  simpl.
- rewrite <- H10 in H5, H8; clear len'0 H10.
- simpl in H5.
- inversion H5; clear H5; subst len'.
- simpl in H8.
+ rewrite <- H11 in H6, H9; clear len'0 H11.
+ simpl in H6.
+ inversion H6; clear H6; subst len'.
+ simpl in H9.
  rewrite memory_block_array_tuchar by omega.
  replace  (Zlength
      (intlist_to_Zlist (Zlist_to_intlist (dd ++ firstn (Z.to_nat k) data))))
@@ -351,17 +421,17 @@ entailer!.
  unfold k in *.
  apply Nat2Z.inj_le; rewrite Z2Nat.id by omega.
  rewrite <- Zlength_correct.
- fold k in H8;  simpl in H8.
- unfold Int.ltu in H8; if_tac in H8; try inv H8.
- unfold k in H5; repeat rewrite Int.unsigned_repr in H5 by omega.
+ fold k in H9;  simpl in H9.
+ unfold Int.ltu in H9; if_tac in H9; try inv H9.
+ unfold k in H6; repeat rewrite Int.unsigned_repr in H6 by omega.
  omega.
 }
 
  after_call.
  unfold sha_update_inv.
  entailer.
- rewrite negb_true_iff in H6.
- apply ltu_repr_false in H6; [ | omega..].
+ rewrite negb_true_iff in H8.
+ apply ltu_repr_false in H8; [ | omega..].
  clear TC0  TC1.
  apply exp_right with (Zlist_to_intlist (dd ++ firstn (Z.to_nat k) data)).
  assert (LL: length (dd ++ firstn (Z.to_nat k) data) = CBLOCK). {
@@ -388,7 +458,7 @@ assert (length (Zlist_to_intlist (dd ++ firstn (Z.to_nat k) data)) = LBLOCK). {
 }
  entailer!.
  *
-  rewrite H5. 
+  rewrite H6. 
   apply Nat2Z.inj_ge.
   rewrite Nat2Z.inj_sub.
   change (Z.of_nat (LBLOCK*4)) with 64%Z.
@@ -396,10 +466,10 @@ assert (length (Zlist_to_intlist (dd ++ firstn (Z.to_nat k) data)) = LBLOCK). {
   clear - H3. apply Nat2Z.inj_le. rewrite <- Zlength_correct.
   change (Z.of_nat (LBLOCK*4)%nat) with CBLOCKz; clear - H3; omega.
  * 
-  apply Zlength_length in H5; auto.
-  rewrite H5. exists 1%Z; reflexivity.
+  apply Zlength_length in H6; auto.
+  rewrite H6. exists 1%Z; reflexivity.
  *
-  rewrite H5. rewrite Zlist_to_intlist_to_Zlist.
+  rewrite H6. rewrite Zlist_to_intlist_to_Zlist.
   f_equal. f_equal. clear - H0.
   rewrite Z2Nat.inj_sub by omega.
   rewrite Zlength_correct, Nat2Z.id.
@@ -408,11 +478,11 @@ assert (length (Zlist_to_intlist (dd ++ firstn (Z.to_nat k) data)) = LBLOCK). {
   rewrite Forall_app; split; auto.
   apply Forall_firstn; auto.
  *
-  rewrite H5. f_equal.
+  rewrite H6. f_equal.
   f_equal.
   auto.
  *
-  rewrite H5. do 2 f_equal.
+  rewrite H6. do 2 f_equal.
   rewrite Nat2Z.inj_sub. f_equal; auto.
   apply Nat2Z.inj_le.
   rewrite <- KK. omega.
