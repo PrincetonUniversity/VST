8cacadf0ab8c593aa18e652d356045e520347129
Lennart Beringer <eberinge@princeton.edu>
2014-09-10 00:23:41 +0000

    first proof of loop body

--- sha/HMAC_LoopBodyGT.v
+++ sha/HMAC_LoopBodyGT.v
@@ -12,9 +12,47 @@ Require Import sha.hmac_sha256.
 
 Require Import HMAC_definitions.
 
-Lemma eval_var_rel_expr: forall _x t rho CONT b i n, 0 <= i < 64 -> eval_var _x t rho = Vptr b n -> 
-      TT * array_at tuchar Tsh CONT i 64 (Vptr b n) |-- rel_expr (Evar _x t) (Vptr b n) rho.
-Proof. intros. admit. Qed.
+Lemma In_Nlist {A:Type}: forall (n:A) l x, In x (Nlist n l) -> x=n.
+Proof. intros n l.
+  induction l; simpl; intros. contradiction.
+  destruct H; eauto.
+Qed.
+Lemma zeropad_isbyteZ: forall l, Forall isbyteZ l -> Forall isbyteZ (HMAC_FUN.zeroPad l).
+Proof. unfold isbyteZ, HMAC_FUN.zeroPad; intros.
+  rewrite Forall_forall in *. intros.
+  apply in_app_or in H0.
+  destruct H0. apply (H _ H0).
+  apply In_Nlist in H0. subst; omega.
+Qed.
+Lemma mapnth': forall {A B : Type} (f : A -> B) (l : list A) (d : A) (n : nat) fd,
+      fd = f d -> nth n (map f l) fd = f (nth n l d).
+Proof. intros; subst. apply map_nth. Qed.
+Lemma map_unsigned_Brepr_isbyte: forall l, Forall isbyteZ l ->
+      map Byte.unsigned (map Byte.repr l) = l.
+Proof. intros. induction l; simpl in *. trivial.
+   rewrite IHl. rewrite Byte.unsigned_repr. trivial. 
+   rewrite Forall_forall in H. 
+   assert (In a (a::l)). left. trivial. 
+   specialize (H _ H0); clear H0.
+      unfold isbyteZ in H. unfold Byte.max_unsigned, Byte.modulus. simpl. omega.
+   rewrite Forall_forall in *. intros. apply H. right; trivial.
+Qed.
+Lemma Ztest_Bytetest:
+ forall a, Z.testbit (Byte.unsigned a) = Byte.testbit a.
+Proof. reflexivity. Qed.
+Hint Rewrite Ztest_Bytetest : testbit.
+
+Lemma nthD_1 {A B}: forall (f: A ->B) n l d fx dd, (n < length l)%nat ->
+      nth n (map f l) d = fx -> 
+      exists x, In x l /\ nth n l dd = x /\ f x = fx.
+Proof. intros f n.
+  induction n; simpl; intros.
+    destruct l; simpl in *. omega.
+      exists a; split. left; trivial. split; trivial.
+    destruct l; simpl in *. omega. 
+    destruct (IHn l d fx dd) as [? [? [? ?]]]. omega. trivial.
+    exists x; eauto.
+Qed.
 
 (*Same proof as semax_loadstore_array*)
 Lemma NEWsemax_loadstore_array:
@@ -265,161 +303,187 @@ Proof. intros.
                         (map Byte.unsigned
                            (HMAC_FUN.mkArg
                               (map Byte.repr (HMAC_FUN.mkKey (key a))) Ipad)))))) as IPAD.
-     simple eapply semax_seq'.
+destruct (nth_mapVintZ i (map Byte.unsigned (map Byte.repr (HMAC_FUN.mkKey (key a))))) as [n Hn].
+    rewrite Zlength_correct, map_length. rewrite map_length, HMAC_FUN.mkKey_length. simpl. assumption.
+simple eapply semax_seq'.
 
 eapply semax_pre0; [ apply now_later | ].
-
-eapply semax_post_flipped'.
+{ eapply semax_post_flipped'.
    eapply NEWsemax_loadstore_array. 
-     reflexivity. trivial. reflexivity. reflexivity.  reflexivity.
-     entailer; repeat instantiate_Vptr.
- destruct (k_ipad VALS) eqn:?Hipad; try contradiction.
- rewrite <- Hipad in *.
-*  
- repeat apply andp_right; rel_expr. 
-  intro; simpl. rewrite <- H1. rewrite Hipad. reflexivity.
-  simpl typeof.
+     reflexivity. trivial. reflexivity. reflexivity. reflexivity.
+     { entailer; repeat instantiate_Vptr.
+       destruct (k_ipad VALS) eqn:?Hipad; try contradiction.
+       rewrite <- Hipad in *.
+       repeat apply andp_right; rel_expr.  
+       intro; simpl. rewrite <- H1. rewrite Hipad. reflexivity.
+       simpl typeof. simpl.
+         instantiate (2:=Tsh). repeat rewrite sepcon_assoc. rewrite sepcon_comm.
+         erewrite (split3_array_at' i tuchar Tsh _ i _ (k_ipad VALS)). 
+         rewrite array_at_emp. normalize. rewrite (sepcon_comm TT). repeat rewrite sepcon_assoc. 
+         apply sepcon_derives. rewrite Hipad. unfold add_ptr_int. simpl. rewrite mul_repr, Z.mul_1_l. cancel.
+         entailer.
+         reflexivity. 
+         reflexivity.
+         reflexivity.
+         reflexivity.
+         omega.
+         discriminate.
+         intros; simpl. 
+           unfold ZnthV, cVint. simpl. if_tac. omega. 
+           rewrite Hn. simpl. reflexivity.
+         reflexivity.
+     }
+     instantiate (5:=1%nat). reflexivity. 
+     trivial. 
+     trivial. 
+     split; omega. 
 
-works up to here.
+   eapply derives_refl.
+}
 
-     Focus 2. instantiate (1:=k_ipad VALS). instantiate (1:=64). instantiate (1:=i).
-              instantiate (1:=KKEY). instantiate (1:=Tsh). instantiate (1:=1%nat). simpl. reflexivity.
-     2: apply writable_share_top.
-     Focus 3. instantiate (1:=i). unfold in_range. omega.
-  match goal with |- ?PQR  |-- _ =>
-       apply derives_trans with
-       (PQR
-   && (TT * `(array_at tuchar Tsh KKEY i 64 (k_ipad VALS)&& !!isptr (k_ipad VALS))))
-   end.
-(*     && (TT * `(array_at tuchar Tsh (Vfloat oo fz) 0 n z))))
-  end.*)
-  apply andp_right; auto. 
-    rewrite (array_at_isptr tuchar _ _ _ 64 (k_ipad VALS)). entailer. cancel.
-  normalize. rename H1 into isPtr_Ipad.
-  remember (k_ipad VALS) as v. destruct v; try contradiction.
-  clear Heqv isPtr_Ipad.
-  apply andp_right.
-  go_lowerx. normalize.
-  apply andp_left2.
-  (*subst. repeat instantiate_Vptr.*)
-  apply andp_right.
-    apply eval_var_rel_expr. trivial. rewrite <- H10; trivial.
-    rel_expr.
-  go_lowerx. normalize.
-  apply andp_left2. 
-    entailer. rel_expr. apply rel_expr'_cast.
-    rel_expr.
- unfold eval_var in H.
-  remember (Map.get (ve_of rho) _x) as d; destruct d; inv H. destruct p.
-  remember (eqb_type t t0) as bb.
-  destruct bb; inv H1; apply eq_sym in Heqbb.
-    apply eqb_type_spec in Heqbb. subst t. apply eq_sym in Heqd. go_lower.  rel_expr. instantiate (1:=b). unfold seplog.prop. rewrite Heqd. normalize. simpl. entailer.
-         simpl. instantiate (1:=Tsh).
-   
- admit.
-  rel_expr.
-  rel_expr.
-    rewrite <- H10. remember ( (cVint
+  normalize.
+(*destruct (nth_mapVintZ i (map Byte.unsigned (map Byte.repr (HMAC_FUN.mkKey (key a))))) as [n Hn].
+    rewrite Zlength_correct, map_length. rewrite map_length, HMAC_FUN.mkKey_length. simpl. assumption.*)
+(*simple eapply semax_seq'.*)
+
+   assert (Keyisbyte: Forall isbyteZ (HMAC_FUN.mkKey (key a))).
+     unfold HMAC_FUN.mkKey.
+     destruct (Zlength (key a) >? Z.of_nat SHA256_BlockSize);
+       apply zeropad_isbyteZ; trivial.
+eapply semax_pre0; [ apply now_later | ].
+{ eapply semax_post_flipped'.
+   eapply NEWsemax_loadstore_array. 
+     reflexivity. trivial. reflexivity. reflexivity. reflexivity.
+     { entailer; repeat instantiate_Vptr.
+       destruct (k_opad VALS) eqn:?Hopad; try contradiction.
+       rewrite <- Hopad in *.
+       repeat apply andp_right; rel_expr.  
+       intro; simpl. rewrite <- H1. rewrite Hopad. reflexivity.
+       simpl typeof. simpl.
+         instantiate (2:=Tsh). repeat rewrite sepcon_assoc. rewrite sepcon_comm.
+         erewrite (split3_array_at' i tuchar Tsh _ i _ (k_opad VALS)); try reflexivity. 
+         rewrite array_at_emp. normalize.
+         rewrite <- (sepcon_comm (array_at tuchar Tsh
+  (cVint
      (force_int
       oo ZnthV tuchar
            (map Vint
               (map Int.repr
-                 (map Byte.unsigned (map Byte.repr (HMAC_FUN.mkKey (key a))))))))) as CONTENT.
-assert (tarray tuchar 65 = tptr tuchar). admit. rewrite H6.
-clear - H10. Print rel_expr'.
-SearchAbout eval_var.
-assert (). 
- rel_expr. 
-       unfold eval_var in H10. remember (Map.get (ve_of rho) _k_ipad) as d. destruct d.
-         destruct p. remember (eqb_type (tarray tuchar 65) t) as bb.
-         destruct bb; inv H10; apply eq_sym in Heqbb.
-         apply eqb_type_spec in Heqbb. subst t. apply eq_sym in Heqd.  rel_expr. instantiate (1:=b0). entailer.
-         simpl. instantiate (1:=Tsh).
-         rewrite (split3_array_at' Z0 tuchar Tsh _ i _ (Vptr b0 Int.zero)(Vptr b0 Int.zero)).
-SearchAbout mapsto.
-         unfold add_ptr_int; simpl. rewrite Int.add_zero. Focus 2. admit.   rel_expr.
-SearchAbout mapsto array_at.
-Print rel_expr'. unfold Evar.
-  rel_expr.
-  rel_expr.
-  simple eapply rel_expr_cast; [ | try (simpl; rewrite_eval_id; reflexivity) ].
-  simple eapply rel_expr_binop; [ |  | ].
-  apply andp_left1. rel_expr.
-  apply andp_left2. rel_expr.
-  reflexivity.
-  reflexivity.
+                 (map Byte.unsigned (map Byte.repr (HMAC_FUN.mkKey (key a))))))))
+  (Z.succ i) 64 (k_opad VALS))).
+         repeat rewrite <- sepcon_assoc. apply sepcon_derives. entailer.
+         rewrite Hopad. unfold add_ptr_int. simpl. rewrite mul_repr, Z.mul_1_l. cancel.
+         omega.
+         discriminate.
+         intros; simpl. 
+           unfold ZnthV, cVint. simpl. if_tac. omega. 
+           rewrite Hn. simpl. reflexivity.
+         reflexivity.
+     }
+     instantiate (5:=0%nat). reflexivity. 
+     trivial. 
+     trivial. 
+     split; omega. 
 
-rewrite (array_at_isptr tuchar _ _ _ 64 (k_ipad VALS)).
-rewrite (array_at_isptr tuchar _ _ _ 64 (k_opad VALS)).
-  go_lowerx. normalize.
-  repeat instantiate_Vptr; repeat apply andp_right.
-      destruct (k_ipad VALS); try contradiction. rewrite H10.
-       unfold eval_var in H10. remember (Map.get (ve_of rho) _k_ipad) as d. destruct d.
-         destruct p. remember (eqb_type (tarray tuchar 65) t) as bb.
-         destruct bb; inv H10; apply eq_sym in Heqbb.
-         apply eqb_type_spec in Heqbb. subst t. apply eq_sym in Heqd.  rel_expr. instantiate (1:=b0). entailer.
-         simpl. instantiate (1:=Tsh). entailer.
-         rewrite (split3_array_at' i tuchar Tsh _ i _ (eval_var _k_ipad (tarray tuchar 65) rho) (eval_var _k_ipad (tarray tuchar 65) rho)).
-         unfold add_ptr_int; simpl. Focus 2. admit. 
-  destruct (k_opad VALS); try contradiction.
-  repeat instantiate_Vptr; repeat apply andp_right.
-     instantiate (1:=k_ipad VALS). rewrite H10. clear - H10. unfold eval_var in H10. rel_expr. repeat instantiate_Vptr; repeat apply andp_right.
-  rel_expr. rel_expr. admit.array_at_local_facts
-                        instantiate (1:=i). rel_expr. admit.
-     Focus 5. simpl. reflexivity. Locate semax_loadstore_array'. 2: reflexivity. 2: trivial. 2: reflexivity.
-     Focus 4. apply andp_right.  
-                apply andp_right.
+   entailer. rewrite (split_array_at (i+1) tuchar Tsh _ i 64); try omega.
+   rewrite (split_array_at (i+1) tuchar Tsh _ i 64); try omega.
+   rewrite (split_array_at i tuchar Tsh _ 0 (i+1)); try omega.
+   rewrite (split_array_at i tuchar Tsh _ 0 (i+1)); try omega.
+   cancel. 
+   assert (ARITH1: forall PAD BPAD 
+     (HPAD: PAD = Byte.intval BPAD) k, i <= k < i+1 ->
+     (upd
+     (cVint
+        (force_int
+         oo ZnthV tuchar
+              (map Vint
+                 (map Int.repr
+                    (map Byte.unsigned
+                       (map Byte.repr (HMAC_FUN.mkKey (key a)))))))) i
+     (Vint (Int.zero_ext 8 (Int.xor n (Int.repr PAD))))) k
+     = (cVint
+         (force_int
+          oo ZnthV tuchar
+               (map Vint
+                  (map Int.repr
+                     (map Byte.unsigned
+                        (HMAC_FUN.mkArg
+                           (map Byte.repr (HMAC_FUN.mkKey (key a))) BPAD)))))) k).
+   { intros. unfold cVint, ZnthV, upd. if_tac. subst; simpl. 2: omega.
+     if_tac. omega. simpl. f_equal.
 
-SearchAbout rel_lvalue. rel_expr.
-          instantiate (1:= i). instantiate (1:= k_ipad VALS). rel_expr.
-eauto.
-                  apply rel_expr_array_load. reflexivity 
-     hoist_later_in_pre.
-     simple eapply semax_seq'.
-   eapply semax_loadstore_array.
-   simple eapply (semax_loadstore_array sx);
-      remember (Ederef
-        (Ebinop Oadd (Evar _k_ipad (tarray tuchar 65)) (Etempvar _i tint)
-           (tptr tuchar)) tuchar) as e1.
-        eapply semax_loadstore_array'.
-assert (typeof e1 = tuchar). subst e1. simpl. reflexivity.
-      remember (Ederef
-        (Ebinop Oadd (Evar _k_ipad (tptr tuchar)) (Etempvar _i tint)
-           (tptr tuchar)) tuchar) as e1'.
-assert (typeof e1' = tuchar). subst e1'. simpl. reflexivity.
-          reflexivity. apply I. reflexivity.
-        Focus 8. entailer. 
-          repeat apply andp_right. 
-         (*rel_expr (Evar _k_ipad (tarray tuchar 65)) ?7539 rho*)
-          Focus 2. eapply rel_expr_cast.
-                   eapply rel_expr_binop.
-          instantiate (1:= k_ipad VALS). rel_expr. unfold eval_var in H7.
-          Focus 2. rel_expr.
-          Focus 2. eapply rel_expr_cast.
-                   eapply rel_expr_binop. 
-                   (* rel_expr
-(*        eapply semax_loadstore_array.?*)
-          reflexivity. apply I. reflexivity. admit. (*TODO tarray/tptr*)
-          reflexivity. 
-        entailer. 
-          repeat apply andp_right. 
-         (*rel_expr (Evar _k_ipad (tarray tuchar 65)) ?7539 rho*)
-          instantiate (1:= k_ipad VALS). rel_expr. unfold eval_var in H7.
-          Focus 2. rel_expr.
-          Focus 2. eapply rel_expr_cast.
-                   eapply rel_expr_binop. 
-                   (* rel_expr
-      (Ederef
-         (Ebinop *)
-                  Focus 2. rel_expr. 
-                  Focus 2. simpl. eapply rel_expr_deref.
-                  rel_expr. auto.
-                apply andp_right. 
-SearchAbout rel_lvalue. rel_expr. 
-           eapply rel_expr_array_load'.
-       reflexivity. apply I. reflexivity.
-     simple eapply semax_loadstore_array.
-       reflexivity. apply I. reflexivity.
-       unfold tarray; simpl. admit. (*TODO: refine Lemma semax_loadstore_array' in array_lemmas.v*)
-       reflexivity.
-     *)*)
+    rewrite map_unsigned_Brepr_isbyte in Hn; trivial.
+    rewrite (nth_indep _ _ (Vint Int.zero)) in Hn.
+     erewrite mapnth' in Hn; try reflexivity.
+     inversion Hn; clear Hn.
+    rewrite H18.
+    eapply nthD_1 in H18. instantiate (1:=Z0) in H18.
+    destruct H18 as [z [KeyZ [NZ NN]]].
+    rewrite Forall_forall in Keyisbyte. apply Keyisbyte in KeyZ.
+     unfold HMAC_FUN.mkArg.
+     rewrite (nth_indep _ _ (Vint Int.zero)).
+     erewrite mapnth'. unfold force_int. 2: reflexivity.
+     erewrite mapnth'. simpl.
+     Focus 2. instantiate (1:=Z0). reflexivity.
+     erewrite mapnth'.
+     Focus 2. instantiate (1:=Byte.zero). reflexivity.
+     erewrite mapnth'. simpl.
+     Focus 2. instantiate (1:=(Byte.zero, Byte.zero)). reflexivity.  
+     rewrite combine_nth.
+     unfold sixtyfour. rewrite nth_Nlist. simpl. unfold Opad.
+     erewrite mapnth'.
+     Focus 2. instantiate (1:=0). reflexivity.
+     rewrite NZ. clear NZ. subst n.
+     apply Int.same_bits_eq. clear - KeyZ. intros.
+        unfold Int.zero_ext. (*, Int.Zzero_ext. simpl.*)
+     rewrite Int.testbit_repr; trivial.
+     rewrite Int.testbit_repr; trivial.
+     rewrite Int.Zzero_ext_spec; try omega.
+
+     rewrite sha_lemmas.Ztest_Inttest, Int.bits_xor; trivial.
+     rewrite Ztest_Bytetest.
+     if_tac.
+        assert (iB: 0 <= i < Byte.zwordsize).
+          unfold Byte.zwordsize; simpl; omega.
+        rewrite Byte.bits_xor; trivial.
+        repeat rewrite Int.testbit_repr; trivial. 
+        repeat rewrite Byte.testbit_repr; trivial.
+     rewrite Byte.bits_above. trivial. apply H0.
+
+    destruct H as [_ H]. apply Z2Nat.inj_lt in H; try omega. apply H.
+    rewrite map_length. unfold sixtyfour. rewrite HMAC_FUN.mkKey_length, length_Nlist. reflexivity.
+    repeat rewrite map_length. rewrite combine_length. rewrite map_length.
+      unfold sixtyfour. rewrite HMAC_FUN.mkKey_length, length_Nlist. simpl.
+      destruct H as [_ H]. apply Z2Nat.inj_lt in H; try omega. apply H.
+    rewrite HMAC_FUN.mkKey_length. simpl.
+      destruct H as [_ H]. apply Z2Nat.inj_lt in H; try omega. apply H.
+    repeat rewrite map_length. rewrite HMAC_FUN.mkKey_length. simpl.
+      destruct H as [_ H]. apply Z2Nat.inj_lt in H; try omega. apply H.
+  }
+
+  assert (Arth92 := ARITH1 54 Ipad (eq_refl _)).
+  assert (Arth54 := ARITH1 92 Opad (eq_refl _)). clear ARITH1.
+  erewrite (array_at_ext tuchar Tsh _ _ i (i+1) Arth92).
+  erewrite (array_at_ext tuchar Tsh _ _ i (i+1) Arth54).
+  clear Arth54 Arth92; cancel.
+
+  assert (ARITH1: forall PAD k, i + 1 <= k < 64 ->
+    upd (cVint
+     (force_int
+      oo ZnthV tuchar
+           (map Vint
+              (map Int.repr
+                 (map Byte.unsigned (map Byte.repr (HMAC_FUN.mkKey (key a))))))))
+     i (Vint (Int.zero_ext 8 (Int.xor n (Int.repr PAD)))) k 
+   = cVint (force_int
+      oo ZnthV tuchar
+        (map Vint
+           (map Int.repr
+              (map Byte.unsigned (map Byte.repr (HMAC_FUN.mkKey (key a)))))))
+    k).
+   { clear. intros. unfold upd. if_tac. subst. omega. trivial. }
+
+  rewrite (array_at_ext tuchar Tsh _ _ (i+1) 64 (ARITH1 92)).
+  rewrite (array_at_ext tuchar Tsh _ _ (i+1) 64 (ARITH1 54)). cancel.
+ }
+Qed.
