cc49e9bd71bb18dc80cb37d625497a0c24d06ccc
Joey Dodds <dist0rtedwave@gmail.com>
2014-12-10 12:51:24 -0500

    proved some lemmas about typeof

--- mc_reify/get_set_reif_soundness.v
+++ mc_reify/get_set_reif_soundness.v
@@ -105,7 +105,7 @@ assert (e = eq_refl) by apply msl.Axioms.proof_irr; subst;
 try solve [eexists; reflexivity].
 Qed. *)
 
-Lemma exprD'_App  e1 e2 tus tvs ty1 ty2 v:
+Lemma exprD'_App_R_typ  e1 e2 tus tvs ty1 ty2 v:
 exprD' tus tvs ty2 (App e1 e2) = Some v ->
 typeof_expr tus tvs e2 = Some ty1 ->
 (exists v1 , exprD' tus tvs (tyArr ty1 ty2) e1 = Some v1) /\ 
@@ -137,21 +137,144 @@ destruct (exprD' tus tvs ty1 e2) eqn:?; try congruence.
 eexists; eauto.
 Qed.
 
+Lemma exprD'_App_L_typ  e1 e2 tus tvs ty1 ty2 v:
+exprD' tus tvs ty2 (App e1 e2) = Some v ->
+typeof_expr tus tvs e1 = Some (tyArr ty1 ty2) ->
+(exists v1 , exprD' tus tvs (tyArr ty1 ty2) e1 = Some v1) /\ 
+(exists v2, exprD' tus tvs ty1 e2 = Some v2).
+Proof.
+intros.
+assert (X := @exprD'_typeof_expr typ _ _ _ _ _ _ _ tus (App e1 e2) tvs
+ty2 v). assert (H2 : typeof_expr tus tvs (App e1 e2) = Some ty2).
+apply X. intuition. clear X.
+change (App e1 e2) with (AppN.apps e1 (e2 :: nil)) in *.
+rewrite AppN.typeof_expr_apps in H2; auto with typeclass_instances.
+unfold AppN.typeof_apps in H2. 
+destruct (typeof_expr tus tvs e1) eqn:?; try congruence.
+simpl in H2. destruct (typeof_expr tus tvs e2) eqn:?; try congruence.
+simpl. destruct t; simpl in H2; try congruence.
+destruct (typ_eq_dec t0 t1) eqn : ?; try congruence. 
+inversion H2; subst; clear H2. 
+inversion H0; subst; clear H0.
+simpl in H.
+change (App e1 e2) with (AppN.apps e1 (e2 :: nil)) in *.
+clear Heqs. 
+rewrite AppN.exprD'_apps in H; auto with typeclass_instances.
+unfold AppN.apps_sem' in H. 
+destruct (typeof_expr tus tvs e1) eqn:?; try congruence.
+destruct (exprD' tus tvs t e1) eqn :?; try congruence.
+inversion Heqo; subst; clear Heqo. split. eexists. apply Heqo2.
+simpl in H. 
+destruct (exprD' tus tvs ty1 e2) eqn:?; try congruence.
+eexists; eauto.
+Qed.
+
+Ltac destruct_match H :=
+match type of H with
+| context [ match ?x with _ => _  end] => destruct x eqn:?
+end.
+
+
+Lemma typeof_app : forall e1 e2 t tvs tus,
+typeof_expr tus tvs (App e1 e2) = Some t ->
+exists t2, typeof_expr tus tvs e1 = Some (tyArr t2 t) /\
+typeof_expr tus tvs e2 = Some (t2). 
+Proof.
+intros. change (App e1 e2) with (AppN.apps e1 (e2 :: nil)) in *.
+rewrite AppN.typeof_expr_apps in H; auto with typeclass_instances.
+unfold AppN.typeof_apps in H. 
+destruct (typeof_expr tus tvs e1) eqn:?; try congruence.
+destruct t0; try (simpl in H; destruct_match H;  congruence).
+simpl in H.
+repeat (destruct_match H; try congruence).
+destruct_match Heqo1; try congruence. subst. inversion H.
+subst. 
+eexists. split. reflexivity. auto.
+Qed.
+
+Lemma exprD_ex_typs : forall tus tvs t e1 e2 v,
+exprD' tus tvs t (App e1 e2) = Some v ->
+exists t2, typeof_expr tus tvs e1 = Some (tyArr t2 t) /\
+typeof_expr tus tvs e2 = Some (t2). 
+intros.
+assert (X := @exprD'_typeof_expr typ _ _ _ _ _ _ _ tus (App e1 e2) tvs
+t v). 
+assert (H2 : typeof_expr tus tvs (App e1 e2) = Some t).
+apply X. intuition. clear X.
+apply typeof_app; eauto.
+Qed.
+
+intros.
+destruct (exprD_ex_L_typ _ _ _ _ _ _ H).
+destruct (exprD_ex_R_typ _ _ _ _ _ _ H).
+eexists; split; eauto.
+
+
+exists 
+Proof.
+intros.
+assert (X := @exprD'_typeof_expr typ _ _ _ _ _ _ _ tus (App e1 e2) tvs
+ty2 v). 
+assert (H2 : typeof_expr tus tvs (App e1 e2) = Some ty2).
+apply X. intuition. clear X.
+apply typeof_app in H2. destruct H2. destruct H0. eexists; eauto.
+Qed.
+
 
 Lemma set_reif_eq2 :
-forall typ i vr tr,
-reflect nil nil (App (App (Inj (inr (Data (fset typ i)))) vr) tr) (typtree typ) =
-reflect nil nil (set_reif i vr tr typ) (typtree typ).
+forall tus tvs typ i vr tr,
+exprD' tus tvs (typtree typ) (App (App (Inj (inr (Data (fset typ i)))) vr) tr)  =
+exprD' tus tvs (typtree typ) (set_reif i vr tr typ).
 Proof.
 intros. 
-unfold reflect. unfold exprD. cbv [split_env]. simpl.
-induction i. simpl.
-match goal with 
-[ |-  match ?d with _ => _ end = match ?x with _ => _ end ] => 
-destruct d eqn:?, x eqn:?
-end; auto.
-f_equal.
+induction i;
+simpl;
 destruct (as_tree tr) eqn:?; destruct_as_tree; auto.
+apply as_tree_l in Heqo. subst.
+unfold node in *.
+unfold func in *.  
+destruct (exprD' tus tvs (typtree typ)
+     (App (App (Inj (inr (Data (fset typ i~1)))) vr)
+        (App (App (App (Inj (inr (Data (fnode t)))) e1) e0) e))) eqn:Eql,
+(exprD' tus tvs (typtree typ)
+     (App (App (App (Inj (inr (Data (fnode t)))) e1) e0)
+        (set_reif i vr e typ))) eqn :Eqr; auto.
+destruct (exprD_ex_L_typ _ _ _ _ _ _ Eql).
+
+destruct (exprD'_ex_L_typ in Eql.
+
+ unfold exprD' at 2.  simpl.
+rewrite exprD'_App_L_rw at 1.
+erewrite exprD'_App_L_rw.
+erewrite exprD'_App_L_rw.
+ Focus 2. simpl.
+
+destruct (exprD_ex_R_typ _ _ _ _ _ _ Heqo).
+eapply exprD'_App_R_typ in Heqo; eauto.
+destruct Heqo. destruct H. destruct H0.
+eapply exprD'_App_R_typ in H.
+destruct H. destruct H. destruct H1. 
+
+simpl. destruct t; simpl in H2; try congruence.
+destruct (typ_eq_dec t0 t1) eqn : ?; try congruence. 
+inversion H2; subst; clear H2. 
+inversion H0; subst; clear H0.
+simpl in H.
+change (App e1 e2) with (AppN.apps e1 (e2 :: nil)) in *.
+clear Heqs. 
+rewrite AppN.exprD'_apps in H; auto with typeclass_instances.
+unfold AppN.apps_sem' in H. 
+destruct (typeof_expr tus tvs e1) eqn:?; try congruence.
+destruct (exprD' tus tvs t e1) eqn :?; try congruence.
+inversion Heqo; subst; clear Heqo. split. eexists. apply Heqo2.
+simpl in H. 
+destruct (exprD' tus tvs ty1 e2) eqn:?; try congruence.
+eexists; eauto.
+
+
+Focus 2. simpl. 
+
+
 erewrite exprD'_App_L_rw in Heqo. Focus 3.
 erewrite exprD'_App_L_rw; eauto. simpl. unfold typeof_func_opt. reflexivity.
 
