09d22927245a440bfe7613711af9a4251ce1fcb5
Qinxiang <caoqinxiang@gmail.com>
2015-02-12 16:08:18 -0500

    Reified nth_error and replace_nth is proved sound.

--- mc_reify/set_reif.v
+++ mc_reify/set_reif.v
@@ -171,28 +171,6 @@ match msubst_efield_denote_reif T1 T2 efs with
 | None => none_reif (tylist tygfield)
 end.
 
-Fixpoint rnth_error (ty: typ) (xs: expr typ func) (n: nat) : expr typ func :=
-  match xs with
-  | Inj (inr (Data (fnil _))) => none_reif ty
-  | App (App (Inj (inr (Data (fcons _)))) hd) tl => 
-    match n with
-    | O => some_reif hd ty
-    | S n0 => rnth_error ty tl n0
-    end
-  | _ => none_reif ty
-  end.
-
-Fixpoint rreplace_nth (ty: typ) (n: nat) (xs: expr typ func) (x: expr typ func) : expr typ func :=
-  match xs with
-  | Inj (inr (Data (fnil _))) => xs
-  | App (App (Inj (inr (Data (fcons _)))) hd) tl => 
-    match n with
-    | O => App (App (Inj (inr (Data (fcons ty)))) x) tl
-    | S n0 => App (App (Inj (inr (Data (fcons ty)))) hd) (rreplace_nth ty n0 tl x)
-    end
-  | _ => xs
-  end.
-
 Lemma Forall_reverse :
 forall A P (l: list A),
 Forall P l <->
