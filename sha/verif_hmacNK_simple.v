7006e08f2fcbbebe599d01ba8db4b75aa01dec89
Lennart Beringer <eberinge@princeton.edu>
2015-11-21 21:03:17 -0500

    The following 5 files verif_hmacNK_X.v now use the freezer: update, final, simple, intit_part1, init_part2, init

--- sha/verif_hmacNK_simple.v
+++ sha/verif_hmacNK_simple.v
@@ -32,8 +32,8 @@ rename keyVal into k. rename msgVal into d.
 destruct KEY as [kl key].
 destruct MSG as [dl data]. simpl in *.
 rename H into KL. rename H0 into DL. 
-assert_PROP (isptr md) as isPtrMD by entailer!.
-forward_if  (
+Time assert_PROP (isptr md) as isPtrMD by entailer!. (*1.3*)
+Time forward_if  (
   PROP  (isptr c)
    LOCAL  (lvar _c t_struct_hmac_ctx_st c; temp _md md; temp _key k;
    temp _key_len (Vint (Int.repr kl)); temp _d d;
@@ -41,46 +41,53 @@ forward_if  (
    SEP  (data_at_ Tsh t_struct_hmac_ctx_st c; data_block Tsh key k;
    data_block Tsh data d; K_vector kv;
    memory_block shmd 32 md)).
-  { apply denote_tc_comparable_split. 
+  (*3.3*)
+  { clear H. apply denote_tc_comparable_split. 
     apply sepcon_valid_pointer2. apply memory_block_valid_ptr. auto. omega.
     apply valid_pointer_zero. }
   { (*Branch1*) exfalso. subst md. contradiction.  }
-  { (* Branch2 *) forward. entailer. } 
-normalize.
-assert_PROP (isptr k). { unfold data_block. normalize. rewrite data_at_isptr with (p:=k). entailer!. (*Issue: need to do unfold data_block. normalize. rewrite data_at_isptr with (p:=k). here is NEW*) }
-rename H into isPtrK. 
-forward_call (c, k, kl, key, kv, HMACabs nil nil nil). 
+  { (* Branch2 *) Time forward. (*0.3*) Time entailer!. (*1.9*)}
+Time normalize. (*0.8*)
+freeze [2;4] FR1.
+assert_PROP (isptr k) as isPtrK.
+{ unfold data_block. Time normalize. (*1.6 versus 2.2*) rewrite data_at_isptr with (p:=k). Time entailer!. (*1.6 versus 2.5*) } 
+
+Time forward_call (c, k, kl, key, kv, HMACabs nil nil nil). (*3*)
  { apply isptrD in isPtrK. destruct isPtrK as [kb [kofs HK]]. rewrite HK.
-   unfold initPre. Time entailer!. (*1.1*)
+   unfold initPre. Time entailer!. (*0.6 versus 1.1*)
  }
-assert_PROP (s256a_len (absCtxt (hmacInit key)) = 512).
-  { unfold hmacstate_. Intros r. apply prop_right. apply H. } 
-rename H into H0_len512.
-forward_call (hmacInit key, c, d, dl, data, kv).
-  { rewrite H0_len512. assumption. } 
+freeze [1;2;3] FR2. 
+assert_PROP (s256a_len (absCtxt (hmacInit key)) = 512 /\ 
+             field_compatible (Tstruct _hmac_ctx_st noattr) [] c).
+  { unfold hmacstate_. Intros r. entailer!. apply H. }
+destruct H as [H0_len512 FC_c].
+thaw FR2. 
+thaw FR1. 
+freeze [0;3] FR3.
+Time forward_call (hmacInit key, c, d, dl, data, kv). (*2.8*)
+  { rewrite H0_len512; assumption. } 
 
-assert_PROP (field_compatible (Tstruct _hmac_ctx_st noattr) [] c).
-{ unfold hmacstate_; entailer. }
-rename H into FC_c.
-remember  (hmacInit key) as h0.
-forward_call ((hmacUpdate data h0), c, md, shmd, kv).
-forward_call (fst (hmacFinal (hmacUpdate data h0)), c). 
-(*VST Issue: 
-remember (hmacFinal (hmacUpdate data h0)) as h2.  
-forward_call (h2,c). does not work*)
-
-forward.
-
-assert_PROP (field_compatible (tarray tuchar (sizeof cenv_cs t_struct_hmac_ctx_st)) [] c).
-{ unfold data_block at 1. unfold Zlength. simpl. rewrite data_at_data_at'. normalize. }
+thaw FR3. 
+freeze [2;3] FR4.
+Time forward_call (hmacUpdate data (hmacInit key), c, md, shmd, kv). (*2.3*) 
+freeze [0;2;3] FR5.
+forward_call (fst (hmacFinal (hmacUpdate data (hmacInit key))), c). 
+freeze [0;1] FR6.
+Time forward. (*4.2*)
+(*assert_PROP (field_compatible (tarray tuchar (sizeof cenv_cs t_struct_hmac_ctx_st)) [] c).
+{ thaw FR6. unfold data_block at 1. unfold Zlength. simpl. rewrite data_at_data_at'. Time normalize. (*13.4 SLOW*) }*)
 Exists c. entailer!.
+thaw FR6. thaw FR5. Time cancel. (*2.2*)
+thaw FR4. Time cancel. (*2.1*)
 rewrite <- (hmac_sound key data). unfold hmac.
-cancel.  clear H3.
+Time cancel.
 unfold data_block.
   rewrite Zlength_correct; simpl.
 rewrite <- memory_block_data_at_; trivial.
 normalize.
-  rewrite (memory_block_data_at_ Tsh (tarray tuchar (sizeof (@cenv_cs CompSpecs) t_struct_hmac_ctx_st))).
+assert_PROP (field_compatible (tarray tuchar (sizeof cenv_cs t_struct_hmac_ctx_st)) [] c).
+{ rewrite data_at_data_at'. Time normalize. (*8*) }
+rewrite (memory_block_data_at_ Tsh (tarray tuchar (sizeof (@cenv_cs CompSpecs) t_struct_hmac_ctx_st))).
   2: trivial.
   eapply derives_trans. apply data_at_data_at_. apply derives_refl.
-Qed.
+Time Qed. (*15*)
--- sha/verif_hmacNK_update.v
+++ sha/verif_hmacNK_update.v
@@ -35,20 +35,14 @@ assert (FC_md_ctx: field_compatible t_struct_hmac_ctx_st [StructField _md_ctx] c
 assert (FC_i_ctx: field_compatible t_struct_hmac_ctx_st [StructField _i_ctx] c).
  {red in FC_c. red. intuition.  constructor. trivial. right; left. reflexivity. }
 unfold_data_at 1%nat.
-rewrite (field_at_data_at  _ _ [StructField _i_ctx]).
-rewrite (field_at_data_at _ _ [StructField _md_ctx]).  
-rewrite field_address_offset by auto with field_compatible.
+freeze [2;3] FR.
+rewrite (field_at_data_at _ _ [StructField _md_ctx]).   
 rewrite field_address_offset by auto with field_compatible.
 simpl @nested_field_type.
 make_Vptr c.
 simpl. rewrite Int.add_zero.
-Time forward_call (ctx, data, Vptr b i, d, Tsh, len, kv). (*21.9 secs*) 
- (*Issue: the forward_call takes ca 50secs in newComCert, instead of 6secs in Master, 
-   no matter wther the field_at_data_at stuff s done outside of the call or inside.
-   Resolved (11/4/15): with the faster canceler, now 12.5 sec.
- *)
-  { (* rewrite field_at_data_at at 1.*)
-    unfold sha256state_. Exists (fst ST).
+Time forward_call (ctx, data, Vptr b i, d, Tsh, len, kv). (*6 versus 21 *) 
+  { unfold sha256state_. Exists (fst ST).
     rewrite prop_true_andp by auto. 
     change_compspecs CompSpecs.
    change (@data_block spec_sha.CompSpecs Tsh data d)
@@ -58,20 +52,19 @@ Time forward_call (ctx, data, Vptr b i, d, Tsh, len, kv). (*21.9 secs*)
   }
   intuition.
 rewrite sublist_same; trivial.
-Time forward. (*12.4*)
-cancel.
+freeze [0;1;2] FR1.
+Time forward. (*12 versus 12.4*)
+thaw FR1. 
+change (@data_block spec_sha.CompSpecs Tsh data d)
+     with (@data_block CompSpecs Tsh data d). Time cancel. (*0.2*)
 unfold hmacstate_, sha256state_, hmac_relate.
-Intros r. Exists (r,(iCtx ST, oCtx ST)).
-simpl.
-Time normalize. (*2.6*) 
-unfold_data_at 3%nat. 
+Intros r.  Exists (r,(iCtx ST, oCtx ST)).
+Time entailer!. (*2.1*)
+thaw FR. 
+unfold_data_at 2%nat.
 destruct ST as [ST1 [ST2 ST3]]. simpl in *.
-rewrite (field_at_data_at _ _ [StructField _i_ctx]).
+Time cancel. (*0.5*)
 rewrite (field_at_data_at _ _ [StructField _md_ctx]).
 rewrite field_address_offset by auto with field_compatible.
-rewrite field_address_offset by auto with field_compatible.
-simpl. rewrite Int.add_zero.
-   change (@data_block spec_sha.CompSpecs Tsh data data')
-     with (@data_block CompSpecs Tsh data data').
-Time cancel. (*0.5 *)
-Time Qed. (*20.3*)
+simpl. rewrite Int.add_zero. apply derives_refl.
+Time Qed. (*9.5 versus 20.3*)
