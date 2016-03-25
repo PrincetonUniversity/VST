b97e13c7867101424f94e19c9955f29e936ce3bb
Santiago <scuellar@princeton.edu>
2016-03-25 17:09:24 -0400

    merge

---
+++ concurrency/sepcomp.v
@@ -0,0 +1,23 @@
+(*Add LoadPath "../sepcomp" as sepcomp.*)
+
+Module SepComp.
+Require Export semantics.
+Require Export mem_lemmas.
+Require Export structured_injections.
+Require Export effect_semantics.
+Require Export reach. 
+Require Export simulations.
+(*Require Export closed_simulations.*)
+(*Require Export barebones_simulations.*)
+(*Require Export effect_properties.*)
+(*Require Export open_safety.*)
+Require Export extspec.
+Require Export Address.
+(*Require Export relyguarantee_lemmas.*)
+Require Export arguments.
+End SepComp.
+
+Module SepCompExtras.
+Require Export mem_wd.
+(* Require Export nucular_semantics. *)
+End SepCompExtras.
