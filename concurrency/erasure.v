c9d38a2003e3c3a5791f3bfa9ded15a5ad613a0a
Santiago <scuellar@princeton.edu>
2016-03-30 18:16:27 -0400

    Setting up the erasure theorem.

--- concurrency/erasure.v
+++ concurrency/erasure.v
@@ -11,18 +11,53 @@ Require Import compcert_threads. Import Concur.
 (*The simulations*)
 Require Import wholeprog_simulations. 
 
-Module Erasure (SCH: Scheduler NatTID)(SEM: Semantics).
+Module Type Parching (SCH: Scheduler NatTID)(SEM: Semantics).
+  
   Import SEM.
-  Parameter genv: G.
-  Parameter main: Values.val.
     
   Module JSEM:= JuicyMachineSig SEM.
   Module JuicyMachine:= CoarseMachine NatTID SCH JSEM.
   Definition JMachineSem:= JuicyMachine.MachineSemantics.
+  Notation jstate:= JSEM.machine_state.
   
   Module DSEM:= ShareMachineSig SEM.
   Module DryMachine:= CoarseMachine NatTID SCH DSEM.
   Definition DMachineSem:= DryMachine.MachineSemantics.
+  Notation dstate:= DSEM.machine_state.
+
+  Parameter parch_state : jstate ->  dstate.
+  Parameter match_st : jstate ->  dstate -> Prop.
+  Axiom parch_match: forall (js: jstate) (ds: dstate),
+      match_st js ds <-> ds = parch_state js.
+
+  (*Core Diagram*)
+  Axiom parched_diagram: forall genv U U' m m' jst jst',  
+      corestep JMachineSem genv (U, jst) m (U', jst') m' ->
+      corestep DMachineSem genv (U, parch_state jst) m (U', parch_state jst') m'.
+
+  (*Halted diagram*)
+  Axiom parched_halted: forall U js v1,
+          halted JMachineSem (U, js) = Some v1 ->
+          halted DMachineSem (U, parch_state js) = Some v1.
+End Parching.
+  
+  
+Module Erasure (SCH: Scheduler NatTID)(SEM: Semantics)(PC: Parching SCH SEM).
+  Import SEM.
+  Import PC.
+  Parameter genv: G.
+  Parameter main: Values.val.
+    
+  (*Module JSEM:= JuicyMachineSig SEM.
+  Module JuicyMachine:= CoarseMachine NatTID SCH JSEM.
+  Definition JMachineSem:= JuicyMachine.MachineSemantics.*)
+  Notation jmachine_state:= JuicyMachine.MachState.
+  
+  (*Module DSEM:= ShareMachineSig SEM.
+  Module DryMachine:= CoarseMachine NatTID SCH DSEM.
+  Definition DMachineSem:= DryMachine.MachineSemantics.*)
+  Notation dmachine_state:= DryMachine.MachState.
+  
 
   (* The main erasure theorem is stated with Wholeprog_sim
      which might change, but will escentially have the same
@@ -47,9 +82,11 @@ Module Erasure (SCH: Scheduler NatTID)(SEM: Semantics).
       halt_inv j g1 args1 m1 g2 args2 m2.
 
   Definition core_data:= unit.
-  Definition match_state (d: core_data) (j: SM_Injection)
-             (JS: JuicyMachine.MachState)(M1: mem)
-             (DS: DryMachine.MachState)(M2: mem):= True.
+  Inductive match_state :  core_data ->  SM_Injection -> jmachine_state ->  mem -> dmachine_state -> mem -> Prop:=
+  MATCH: forall d j js ds U m, match_st js ds -> match_state d j  (U, js) m (U, ds) m.
+  Definition parch_machine (jms: jmachine_state): dmachine_state:=
+    match jms with (U, jm) => (U, parch_state jm) end.
+  
   Definition core_ord: unit-> unit -> Prop := fun _ _ => False.
   Definition core_ord_wf:  well_founded core_ord.
       constructor; intros y H; inversion H.
@@ -74,29 +111,78 @@ Module Erasure (SCH: Scheduler NatTID)(SEM: Semantics).
          j)).
     
     exists mu, tt.
-    admit.
-    (*eexists. (* Fill in! *)
-    split.
-    unfold mu; apply initial_SM_as_inj.
-    split.
-    unfold initial_core, DMachineSem,  DryMachine.MachineSemantics.
-    unfold DryMachine.init_machine.
-     *)
+    exists (parch_machine JS).
+    split; [|split].
+    - unfold mu; apply initial_SM_as_inj.
+    - unfold initial_core, DMachineSem,  DryMachine.MachineSemantics, DryMachine.init_machine.
+      admit.
+    - destruct JS as [U js]; constructor.
+      apply parch_match; reflexivity.
     Qed.
-    
-    Theorem erasure:
+
+  Lemma core_step :
+    forall (st1 : JuicyMachine.MachState) (m1 : mem)
+     (st1' : JuicyMachine.MachState) (m1' : mem),
+   corestep JMachineSem genv st1 m1 st1' m1' ->
+   forall (cd : core_data) (st2 : DryMachine.MachState)
+     (mu : SM_Injection) (m2 : mem),
+   match_state cd mu st1 m1 st2 m2 ->
+   exists
+     (st2' : DryMachine.MachState) (m2' : mem) (cd' : core_data) 
+   (mu' : SM_Injection),
+     match_state cd' mu' st1' m1' st2' m2' /\
+     (semantics_lemmas.corestep_plus DMachineSem genv st2 m2 st2' m2' \/
+      semantics_lemmas.corestep_star DMachineSem genv st2 m2 st2' m2' /\
+      core_ord cd' cd).
+       intros jmst m1 jmst' m1' STEP cd dmst mu m2 MATCH.
+       exists (parch_machine jmst'), m1', tt, mu.
+       destruct jmst' as [U' jst'].
+       split; [|left].
+       - constructor.
+         apply parch_match; reflexivity.
+       - inversion MATCH; subst.
+         apply semantics_lemmas.corestep_plus_one.
+         apply parch_match in H; subst ds.
+         unfold parch_machine. apply parched_diagram; assumption.
+  Qed.
+
+  Lemma core_halted:
+     forall (cd : core_data) (mu : SM_Injection)
+     (c1 : JuicyMachine.MachState) (m1 : mem) (c2 : DryMachine.MachState)
+     (m2 : mem) (v1 : Values.val),
+   match_state cd mu c1 m1 c2 m2 ->
+   halted JMachineSem c1 = Some v1 ->
+   exists (j : SM_Injection) (v2 : Values.val),
+     halt_inv j genv v1 m1 genv v2 m2 /\ halted DMachineSem c2 = Some v2.
+  Proof.
+    intros cd mu jmst m1 dmst m2 v1 MATCH HALT.
+    exists mu, v1.
+    inversion MATCH; subst.
+    split.
+    - constructor; try reflexivity.
+    - apply parch_match in H; subst ds.
+      apply parched_halted; assumption.
+  Qed.
+  
+  Theorem erasure:
     Wholeprog_sim.Wholeprog_sim
       JMachineSem DMachineSem
       genv genv
       main
       ge_inv init_inv halt_inv.
-      apply (Wholeprog_sim.Build_Wholeprog_sim
-            JMachineSem DMachineSem
-            genv genv
-            main
-            ge_inv init_inv halt_inv
-            core_data match_state core_ord core_ord_wf).
-      - reflexivity.
-      - apply core_initial.                                                         -          
+  Proof.
+    apply (Wholeprog_sim.Build_Wholeprog_sim
+             JMachineSem DMachineSem
+             genv genv
+             main
+             ge_inv init_inv halt_inv
+             core_data match_state core_ord core_ord_wf).
+    - reflexivity.
+    - apply core_initial.
+    - apply core_step.
+    - apply core_halted.
+  Qed.
+    
+End Erasure.
 
   
