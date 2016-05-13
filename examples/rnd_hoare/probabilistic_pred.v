6458bc44301f2d20676335a7a4e7cfd90aa38952
Qinxiang <caoqinxiang@gmail.com>
2016-05-13 03:09:02 -0400

    More progress in modeling Pr and triple (partial/total).

--- examples/rnd_hoare/probabilistic_pred.v
+++ examples/rnd_hoare/probabilistic_pred.v
@@ -1,11 +1,12 @@
 Require Import Coq.Sets.Ensembles.
 Require Import RndHoare.random_oracle.
 Require Import RndHoare.imperative.
+Import RndHoare.imperative.Randomized.
 Require Import RndHoare.measure.
 
 Local Open Scope R.
 
-Record RandomDistribution {ora: RandomOracle}: Type := {
+Class RandomDistribution (ora: RandomOracle): Type := {
   raw_ms: Measure (RandomHistory);
   pr_universe:
     measure_function _ raw_ms (Full_MSet _) = 1;
@@ -36,13 +37,104 @@ Record RandomDistribution {ora: RandomOracle}: Type := {
       measure_function _ raw_ms P1 = measure_function _ raw_ms P2
 }.
 
-(* TODO: Axiom/Theorem: Any ensemble on terminating states are measurable. *)
+Definition is_measurable {ora: RandomOracle} {psi: RandomDistribution ora} (P: Ensemble RandomHistory): Prop := measurable _ raw_ms P.
 
-(* TODO: so, the overall probability of an event of terminating states is well defined. *)
+(***************************)
+(* overload measurable_set *)
+(***************************)
 
-Goal forall (A: Type) (P: A -> Prop) (a: A) (F: {a: A | P a} -> Prop), Prop.
-intros.
-refine (~ P a \/ exists Pr: P a, F (exist _ a Pr)).
-Defined.
-Print Unnamed_thm.
+Definition measurable_set {ora: RandomOracle} (psi: RandomDistribution ora): Type := measurable_set raw_ms.
+  
+Definition ms_construct {ora: RandomOracle} (psi: RandomDistribution ora) (P: Ensemble RandomHistory) (Pr: is_measurable P):measurable_set psi := exist _ P Pr.
 
+Definition GProb {ora: RandomOracle} {psi: RandomDistribution ora} (P: measurable_set psi): R :=
+  measure_function _ raw_ms P.
+
+Definition tm_meta_pred {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (P: state -> Prop): MetaState state -> Prop :=
+  fun s => match s with Terminating s' => P s' | _ => False end.
+
+Module PreSubsets.
+
+Definition valid_set {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora}  (P: MetaState state -> Prop) (sigma: global_state): Ensemble RandomHistory :=
+  fun h => match sigma h with Some s => P s | _ => False end.
+
+Definition ntm_set {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (sigma: global_state): Ensemble RandomHistory :=
+  valid_set (eq (NonTerminating _)) sigma.
+
+Definition tm_set {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (P: state -> Prop) (sigma: global_state): Ensemble RandomHistory :=
+  valid_set (tm_meta_pred P) sigma.
+
+Lemma tm_set_measurable: forall {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (P: state -> Prop) (sigma: global_state),
+  is_measurable (tm_set P sigma).
+Admitted.
+
+Lemma valid_set_measurable: forall {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (P: MetaState state -> Prop) (sigma: global_state),
+  is_measurable (valid_set (fun _ => True) sigma) ->
+  is_measurable (valid_set P sigma).
+Admitted.
+
+End PreSubsets.
+
+(***************************)
+(* overload global state   *)
+(***************************)
+
+Definition global_state {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora}: Type := {sigma: global_state | is_measurable (PreSubsets.valid_set (fun _ => True) sigma)}.
+
+Definition is_substate {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (P: MetaState state -> Prop) (sigma sigma': global_state): Prop :=
+  forall h,
+    match proj1_sig sigma h with
+    | Some s => (P s /\ proj1_sig sigma' h = Some s) \/ (~ P s /\ proj1_sig sigma' h = None)
+    | None => proj1_sig sigma' h = None
+    end.
+
+Definition valid_set {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora}  (P: MetaState state -> Prop) (sigma: global_state): measurable_set psi :=
+  ms_construct psi
+   (PreSubsets.valid_set P (proj1_sig sigma)) 
+   (PreSubsets.valid_set_measurable _ _ (proj2_sig sigma)).
+
+Definition ntm_set {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (sigma: global_state): measurable_set psi :=
+  valid_set (eq (NonTerminating _)) sigma.
+
+Definition tm_set {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (P: state -> Prop) (sigma: global_state): measurable_set psi :=
+  valid_set (tm_meta_pred P) sigma.
+
+Definition vPr {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (sigma: global_state): R := GProb (ms_construct _ _ (proj2_sig sigma)).
+
+Definition nPr {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (sigma: global_state): R := GProb (ntm_set sigma).
+
+Definition PTriple {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (P: global_state -> Prop) (c: cmd) (Q: global_state -> Prop): Prop :=
+  forall p (s1: global_state),
+    P s1 ->
+    p > 0 ->
+    vPr s1 = p ->
+    nPr s1 = 0 ->
+    forall (s2: global_state),
+      command_oaccess c (proj1_sig s1) (proj1_sig s2) ->
+      vPr s2 = p /\
+      (nPr s2 = 0 -> Q s2).
+
+Definition TTriple {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (P: global_state -> Prop) (c: cmd) (Q: global_state -> Prop): Prop :=
+  forall p (s1: global_state),
+    P s1 ->
+    p > 0 ->
+    vPr s1 = p ->
+    nPr s1 = 0 ->
+    forall (s2: global_state),
+      command_oaccess c (proj1_sig s1) (proj1_sig s2) ->
+      vPr s2 = p /\
+      nPr s2 = 0 /\
+      Q s2.
+
+Definition Pr {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (P: state -> Prop): global_state -> R :=
+  fun sigma => GProb (tm_set P sigma) / vPr sigma.
+
+Definition ConditionalPred {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (P: global_state -> Prop) (condition: state -> Prop): global_state -> Prop :=
+  fun sigma => GProb (tm_set condition sigma) = 0 \/ exists sigma', is_substate (tm_meta_pred condition) sigma sigma' /\ P sigma'.
+
+
+Definition ProbEquiv {imp: Imperative} {sss: SmallStepSemantics} {psi: RandomDistribution ora} (sigma1 sigma2: global_state): Prop :=
+  forall P: MetaState state -> Prop,
+    GProb (valid_set P sigma1) = GProb (valid_set P sigma2).
+
+Require Import Morphisms.
