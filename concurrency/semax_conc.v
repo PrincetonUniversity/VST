e830126202e4fdd6c99c53d6eabf9be838702535
Jean-Marie Madiot <madiot@gmail.com>
2016-05-25 17:23:21 -0400

    semax_conc cleaning and explanations

--- concurrency/semax_conc.v
+++ concurrency/semax_conc.v
@@ -1,26 +1,25 @@
-Require Import veric.base.
 Require Import msl.rmaps.
-Require Import veric.compcert_rmaps.
-Import Mem.
 Require Import msl.msl_standard.
-Require Import veric.juicy_mem veric.juicy_mem_lemmas veric.juicy_mem_ops.
-Require Import sepcomp.extspec.
+Require Import msl.seplog.
+Require Import veric.base.
+Require Import veric.compcert_rmaps.
+Require Import veric.juicy_mem.
+Require Import veric.juicy_mem_lemmas.
+Require Import veric.juicy_mem_ops.
 Require Import veric.juicy_extspec.
 Require Import veric.tycontext.
 Require Import veric.expr2.
 Require Import veric.semax.
 Require Import veric.semax_call.
-
+Require Import sepcomp.extspec.
 Require Import floyd.proofauto.
 
-(* we don't really know these variables *)
-(* let's suppose that for now we don't need _lock_t, we replace lock_t with int *)
-
+(* Variables to be instantiated once the program is known. *)
 Definition _f := 1%positive.      (* alpha-convertible *)
 Definition _args := 2%positive.   (* alpha-convertible *)
 Definition _lock := 1%positive.   (* alpha-convertible *)
 Definition _lock_t := 2%positive. (* 2 is the number given by
-clightgen threads.h is included first *)
+clightgen when threads.h is included first *)
 
 Variable voidstar_funtype : type.
 Definition tlock := Tstruct _lock_t noattr.
@@ -29,9 +28,11 @@ Definition tlock := Tstruct _lock_t noattr.
 Axiom lock_inv : share -> val -> mpred -> mpred.
 
 Axiom lock_inv_share_join : forall sh1 sh2 sh v R, sepalg.join sh1 sh2 sh ->
-  lock_inv sh1 v R * lock_inv sh2 v R = lock_inv sh v R.
+  (lock_inv sh1 v R * lock_inv sh2 v R = lock_inv sh v R)%logic.
 
-Axiom lock_inv_isptr : forall sh v R, `(lock_inv sh v R) |-- !!(isptr v).
+Axiom lock_inv_isptr : forall sh v R, (`(lock_inv sh v R) |-- !!(isptr v))%logic.
+
+(*+ Deep embedding of [mpred]s *)
 
 Inductive Pred :=
   | Mapsto :  Share.t -> type -> val -> val -> Pred
@@ -55,133 +56,118 @@ Fixpoint Interp (p : Pred) : mpred :=
   | Pred_list l => fold_right sepcon emp (map Interp l)
   end.
 
+(*+ Specification of each concurrent primitive *)
+
 Definition acquire_spec :=
    WITH v : val, sh : share, R : Pred
    PRE [ _lock OF tptr tlock ]
      PROP (readable_share sh)
      LOCAL (temp _lock v)
-     SEP ((lock_inv sh v (Interp R)))
+     SEP (lock_inv sh v (Interp R))
    POST [ tvoid ]
      PROP ()
      LOCAL ()
-     SEP ((lock_inv sh v (Interp R)) ; Interp R).
+     SEP (lock_inv sh v (Interp R); Interp R).
 
 Definition release_spec :=
    WITH v : val, sh : share, R : Pred
    PRE [ _lock OF tptr tlock ]
      PROP (readable_share sh)
      LOCAL (temp _lock v)
-     SEP ((lock_inv sh v (Interp R)) ; Interp R)
+     SEP (lock_inv sh v (Interp R); Interp R)
    POST [ tvoid ]
      PROP ()
      LOCAL ()
-     SEP ((lock_inv sh v (Interp R))).
+     SEP (lock_inv sh v (Interp R)).
 
 Definition makelock_spec cs :=
    WITH v : val, sh : share, R : Pred
    PRE [ _lock OF tptr tlock ]
      PROP (writable_share sh)
      LOCAL (temp _lock v)
-     SEP ((@data_at_ cs sh tlock v))
+     SEP (@data_at_ cs sh tlock v)
    POST [ tvoid ]
      PROP ()
      LOCAL ()
-     SEP ((lock_inv sh v (Interp R))).
-
-(* Definition makelock_spec cs := *)
-(*    WITH v : val, sh : share *)
-(*    PRE [ _lock OF tptr tlock ] *)
-(*      PROP (writable_share sh) *)
-(*      LOCAL (temp _lock v) *)
-(*      SEP ((@data_at_ cs sh tlock v)) *)
-(*    POST [ tvoid ] *)
-(*      PROP () *)
-(*      LOCAL () *)
-(*      SEP ((lock_inv sh v emp)). *)
+     SEP (lock_inv sh v (Interp R)).
 
 Definition freelock_spec cs :=
    WITH v : val, sh : share, R : Pred
    PRE [ _lock OF tptr tlock ]
      PROP (writable_share sh)
      LOCAL (temp _lock v)
-     SEP (Interp R ; (lock_inv sh v (Interp R)))
+     SEP (Interp R ; lock_inv sh v (Interp R))
    POST [ tvoid ]
      PROP ()
      LOCAL ()
-     SEP (Interp R ; (@data_at_ cs sh tlock v)).
+     SEP (Interp R ; @data_at_ cs sh tlock v).
 
-(* See discussion about spawn in threadlib.v *)
+(* Notes about spawn_thread:
+
+As in makelock/acquire/... we get a universe inconsistency when we add
+"WITH P" to the specification.  This universe inconsistency will
+eventually disappear in a different model for rmaps tracking
+covariance and contravariance.  In the meantime, we use a the deep
+embedding [Pred] and an embedding [PrePost] of the pre- and
+post-condition depending on Type term [ty].
+
+The spawned function has for argument name [_y], which is
+existentially quantified.  The function also can use a list of global
+variables [globals].
+
+For now, the specification of the spawned function has to be exactly
+of the form that you can see below (inside the "match PrePost ...").
+Cao Qinxiang is working on a notion of sub-specification that might
+enable us to have smoother specifications.
+
+The postcondition might not be emp, so we have potential memory leaks
+when a thread exists (those maps are still handled by the concurrent
+machine).
+
+To enable joinable threads, the postcondition would be [tptr tthread]
+with a type [tthread] related to the postcondition through a [thread]
+predicate in the logic.  The [join] would then also be implemented
+using the oracle, as [acquire] is.  The postcondition would be [match
+PrePost with existT ty (w, pre, post) => thread th (Interp (post w b))
+end] *)
 
 Definition spawn_spec :=
   WITH f : val,
        b : val,
-       PrePost : { ty : Type &
-                        (ty *                     (* WITH we call spawn with *)
-                        (ty -> val -> Pred) *       (* precondition *)
-                        (ty -> val -> Pred))%type}  (* postcondition *)
+       PrePost :
+         { ty : Type &
+          (ty *                     (* WITH we call spawn with *)
+          (ty -> val -> Pred) *       (* precondition *)
+          (ty -> val -> Pred))%type}  (* postcondition *)
    PRE [_f OF tptr voidstar_funtype, _args OF tptr tvoid]
      PROP ()
      LOCAL (temp _args b)
      SEP
-     (EX _y : ident,
-match PrePost with existT ty (_, pre, post) =>
+     (EX _y : ident, EX globals : list (ident * val),
+      match PrePost with existT ty (_, pre, post) =>
       (func_ptr'
         (WITH y : val, x : ty
           PRE [ _y OF tptr tvoid ]
-            PROP  ()
-            LOCAL (temp _y y)
-            SEP   (Interp (pre x y))
+            PROP ()
+            (LOCALx (temp _y y :: map (fun x => gvar (fst x) (snd x)) globals)
+            (SEP   (Interp (pre x y))))
           POST [tptr tvoid]
             PROP  ()
             LOCAL ()
-            SEP   (Interp (post x y))
-       )
+            SEP   (Interp (post x y)))
        f)
-end;
-match PrePost with existT ty (w, pre, post) =>
+      end;
+      match PrePost with existT ty (w, pre, post) =>
       Interp (pre w b)
-end
-     )
-   POST [ tvoid (* tptr tthread *) ]
-   (* EX th : val, *)
+      end)
+   POST [ tvoid  ]
      PROP  ()
-     LOCAL ((* temp ret_temp th *))
-     SEP   (emp
-(* match PrePost with existT ty (w, pre, post) => *)
-(* thread th (Interp (post w b)) *)
-(* end *)
-           ).
+     LOCAL ()
+     SEP   (emp).
+
+(*+ Adding the specifications to an ext_spec *)
 
 Require Import veric.semax_ext.
-Require veric.NullExtension.
-
-(* this? *)
-(*
-Variables _acquire _release _makelock _freelock _spawn : ident.
-
-Definition threadspecs cs :=
-  (_acquire, acquire_spec) ::
-  (_release, release_spec) ::
-  (_makelock, makelock_spec cs) ::
-  (_freelock, freelock_spec cs) ::
-  (_spawn, spawn_spec) ::
-  nil.
-
-Definition CEspec cs :=
-  add_funspecs
-    veric.NullExtension.Espec
-    (fun s =>
-       match s with
-       | "acquire"%string => _acquire
-       | "release"%string => _release
-       | "makelock"%string => _makelock
-       | "freelock"%string => _freelock
-       | "spawn"%string => _spawn
-       | _ => 1%positive
-       end)
-    (threadspecs cs).
-*)
-(* or this? *)
 
 Definition threadspecs (cs : compspecs) (ext_link : string -> ident) :=
   (ext_link "acquire"%string, acquire_spec) ::
@@ -197,22 +183,16 @@ Definition CEspec (cs : compspecs) (ext_link : string -> ident) :=
     ext_link
     (threadspecs cs ext_link).
 
-(** * question: cs?  *)
-
-(** * question: ext_link? *)
-
-(** * question: Pred? *)
-
 Lemma semax_conc' cs (ext_link: Strings.String.string -> ident) id sig callingc A P Q :
   let fs := threadspecs cs ext_link in
   let f := mk_funspec sig callingc A P Q in
   in_funspecs (ext_link id, f) fs ->
   funspecs_norepeat fs ->
-  (forall n, semax.semax_external
-        (* veric.SeparationLogicSoundness.SoundSeparationLogic.CSL.semax_external *)
-          (** * it seems the Espec is already instanciated *)
-          (CEspec cs ext_link) (fst (split (fst sig)))
-          (EF_external id (funsig2signature sig callingc)) _ P Q n).
+  (forall n,
+      semax.semax_external
+        (** * it seems the Espec is already instanciated *)
+        (CEspec cs ext_link) (fst (split (fst sig)))
+        (EF_external id (funsig2signature sig callingc)) _ P Q n).
 Proof.
   apply semax_ext'.
 Qed.
@@ -228,12 +208,3 @@ Lemma semax_conc cs (ext_link: Strings.String.string -> ident) id ids sig sig' c
 Proof.
   apply semax_ext.
 Qed.
-
-(* this makes a dependency (because of the notations for the function
-specs), but in real life we'll call this with these specs and use the
-proofs in SeparationLogicSoundness to prove this, right? *)
-
-(* later: naming problem:
-- all functions use _lock (and spawn uses _f and _args) : alpha-conversion?
-- makelock/freelock use _lock_t (but it will be replaced with an integer, right?)
- *)
