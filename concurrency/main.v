6ff18819bd36b559aaf58a2e165880b2e9ca11af
Santiago <scuellar@princeton.edu>
2016-07-06 15:59:42 -0400

    Working on Main.

--- concurrency/main.v
+++ concurrency/main.v
@@ -1,9 +1,49 @@
+(** *Semax Imports*)
+Require Import Coq.Strings.String.
 
-(*Require Import semax_to_juicy_machine.*)
-Require Import erasure_signature.
-Require Import erasure_proof.
-Require Import erasure_safety.
+Require Import compcert.lib.Integers.
+Require Import compcert.common.AST.
+Require Import compcert.cfrontend.Clight.
+Require Import compcert.common.Globalenvs.
+Require Import compcert.common.Memory.
+Require Import compcert.common.Memdata.
+Require Import compcert.common.Values.
 
+Require Import msl.Coqlib2.
+Require Import msl.eq_dec.
+Require Import veric.initial_world.
+Require Import veric.juicy_mem.
+Require Import veric.juicy_mem_lemmas.
+Require Import veric.semax_prog.
+Require Import veric.compcert_rmaps.
+Require Import veric.Clight_new.
+Require Import veric.Clightnew_coop.
+Require Import veric.semax.
+Require Import veric.semax_ext.
+Require Import veric.juicy_extspec.
+Require Import veric.initial_world.
+Require Import veric.juicy_extspec.
+Require Import veric.tycontext.
+Require Import veric.semax_ext.
+Require Import veric.semax_ext_oracle.
+Require Import sepcomp.semantics.
+Require Import sepcomp.step_lemmas.
+Require Import sepcomp.event_semantics.
+Require Import concurrency.semax_conc.
+Require Import concurrency.juicy_machine.
+Require Import concurrency.concurrent_machine.
+Require Import concurrency.scheduler.
+Require Import concurrency.addressFiniteMap.
+Require Import concurrency.permissions.
+
+Require Import semax_to_juicy_machine.
+
+(** *Erasure Imports*)
+Require Import concurrency.erasure_signature.
+Require Import concurrency.erasure_proof.
+Require Import concurrency.erasure_safety.
+
+(** **)
 
 Module MainSafety (DecayingSEM: DecayingSemantics).
 
@@ -23,6 +63,21 @@ Module MainSafety (DecayingSEM: DecayingSemantics).
   Definition local_erasure:= erasure initU init_rmap init_pmap init_rmap_perm.
   Definition step_diagram:= ErasureProof.core_diagram.
 
+
+  Section Initil.
+    Import 
+    Variables
+      (CS : compspecs)
+      (V : varspecs)
+      (G : funspecs)
+      (ext_link : string -> ident)
+      (ext_link_inj : forall s1 s2, ext_link s1 = ext_link s2 -> s1 = s2)
+      (prog : program)
+      (all_safe : semax_prog.semax_prog (Concurrent_Oracular_Espec CS ext_link) prog V G)
+      (init_mem_not_none : Genv.init_mem prog <> None).
+
+    initial_machine_state
+    
   
 (*From semax_to_juicy_machine*)
 (*Lemma juicy_safety: forall U rmap0 genv main vals js,
