9933c78823ca708348c3a187edd6c3d5b37f1d95
Santiago <scuellar@princeton.edu>
2016-07-06 20:10:16 -0400

    Mining.

--- concurrency/main.v
+++ concurrency/main.v
@@ -45,14 +45,14 @@ Require Import concurrency.erasure_safety.
 
 (** **)
 
-Module MainSafety (DecayingSEM: DecayingSemantics).
+Module MainSafety .
 
-  Module ErasureProof := erasure_proof.Parching DecayingSEM.
+  Module ErasureProof := erasure_proof.Parching.
   Module Erasure := ErasureFnctr ErasureProof.
   Import ErasureProof.
   Import Erasure.
 
-  Module ErasureSafety := ErasureSafety DecayingSEM.
+  Module ErasureSafety := ErasureSafety.
   Import ErasureSafety.
 
   Parameters initU: DryMachine.Sch.
@@ -60,7 +60,7 @@ Module MainSafety (DecayingSEM: DecayingSemantics).
   Parameter init_pmap : DSEM.perm_map.
   Parameter init_rmap_perm:  match_rmap_perm init_rmap init_pmap.
 
-  Definition local_erasure:= erasure initU init_rmap init_pmap init_rmap_perm.
+  (*Definition local_erasure:= erasure initU init_rmap init_pmap init_rmap_perm*)
   Definition step_diagram:= ErasureProof.core_diagram.
 
 
@@ -74,9 +74,65 @@ Module MainSafety (DecayingSEM: DecayingSemantics).
       (ext_link_inj : forall s1 s2, ext_link s1 = ext_link s2 -> s1 = s2)
       (prog : program)
       (all_safe : semax_prog.semax_prog (Concurrent_Oracular_Espec CS ext_link) prog V G)
-      (init_mem_not_none : Genv.init_mem prog <> None).
+      (init_mem_not_none : Genv.init_mem prog <> None)
+      (x: block)
+      (block: (Genv.find_symbol (globalenv prog) (prog_main prog) = Some x)).
 
     
+    Definition js_initial n := initial_machine_state CS V G ext_link prog all_safe
+                                                     init_mem_not_none n.
+    Definition dry_initial_perm :=
+      getCurPerm( proj1_sig (init_m prog init_mem_not_none)).
+    
+    Definition dry_initial_core:=
+      initial_core (juicy_core_sem cl_core_sem) 
+                   (globalenv prog) (Vptr x Int.zero) nil.
+    Definition ds_initial c := DSEM.initial_machine
+                               dry_initial_perm
+                               c.                   
+    Lemma erasure_match: forall n,
+        exists c, dry_initial_core = Some c /\ 
+             ErasureProof.match_st (js_initial n) (ds_initial c).
+    Proof. intros.
+           destruct (dry_initial_core) eqn: AA.
+           exists c; split; auto.
+           constructor.
+           - intro i.
+             unfold js_initial, initial_machine_state, ErasureProof.JTP.containsThread; simpl;
+             unfold ds_initial ,DSEM.initial_machine, ErasureProof.DTP.containsThread; simpl; auto.
+           - intro i.
+             unfold js_initial, initial_machine_state, ErasureProof.JTP.containsThread; simpl;
+             unfold ds_initial ,DSEM.initial_machine, ErasureProof.DTP.containsThread; simpl; auto.
+           - unfold ErasureProof.JTP.getThreadC, ErasureProof.DTP.getThreadC; simpl.
+             intros. 
+             transitivity (Krun c). f_equal.
+             unfold initial_corestate.
+             destruct (spr CS V G ext_link prog all_safe init_mem_not_none); simpl.
+             destruct s;  simpl.
+             unfold dry_initial_core in AA.
+             destruct p.
+             destruct p. congruence.
+             admit.
+           - intros.
+             unfold ErasureProof.JTP.getThreadR;
+               unfold ErasureProof.DTP.getThreadR; simpl.
+             unfold dry_initial_perm. unfold initial_jm; simpl.
+             destruct (spr CS V G ext_link prog all_safe init_mem_not_none) eqn:NN.
+             simpl.
+             unfold init_m.
+             
+             admit. (*Should follow from mem_compatible? *)
+           - intros.
+             unfold ErasureProof.JSEM.ThreadPool.lockRes;
+               unfold ErasureProof.DSEM.ThreadPool.lockRes.
+             auto.
+           - unfold ErasureProof.DSEM.ThreadPool.lockRes.
+             simpl. intros.  rewrite threadPool.find_empty in H0; inversion H0.
+           - unfold ErasureProof.DSEM.ThreadPool.lockRes.
+             simpl. intros.  rewrite threadPool.find_empty in H0; inversion H0.
+    Admitted.
+             
+             
   
 (*From semax_to_juicy_machine*)
 (*Lemma juicy_safety: forall U rmap0 genv main vals js,
