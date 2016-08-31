1a22bb329a17dc8d519f2790b7124366c7454cc7
Andrew W. Appel <appel@princeton.edu>
2016-08-31 08:37:30 -0400

    Added progs/queue2.v, a simplified version of progs/queue.v

---
+++ progs/queue2.c
@@ -0,0 +1,72 @@
+#include <stddef.h>
+
+extern void *mallocN (int n);
+extern void freeN(void *p, int n);
+
+struct elem {
+  int data;
+  struct elem *next;
+};
+
+struct fifo {
+  struct elem *head;
+  struct elem *tail;
+};
+
+struct fifo *fifo_new(void) {
+  struct fifo *Q = (struct fifo *)mallocN(sizeof (*Q));
+  Q->head = NULL;
+  Q->tail = NULL;
+  return Q;
+}
+
+void fifo_put (struct fifo *Q, struct elem *p) {
+  struct elem *h, *t;
+  p->next=NULL;
+  h = Q->head;
+  if (h==NULL) {
+    Q->head=p;
+    Q->tail=p;
+  }
+  else {
+    t = Q->tail;
+    t->next=p;
+    Q->tail=p;
+  }
+}
+
+int fifo_empty (struct fifo *Q) {
+  struct elem *h;
+  h = Q->head;
+  return (h == NULL);
+}
+
+struct elem *fifo_get (struct fifo *Q) {
+  struct elem *h, *n;
+  h=Q->head;
+  n=h->next;
+  Q->head=n;
+  return h;
+}
+
+struct elem *make_elem(int data) {
+  struct elem *p;
+  p = mallocN(sizeof (*p));
+  p->data=data;
+  return p;
+}
+
+int main(void) {
+  int i;
+  struct fifo *Q;
+  struct elem *p;
+  Q = fifo_new();
+  p = make_elem(1);
+  fifo_put(Q,p);
+  p = make_elem(2);
+  fifo_put(Q,p);
+  p = fifo_get(Q);
+  i = p->data; 
+  freeN(p, sizeof(*p));
+  return i;
+}
