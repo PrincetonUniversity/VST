c99bd780afd26f8c02dca1c04eaf906ec0f0bc36
Samuel Gruetter <samuel.gruetter@epfl.ch>
2016-10-10 20:06:56 -0400

    prove xtime_zero except for case when i >= 256 (to be excluded)

--- aes/mult_equiv_lemmas.v
+++ aes/mult_equiv_lemmas.v
@@ -34,15 +34,47 @@ Tactic Notation "int_zero_case_if" constr(v) :=
   [ rewrite E in *; rewrite Int.eq_true in *
   | rewrite (Int.eq_false _  _ E) in *].
 
+(* TODO QQQ am I reinventing the wheel? *)
+Ltac omega' := replace Int.zwordsize with 32 in * by reflexivity; omega.
+
 Lemma xtime_zero: forall i,
   (* TODO what if i too big? *)
   xtime i = Int.zero -> i = Int.zero.
 Proof.
-  intros. unfold xtime in H. int_zero_case_if (Int.and i (Int.repr 128)).
-  (* QQQf: H tells me that bits #6..#0 are 0, and H0 tells me that bit #7 is 0,
-     and we'll have i < 256, so i = Int.zero.
-     Are there already some proofs doing using bit-level reasoning lemmas? *)
-Admitted.
+  intros. unfold xtime in H.
+  erewrite Int.modu_and in H by reflexivity.
+  replace (Int.sub (Int.repr 256) Int.one) with (Int.repr 255) in H by reflexivity.
+  int_zero_case_if (Int.and i (Int.repr 128)).
+  - apply Int.same_bits_eq. intros j ?. rewrite Int.bits_zero.
+    assert (0 <= j <= 6 \/ j = 7 \/ 8 <= j < 32) as C by omega'.
+    destruct C as [C | [C | C]].
+    + subst. apply (f_equal (fun x => Int.testbit x (j + 1))) in H.
+      rewrite Int.bits_and in H by omega'.
+      assert (Int.testbit (Int.repr 255) (j+1) = true) as E. {
+        assert (j = 0 \/ j = 1 \/ j = 2 \/ j = 3 \/ j = 4 \/ j = 5 \/ j = 6) as C1 by omega.
+        repeat destruct C1 as [C1 | C1]; subst j; reflexivity.
+      }
+      rewrite E in H.
+      rewrite andb_true_r in H.
+      rewrite Int.bits_shl in H by omega'.
+      rewrite Int.unsigned_one in H. replace (j + 1 - 1) with j in H by omega.
+      destruct (zlt (j + 1) 1). omega. rewrite H. apply Int.bits_zero.
+    + subst. apply (f_equal (fun x => Int.testbit x 7)) in H0.
+      rewrite Int.bits_and in H0 by omega'.
+      replace (Int.testbit (Int.repr 128) 7) with true in H0 by reflexivity.
+      rewrite andb_true_r in H0. rewrite H0. reflexivity.
+    + admit.
+  - apply (f_equal (fun x => Int.testbit x 0)) in H.
+    rewrite Int.bits_xor in H by omega'.
+    replace (Int.testbit (Int.repr 27) 0) with true in H by reflexivity.
+    rewrite xorb_true_r in H.
+    rewrite Int.bits_zero in H.
+    rewrite Int.bits_and in H by omega'.
+    replace (Int.testbit (Int.repr 255) 0) with true in H by reflexivity.
+    rewrite andb_true_r in H.
+    rewrite Int.bits_shl in H by omega'.
+    destruct (zlt 0 (Int.unsigned Int.one)); discriminate.
+Qed.
 
 Lemma fast_times3: forall (i: int),
   Int.xor i (xtime i) = ff_mult (Int.repr 3) i.
