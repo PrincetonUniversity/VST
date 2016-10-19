e4c445699a230c0ccee97947195f28be61663c6a
Samuel Gruetter <samuel.gruetter@epfl.ch>
2016-10-19 14:54:30 -0400

    include mult_equiv_lemmas in compile.sh

--- aes/compile.sh
+++ aes/compile.sh
@@ -2,10 +2,7 @@
 
 cd .. # now we're in VST
 
-coqc `cat .loadpath` -Q ./aes "" aes/aes.v aes/AES256.v aes/aesutils.v aes/aes_round_lemmas.v aes/forwarding_table_lemmas.v aes/verif_aes256.v
-
-# takes forever:
-# aes/mult_equiv_lemmas.v
+coqc `cat .loadpath` -Q ./aes "" aes/aes.v aes/AES256.v aes/aesutils.v aes/mult_equiv_lemmas.v aes/aes_round_lemmas.v aes/forwarding_table_lemmas.v aes/verif_aes256.v
 
 cd ./aes # go back
 
