aa13b319c892352f4f34b09762fda55279137143
Santiago <scuellar@princeton.edu>
2016-11-07 23:00:58 -0500

    Better fix.

--- concurrency/dry_machine.v
+++ concurrency/dry_machine.v
@@ -435,7 +435,7 @@ Module Concur.
            (** data permissions are computed in a non-deterministic way *)
            (Hneq_perms: forall i,
                  (0 <= Z.of_nat i < LKSIZE)%Z ->
-                 Mem.perm_order'' (pmap_tid'.1 !! b (Int.intval ofs + Z.of_nat i)%Z) (Some Writable)
+                 Mem.perm_order'' (pdata i) (Some Writable)
            )
            (*Hpdata: perm_order pdata Writable*)
            (Hdata_perm: setPermBlock_var (*=setPermBlockfunc*)
