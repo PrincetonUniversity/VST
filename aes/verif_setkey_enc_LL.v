97dcda82742280781c8323c8fb41cad122d56c30
Samuel Gruetter <samuel.gruetter@epfl.ch>
2017-02-21 19:58:55 -0500

    various ways to define key expansion
    the last one is the nicest

--- aes/verif_setkey_enc_LL.v
+++ aes/verif_setkey_enc_LL.v
@@ -25,6 +25,178 @@ Definition tables_uninitialized tables := data_at Ews t_struct_tables (Vundef256
   (Vundef256, (Vundef256, (Vundef256, (Vundef256, 
   (repeat Vundef 10))))))))))) tables.
 
+Definition Nk := 8. (* number of words in key *)
+Definition Nr := 14. (* number of cipher rounds *)
+Definition Nb := 4. (* number of words in a block (state) *)
+
+Definition byte0 (x : int) : Z :=
+  (Z.land (Int.unsigned x) (Int.unsigned (Int.repr 255))).
+Definition byte1 (x : int) : Z :=
+  (Z.land (Int.unsigned (Int.shru x (Int.repr 8))) (Int.unsigned (Int.repr 255))).
+Definition byte2 (x : int) : Z :=
+  (Z.land (Int.unsigned (Int.shru x (Int.repr 16))) (Int.unsigned (Int.repr 255))).
+Definition byte3 (x : int) : Z :=
+  (Z.land (Int.unsigned (Int.shru x (Int.repr 24))) (Int.unsigned (Int.repr 255))).
+
+Definition word_to_int (w : (int * int * int * int)) : int :=
+  match w with (b0, b1, b2, b3) =>
+    (Int.or (Int.or (Int.or
+             b0
+    (Int.shl b1 (Int.repr  8)))
+    (Int.shl b2 (Int.repr 16)))
+    (Int.shl b3 (Int.repr 24)))
+  end.
+
+(* SubWord function from section 5.2: apply S-box to each byte in a word *)
+Definition SubWord (w: int) : int := word_to_int (
+  (Znth (byte0 w) FSb Int.zero),
+  (Znth (byte1 w) FSb Int.zero),
+  (Znth (byte2 w) FSb Int.zero),
+  (Znth (byte3 w) FSb Int.zero)
+).
+
+Definition RotWord(i: int): int := 
+  Int.or (Int.and (Int.shl i (Int.repr 8)) (Int.repr (-1))) (Int.shru i (Int.repr 24)).
+
+(* round constant (RCon) array, described in section 5.2 and explicitly written
+ * out in appendix A.3 (256-bit key expansion example) *)
+Definition RCon : list int := map (fun i => Int.shl i (Int.repr 24)) [
+  (* 0x01000000 *) (Int.repr 1);
+  (* 0x02000000 *) (Int.repr 2);
+  (* 0x04000000 *) (Int.repr 4);
+  (* 0x08000000 *) (Int.repr 8);
+  (* 0x10000000 *) (Int.repr 16);
+  (* 0x20000000 *) (Int.repr 32);
+  (* 0x40000000 *) (Int.repr 64)
+].
+
+Definition if_less_than_8{T: Type}(i: nat)(thenF: Z -> T)(elseF: nat -> nat -> Z -> T): T :=
+  match i with
+  | S i_minus_1 => match i_minus_1 with
+    | S (S (S (S (S (S (S i_minus_8)))))) => elseF i_minus_8 i_minus_1 (Z.of_nat i)
+    | _ => thenF (Z.of_nat i)
+    end
+  | _ => thenF (Z.of_nat i)
+  end.
+
+Fixpoint KeyExpansionWord_test(i: nat)(key: list int): int :=
+  if_less_than_8 i
+  (fun i => (Znth i key Int.zero))
+  (fun i_minus_8 i_minus_1 i =>
+    let temp := KeyExpansionWord_test i_minus_1 key in
+    Int.xor temp (KeyExpansionWord_test i_minus_8 key)).
+
+Fixpoint loop_1_8{T: Type}(i: nat)(funLt8: Z -> T)(funGt8: Z -> T -> T -> T): T :=
+  match i with
+  | S i_minus_1 => match i_minus_1 with
+    | S (S (S (S (S (S (S i_minus_8)))))) =>
+      funGt8 (Z.of_nat i) (loop_1_8 i_minus_1 funLt8 funGt8) (loop_1_8 i_minus_8 funLt8 funGt8)
+    | _ => funLt8 (Z.of_nat i)
+    end
+  | _ => funLt8 (Z.of_nat i)
+  end.
+
+Fixpoint construct_list{T: Type}(size: nat)(f: Z -> list T -> T): list T :=
+  match size with
+  | O => []
+  | S n => let prevs := (construct_list n f) in prevs ++ [f (Z.of_nat n) prevs]
+  end.
+
+Definition KeyExpansion1(key: list int): list int := construct_list (Z.to_nat (Nb*(Nr+1)))
+  (fun (i: Z) (w: list int) =>
+    if (i <? 8) then
+      (Znth i key Int.zero)
+    else
+      let temp := (Znth (i-1) w Int.zero) in
+      let temp' := if (i mod Nk =? 0) then
+        Int.xor (SubWord (RotWord temp)) (Znth (i/Nk) RCon Int.zero)
+      else if (i mod Nk =? 4) then
+        SubWord temp
+      else
+        temp
+      in
+        Int.xor (Znth (i-8) w Int.zero) temp').
+
+Fixpoint grow_list{T: Type}(l: list T)(toAdd: nat)(f: Z -> list T -> T): list T :=
+  match toAdd with
+  | O => l
+  | S n => let prevs := (construct_list n f) in prevs ++ [f (Zlength l + Z.of_nat n) prevs]
+  end.
+
+(*Require Import Coq.Program.Basics. for compose *)
+
+(*
+Fixpoint funPow{T: Type}(f: T -> T)(n: nat): T -> T := match n with
+| O => id
+| S m => fun x : T => f ((funPow f m) x)
+end.
+*)
+
+(* takes forever to check:
+Fixpoint KeyExpansionWord(i: nat)(key: list int){struct i}: int :=
+  if_less_than_8 i
+  (fun i => (Znth i key Int.zero))
+  (fun i_minus_8 i_minus_1 i =>
+    let temp := KeyExpansionWord i_minus_1 key in
+    let temp' := if (i mod Nk =? 0) then
+      Int.xor (SubWord (RotWord temp)) (Znth (i/Nk) RCon Int.zero)
+    else if (i mod Nk =? 4) then
+      SubWord temp
+    else
+      temp
+    in
+      Int.xor (KeyExpansionWord_test i_minus_8 key) temp').
+*)
+
+(*
+Fixpoint KeyExpansionWord(i: nat)(key: list int): int :=
+  if_less_than_8 i
+  (fun i => (Znth (Z.of_nat i) key Int.zero))
+  (fun i_minus_8 i_minus_1 i =>
+    let temp := KeyExpansionWord i_minus_1 key in
+    Int.xor temp (KeyExpansionWord i_minus_8 key)).
+*)
+
+Fixpoint KeyExpansionWordTest1(inat: nat)(key: list int): int :=
+  let i := Z.of_nat inat in
+  match inat with
+  | S i1nat => match i1nat with
+    | S (S (S (S (S (S (S i8nat)))))) => 
+      let temp := KeyExpansionWordTest1 i1nat key in
+      Int.xor temp (KeyExpansionWordTest1 i8nat key)
+    | _ => (Znth i key Int.zero)
+    end
+  | _ => (Znth i key Int.zero)
+  end.
+
+Program Fixpoint KeyExpansionWord(i: Z)(key: list int){measure (Z.to_nat i)}: int :=
+  if i <? Nk then
+    (Znth i key Int.zero)
+  else
+    let temp := KeyExpansionWord (i-1) key in
+    Int.xor temp (KeyExpansionWord (i-Nk) key).
+Next Obligation.
+Abort.
+
+Definition GrowKeyByOne(w: list int): list int :=
+  let i := Zlength w in
+  let temp := (Znth (i-1) w Int.zero) in
+  let temp' := if (i mod Nk =? 0) then
+    Int.xor (SubWord (RotWord temp)) (Znth (i/Nk) RCon Int.zero)
+  else if (i mod Nk =? 4) then
+    SubWord temp
+  else
+    temp
+  in
+    w ++ [Int.xor (Znth (i-8) w Int.zero) temp'].
+
+Fixpoint pow_fun{T: Type}(f: T -> T)(n: nat)(a: T): T := match n with
+| O => a
+| S m => f (pow_fun f m a)
+end.
+
+Definition KeyExpansion: list int -> list int := pow_fun GrowKeyByOne (Z.to_nat (Nb*(Nr+1))).
+
 Definition key_expansion_spec :=
   DECLARE _mbedtls_aes_setkey_enc
     WITH ctx : val, key : val, ctx_sh : share, key_sh : share, key_chars : list Z,
