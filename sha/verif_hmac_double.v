70dc8247af4ae0129321339f727f7cd77d9d5e16
Qinxiang <caoqinxiang@gmail.com>
2017-04-04 07:18:11 -0400

    A fix in sha/hmac.

--- sha/verif_hmac_double.v
+++ sha/verif_hmac_double.v
@@ -79,7 +79,7 @@ forward_if  (
    SEP  (data_at_ Tsh t_struct_hmac_ctx_st c; data_block Tsh key k;
    data_block Tsh data d; K_vector kv;
    memory_block shmd 64 md)).
-  { apply denote_tc_comparable_split.
+  { apply denote_tc_test_eq_split.
        apply sepcon_valid_pointer2. apply memory_block_valid_ptr. auto. omega.
        apply valid_pointer_zero. }
   { (* Branch1 *) exfalso. subst md. contradiction.  }
