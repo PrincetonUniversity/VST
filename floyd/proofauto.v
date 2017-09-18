0eabd0ea06ad169f2fff7fc7a1b46f6387a8637d
Andrew W. Appel <appel@princeton.edu>
2017-09-18 13:11:06 -0400

    Installed Jason Gross's cool new Notation prettyprinter for C.  It still needs a couple of things modified, which Jason is working on.

--- floyd/proofauto.v
+++ floyd/proofauto.v
@@ -35,6 +35,7 @@ Require Export VST.floyd.globals_lemmas.
 Require Export VST.floyd.diagnosis.
 Require Export VST.floyd.freezer.
 Require Export VST.floyd.deadvars.
+Require Export VST.floyd.Clightnotations.
 Export ListNotations.
 
 Hint Rewrite add_repr mul_repr sub_repr : entailer_rewrite.
