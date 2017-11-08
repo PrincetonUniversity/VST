63825bfb3f28c4f31c794c97d36039f8d43e1c96
QinxiangCao <caoqinxiang@gmail.com>
2017-11-08 02:38:46 -0500

    Debug verif_union.

--- progs/verif_union.v
+++ progs/verif_union.v
@@ -3,6 +3,30 @@ Require Import VST.progs.union.
 Instance CompSpecs : compspecs. make_compspecs prog. Defined.
 Definition Vprog : varspecs. mk_varspecs prog. Defined.
 
+Module Single.
+
+Definition fabs_single_spec :=
+ DECLARE _fabs_single
+ WITH x: float32
+ PRE [ _x OF Tfloat F32 noattr]
+   PROP() LOCAL(temp _x (Vsingle x)) SEP()
+ POST [ Tfloat F32 noattr ]
+   PROP() LOCAL (temp ret_temp (Vsingle (Float32.abs x))) SEP().
+
+Definition Gprog : funspecs :=
+    ltac:(with_library prog [ fabs_single_spec ]).
+
+Lemma body_fabs_single: semax_body Vprog Gprog f_fabs_single fabs_single_spec.
+Proof.
+start_function.
+forward.
+forward.
+Abort.
+
+End Single.
+
+Module Float.
+
 Definition fabs_single_spec :=
  DECLARE _fabs_single
  WITH x: float
@@ -11,18 +35,14 @@ Definition fabs_single_spec :=
  POST [ Tfloat F32 noattr ]
    PROP() LOCAL (temp ret_temp (Vfloat (Float.abs x))) SEP().
 
-
 Definition Gprog : funspecs :=
     ltac:(with_library prog [ fabs_single_spec ]).
-
 
 Lemma body_fabs_single: semax_body Vprog Gprog f_fabs_single fabs_single_spec.
 Proof.
 start_function.
-forward.
-(* in this step there's a soundness bug. *) forward.
-forward.
-forward.
-forward.
-Qed.
+assert_PROP(False).
+go_lower.
+Abort.
 
+End Float.
