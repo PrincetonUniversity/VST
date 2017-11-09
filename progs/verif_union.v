fa06fa044e1db66763e3a98c90eabd68988f0909
QinxiangCao <caoqinxiang@gmail.com>
2017-11-09 10:25:55 -0500

    Small edits.

--- progs/verif_union.v
+++ progs/verif_union.v
@@ -7,6 +7,33 @@ Definition samerep (v1 v2: val) :=
   forall bl: list Memdata.memval,
    Memdata.decode_val Mfloat32 bl = v1 <-> Memdata.decode_val Mint32 bl = v2.
 
+Lemma mapsto_single_int: forall sh v1 v2 p,
+  samerep v1 v2 ->
+  mapsto sh (Tfloat F32 noattr) p v1 = mapsto sh (Tint I32 Unsigned noattr) p v2.
+Proof.
+  intros.
+  subst.
+Admitted.
+
+Lemma data_at_single_int: forall sh v1 v2 p1 p2,
+  samerep v1 v2 ->
+  readable_share sh ->
+  p1 = p2 ->
+  data_at sh (Tfloat F32 noattr) v1 p1 = data_at sh (Tint I32 Unsigned noattr) v2 p2.
+Proof.
+  intros.
+  subst.
+  apply pred_ext.
+  + entailer!.
+    erewrite <- mapsto_data_at'; auto.
+    erewrite <- mapsto_data_at'; auto.
+    erewrite mapsto_single_int; auto.
+  + entailer!.
+    erewrite <- mapsto_data_at'; auto.
+    erewrite <- mapsto_data_at'; auto.
+    erewrite mapsto_single_int; auto.
+Qed.
+
 Lemma union_conversion:
 forall {CS} (u:ident) sh p (i1 i2: ident) (t1 t2: type),
   match PTree.get u (@cenv_cs CS) with
@@ -79,12 +106,118 @@ Definition fabs_single_spec :=
 Definition Gprog : funspecs :=
     ltac:(with_library prog [ fabs_single_spec ]).
 
+(* This is not well developped or well tested yet. *)
+Ltac unfold_field_at' :=
+ match goal with
+ | |- context [field_at_mark ?cs ?sh ?t ?gfs ?v ?p] =>
+     let F := fresh "F" in
+       set (F := field_at_mark cs sh t gfs v p);
+       change field_at_mark with @field_at in F;
+     let V := fresh "V" in set (V:=v) in F;
+     let P := fresh "P" in set (P:=p) in F;
+     let T := fresh "T" in set (T:=t) in F;
+     let id := fresh "id" in evar (id: ident);
+     let Heq := fresh "Heq" in
+     assert (Heq: nested_field_type T gfs = Tstruct id noattr)
+           by (unfold id,T; reflexivity);
+     let H := fresh in
+     assert (H:= @field_at_Tstruct cs sh T gfs id noattr
+                          V V P  (eq_refl _) (JMeq_refl _));
+     unfold id in H; clear Heq id;
+     fold F in H; clearbody F;
+     simpl co_members in H;
+     lazy beta iota zeta delta  [nested_sfieldlist_at ] in H;
+     change (@field_at cs sh T) with (@field_at cs sh t) in H;
+     hnf in T; subst T;
+     change v with (protect _ v) in V;
+     simpl in H;
+     unfold withspacer in H; simpl in H;
+     change (protect _ v) with v in V;
+     subst V;
+     repeat match type of H with
+     | context[fst (?A,?B)] => change (fst (A,B)) with A in H
+     | context[snd (?A,?B)] => change (snd (A,B)) with B in H
+     end;
+     subst P;
+     subst F;
+     cbv beta;
+     repeat flatten_sepcon_in_SEP;
+     repeat simplify_project_default_val
+ | |- context [field_at_mark ?cs ?sh ?t ?gfs ?v ?p] =>
+     let F := fresh "F" in
+       set (F := field_at_mark cs sh t gfs v p);
+       change field_at_mark with @field_at in F;
+     let V := fresh "V" in set (V:=v) in F;
+     let P := fresh "P" in set (P:=p) in F;
+     let T := fresh "T" in set (T:=t) in F;
+     let id := fresh "id" in evar (id: ident);
+     let Heq := fresh "Heq" in
+     assert (Heq: nested_field_type T gfs = Tunion id noattr)
+           by (unfold id,T; reflexivity);
+     let H := fresh in
+     assert (H:= @field_at_Tunion cs sh T gfs id noattr
+                          V V P  (eq_refl _) (JMeq_refl _));
+     unfold id in H; clear Heq id;
+     fold F in H; clearbody F;
+     simpl co_members in H;
+     lazy beta iota zeta delta  [nested_ufieldlist_at ] in H;
+     change (@field_at cs sh T) with (@field_at cs sh t) in H;
+     hnf in T; subst T;
+     change v with (protect _ v) in V;
+     simpl in H;
+     unfold withspacer in H; simpl in H;
+     change (protect _ v) with v in V;
+     subst V;
+     repeat match type of H with
+     | context[fst (?A,?B)] => change (fst (A,B)) with A in H
+     | context[snd (?A,?B)] => change (snd (A,B)) with B in H
+     end;
+     subst P;
+     subst F;
+     cbv beta;
+     repeat flatten_sepcon_in_SEP;
+     repeat simplify_project_default_val
+ end.
+
+Ltac unfold_field_at N  :=
+  find_field_at N; unfold_field_at'.
+
+Lemma union_field_address___125: forall p,
+  field_address (Tunion __125 noattr) [UnionField _f] p = field_address (Tunion __125 noattr) [UnionField _i] p.
+Proof.
+  intros.
+  assert (field_compatible (Tunion __125 noattr) [UnionField _f] p <-> field_compatible (Tunion __125 noattr) [UnionField _i] p); [|unfold field_address; if_tac; if_tac; auto; tauto].
+  rewrite !field_compatible_cons; simpl.
+  unfold in_members; simpl.
+  tauto.
+Qed.
 
 Lemma body_fabs_single: semax_body Vprog Gprog f_fabs_single fabs_single_spec.
 Proof.
 start_function.
+forward.
 destruct (fabs_float32_lemma x) as [y [H3 H4]].
+unfold_field_at 1%nat.
+rewrite field_at_data_at.
+erewrite data_at_single_int; [| exact H3 | auto | apply union_field_address___125].
+change (Tint I32 Unsigned noattr) with (nested_field_type (Tunion __125 noattr) [UnionField _i]).
+rewrite <- field_at_data_at.
+forward.
+forward.
+rewrite field_at_data_at.
+erewrite <- data_at_single_int; [| exact H4 | auto | apply union_field_address___125].
+change (Tfloat F32 noattr) with (nested_field_type (Tunion __125 noattr) [UnionField _f]).
+rewrite <- field_at_data_at.
+forward.
+forward.
+unfold data_at_, field_at_.
+unfold_field_at 2%nat.
+simpl.
+entailer!.
+Qed.
 
+(*
+destruct (fabs_float32_lemma x) as [y [H3 H4]].
 pose proof (union_conversion __125 Tsh v_u _f _i tfloat tuint).
 simpl in H. spec H; [auto |].
 spec H. compute; clear; congruence.
@@ -105,7 +238,7 @@ forward.
 unfold data_at_.
 cancel.
 Qed.
-
+*)
 End Single.
 
 Module Float.
