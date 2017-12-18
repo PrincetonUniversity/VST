0a6291e3def2a1129b18a7e4cde90e7f8fccc70a
QinxiangCao <caoqinxiang@gmail.com>
2017-12-18 16:14:44 -0500

    Make jmeq_lemmas more unverse consistent.

--- floyd/jmeq_lemmas.v
+++ floyd/jmeq_lemmas.v
@@ -184,43 +184,54 @@ Lemma JMeq_sumtype_ll: forall (A: Type) (B: Type) (C: Type) (D: Type) (x: A) (y:
   (@JMeq (A + B) (inl x) (C + D) (inl y)) ->
   JMeq x y.
 Proof.
-  unfold not.
   intros.
-  subst.
-  inversion H1.
-  apply JMeq_eq in H1.
-  inversion H1.
-  apply JMeq_refl.
+  exists H.
+  destruct H1 as [?H ?H].
+  revert H1 H2.
+  destruct H, H0.
+  intros.
+  rewrite <- eq_rect_eq in H2.
+  inversion H2.
+  apply eq_refl.
 Qed.
 
 Lemma JMeq_sumtype_rr: forall (A: Type) (B: Type) (C: Type) (D: Type) (x: B) (y: D), @eq Type A C -> @eq Type B D -> 
   (@JMeq (A + B) (inr x) (C + D) (inr y)) ->
   JMeq x y.
 Proof.
-  unfold not.
   intros.
-  subst.
-  apply JMeq_eq in H1.
-  inversion H1.
-  apply JMeq_refl.
+  exists H0.
+  destruct H1 as [?H ?H].
+  revert H1 H2.
+  destruct H, H0.
+  intros.
+  rewrite <- eq_rect_eq in H2.
+  inversion H2.
+  apply eq_refl.
 Qed.
 
 Lemma JMeq_sumtype_lr: forall (A: Type) (B: Type) (C: Type) (D: Type) (x: A) (y: D), @eq Type A C -> @eq Type B D -> ~ (@JMeq (A + B) (inl x) (C + D) (inr y)).
 Proof.
   unfold not.
   intros.
-  subst.
-  apply JMeq_eq in H1.
-  inversion H1.
+  destruct H1 as [?H ?H].
+  revert H1 H2.
+  destruct H, H0.
+  intros.
+  rewrite <- eq_rect_eq in H2.
+  inversion H2.
 Qed.
 
 Lemma JMeq_sumtype_rl: forall (A: Type) (B: Type) (C: Type) (D: Type) (x: B) (y: C), @eq Type A C -> @eq Type B D -> ~ (@JMeq (A + B) (inr x) (C + D) (inl y)).
 Proof.
   unfold not.
   intros.
-  subst.
-  apply JMeq_eq in H1.
-  inversion H1.
+  destruct H1 as [?H ?H].
+  revert H1 H2.
+  destruct H, H0.
+  intros.
+  rewrite <- eq_rect_eq in H2.
+  inversion H2.
 Qed.
 
 Ltac solve_JMeq_sumtype H :=
@@ -233,40 +244,70 @@ Ltac solve_JMeq_sumtype H :=
      |apply JMeq_sumtype_rr in H; auto]
   end.
 
-Lemma JMeq_inl: forall (A: Type) (B: Type) (C: Type) (D: Type) (x: A) (y: C), @eq Type A C -> @eq Type B D -> JMeq x y -> @JMeq (A + B) (inl x) (C + D) (inl y).
+Lemma JMeq_inl: forall (A: Type) (B: Type) (C: Type) (D: Type) (x: A) (y: C), @eq Type B D -> JMeq x y -> @JMeq (A + B) (inl x) (C + D) (inl y).
 Proof.
   intros.
-  subst.
-  apply JMeq_eq in H1.
-  subst.
-  apply JMeq_refl.
+  destruct H0 as [?H ?H].
+  pose (f_equal2 := 
+          fun (A1 : Type) (A2 : Type) (B : Type) (f : forall (_ : A1) (_ : A2), B) 
+            (x1 y1 : A1) (x2 y2 : A2) (H : @eq A1 x1 y1) =>
+          match H in (eq _ y) return (forall _ : @eq A2 x2 y2, @eq B (f x1 x2) (f y y2)) with
+          | eq_refl => fun H0 : @eq A2 x2 y2 => match H0 in (eq _ y) return (@eq B (f x1 x2) (f x1 y)) with
+                                                | eq_refl => @eq_refl B (f x1 x2)
+                                                end
+          end).
+  exists (f_equal2 _ _ _ sum _ _ _ _ H0 H).
+  destruct H, H0.
+  rewrite <- H1.
+  unfold f_equal2.
+  simpl.
+  apply eq_refl.
 Qed.
 
-Lemma JMeq_inr: forall (A: Type) (B: Type) (C: Type) (D: Type) (x: B) (y: D), @eq Type A C -> @eq Type B D -> JMeq x y -> @JMeq (A + B) (inr x) (C + D) (inr y).
+Lemma JMeq_inr: forall (A: Type) (B: Type) (C: Type) (D: Type) (x: B) (y: D), @eq Type A C -> JMeq x y -> @JMeq (A + B) (inr x) (C + D) (inr y).
 Proof.
   intros.
-  subst.
-  apply JMeq_eq in H1.
-  subst.
-  apply JMeq_refl.
+  destruct H0 as [?H ?H].
+  pose (f_equal2 := 
+          fun (A1 : Type) (A2 : Type) (B : Type) (f : forall (_ : A1) (_ : A2), B) 
+            (x1 y1 : A1) (x2 y2 : A2) (H : @eq A1 x1 y1) =>
+          match H in (eq _ y) return (forall _ : @eq A2 x2 y2, @eq B (f x1 x2) (f y y2)) with
+          | eq_refl => fun H0 : @eq A2 x2 y2 => match H0 in (eq _ y) return (@eq B (f x1 x2) (f x1 y)) with
+                                                | eq_refl => @eq_refl B (f x1 x2)
+                                                end
+          end).
+  exists (f_equal2 _ _ _ sum _ _ _ _ H H0).
+  destruct H, H0.
+  rewrite <- H1.
+  unfold f_equal2.
+  simpl.
+  apply eq_refl.
 Qed.
 
 Lemma JMeq_fst: forall (A: Type) (B: Type) (C: Type) (D: Type) (x: A*B) (y: C*D), @eq Type A C -> @eq Type B D -> JMeq x y -> JMeq (fst x) (fst y).
 Proof.
   intros.
-  subst.
-  apply JMeq_eq in H1.
-  subst.
-  apply JMeq_refl.
+  exists H.
+  destruct H1 as [?H ?H].
+  revert H1 H2.
+  destruct H, H0.
+  intros.
+  rewrite <- eq_rect_eq in H2.
+  rewrite H2.
+  apply eq_refl.
 Qed.
 
 Lemma JMeq_snd: forall (A: Type) (B: Type) (C: Type) (D: Type) (x: A*B) (y: C*D), @eq Type A C -> @eq Type B D -> JMeq x y -> JMeq (snd x) (snd y).
 Proof.
   intros.
-  subst.
-  apply JMeq_eq in H1.
-  subst.
-  apply JMeq_refl.
+  exists H0.
+  destruct H1 as [?H ?H].
+  revert H1 H2.
+  destruct H, H0.
+  intros.
+  rewrite <- eq_rect_eq in H2.
+  rewrite H2.
+  apply eq_refl.
 Qed.
 
 Lemma JMeq_pair: forall (A: Type) (B: Type) (C: Type) (D: Type) (a: A) (b: B) (c: C) (d: D), JMeq a b -> JMeq c d -> JMeq (a, c) (b, d).
@@ -306,15 +347,16 @@ Proof.
 Qed.
 
 Lemma JMeq_func: forall (A: Type) (B: Type) (C: Type) (D: Type) (f: A -> B) (g: C -> D) x y,
-  @eq Type A C -> @eq Type B D ->
+  @eq Type B D ->
   JMeq x y -> JMeq f g -> JMeq (f x) (g y).
 Proof.
   intros.
-  subst.
+  exists H.
+  destruct H0.
+  destruct H, x0.
   apply JMeq_eq in H1.
-  apply JMeq_eq in H2.
-  subst.
-  apply JMeq_refl.
+  rewrite <- e, H1.
+  apply eq_refl.
 Qed.
 
 Lemma eq_JMeq: forall A (x y: A), x=y -> JMeq x y.
@@ -323,9 +365,11 @@ Qed.
 
 Lemma list_func_JMeq: forall {A: Type} {B: Type} {C: Type} (a: list A) (b: list B) (f: forall X, list X -> C), @eq Type A B -> JMeq a b -> f A a = f B b.
 Proof.
-  intros; subst.
-  apply JMeq_eq in H0.
-  subst.
+  intros.
+  destruct H0 as [? ?].
+  destruct H.
+  rewrite <- eq_rect_eq in e.
+  rewrite e.
   auto.
 Qed.
 
@@ -344,7 +388,8 @@ Qed.
 Lemma JMeq_sigT: forall {A: Type} {B: Type} (a: A), @eq Type A B -> {b: B | JMeq a b}.
 Proof.
   intros.
-  subst.
+  unfold JMeq.
+  destruct H.
   exists a.
   apply JMeq_refl.
 Qed.
