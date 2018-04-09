c5723a814f6f267de4570b09f3830e1f770ee884
QinxiangCao <caoqinxiang@gmail.com>
2018-04-08 22:12:43 -0400

    Use new symbolic cancel to replace original cancel. See what happens.

--- floyd/forward.v
+++ floyd/forward.v
@@ -667,8 +667,8 @@ Ltac prove_delete_temp := match goal with |- ?A = _ =>
   let Q := fresh "Q" in set (Q:=A); hnf in Q; subst Q; reflexivity
 end.
 
-Ltac cancel_for_forward_call := syntactic_cancel.
-Ltac default_cancel_for_forward_call := syntactic_cancel.
+Ltac cancel_for_forward_call := cancel_for_evar_frame.
+Ltac default_cancel_for_forward_call := cancel_for_evar_frame.
 
 Ltac unfold_post := match goal with |- ?Post = _ => let A := fresh "A" in let B := fresh "B" in first
   [evar (A : Type); evar (B : A -> environ -> mpred); unify Post (@exp _ _ ?A ?B);
