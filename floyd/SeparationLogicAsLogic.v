05981b6f2077292f649034a0eab72504a3409ce1
Qinxiang <caoqinxiang@gmail.com>
2018-05-10 16:49:14 -0400

    Another approach of getting semax_loop_nocontinue and semax_if_seq more easily.

---
+++ floyd/SeparationLogicAsLogic.v
@@ -0,0 +1,187 @@
+From compcert Require Export Clightdefs.
+Require Export VST.veric.SeparationLogic.
+Require Export VST.msl.Extensionality.
+Require Export compcert.lib.Coqlib.
+Require Export VST.msl.Coqlib2 VST.veric.coqlib4 VST.floyd.coqlib3.
+Require Export VST.floyd.jmeq_lemmas.
+Require Export VST.floyd.find_nth_tactic.
+Require Export VST.veric.juicy_extspec.
+Require VST.veric.SeparationLogicSoundness.
+Export SeparationLogicSoundness.SoundSeparationLogic.CSL.
+Require Import VST.veric.NullExtension.
+
+Local Open Scope logic.
+
+Inductive semax {CS: compspecs} {Espec: OracleKind} (Delta: tycontext): (environ -> mpred) -> statement -> ret_assert -> Prop :=
+| semax_ifthenelse :
+   forall P (b: expr) c d R,
+      bool_type (typeof b) = true ->
+     @semax CS Espec Delta (P && local (`(typed_true (typeof b)) (eval_expr b))) c R ->
+     @semax CS Espec Delta (P && local (`(typed_false (typeof b)) (eval_expr b))) d R ->
+     @semax CS Espec Delta (tc_expr Delta (Eunop Cop.Onotbool b (Tint I32 Signed noattr)) && P) (Sifthenelse b c d) R
+| semax_seq:
+  forall R P Q h t,
+    @semax CS Espec Delta P h (overridePost Q R) ->
+    @semax CS Espec (update_tycon Delta h) Q t R ->
+    @semax CS Espec Delta P (Ssequence h t) R
+| semax_break: forall Q,
+    @semax CS Espec Delta (RA_break Q) Sbreak Q
+| semax_continue: forall Q,
+    @semax CS Espec Delta (RA_continue Q) Scontinue Q
+| semax_loop: forall Q Q' incr body R,
+     @semax CS Espec Delta  Q body (loop1_ret_assert Q' R) ->
+     @semax CS Espec Delta Q' incr (loop2_ret_assert Q R) ->
+     @semax CS Espec Delta Q (Sloop body incr) R
+| semax_switch: forall (Q: environ->mpred) a sl R,
+     is_int_type (typeof a) = true ->
+     (forall rho, Q rho |-- tc_expr Delta a rho) ->
+     (forall n,
+     @semax CS Espec Delta 
+               (local (`eq (eval_expr a) `(Vint n)) &&  Q)
+               (seq_of_labeled_statement (select_switch (Int.unsigned n) sl))
+               (switch_ret_assert R)) ->
+     @semax CS Espec Delta Q (Sswitch a sl) R
+| semax_call: forall A P Q NEP NEQ ts x (F: environ -> mpred) ret argsig retsig cc a bl,
+           Cop.classify_fun (typeof a) =
+           Cop.fun_case_f (type_of_params argsig) retsig cc ->
+           (retsig = Tvoid -> ret = None) ->
+          tc_fn_return Delta ret retsig ->
+  @semax CS Espec Delta
+          ((|>((tc_expr Delta a) && (tc_exprlist Delta (snd (split argsig)) bl)))  &&
+         (`(func_ptr (mk_funspec  (argsig,retsig) cc A P Q NEP NEQ)) (eval_expr a) &&
+          |>(F * `(P ts x: environ -> mpred) (make_args' (argsig,retsig) (eval_exprlist (snd (split argsig)) bl)))))
+         (Scall ret a bl)
+         (normal_ret_assert
+          (EX old:val, substopt ret (`old) F * maybe_retval (Q ts x) retsig ret))
+| semax_return: forall (R: ret_assert) ret ,
+      @semax CS Espec Delta
+                ( (tc_expropt Delta ret (ret_type Delta)) &&
+                `(RA_return R : option val -> environ -> mpred) (cast_expropt ret (ret_type Delta)) (@id environ))
+                (Sreturn ret)
+                R
+| semax_set: forall (P: environ->mpred) id e,
+    @semax CS Espec Delta
+        (|> ( (tc_expr Delta e) &&
+             (tc_temp_id id (typeof e) Delta e) &&
+             subst id (eval_expr e) P))
+          (Sset id e) (normal_ret_assert P)
+| semax_set_forward: forall (P: environ->mpred) id e,
+    @semax CS Espec Delta
+        (|> ( (tc_expr Delta e) &&
+             (tc_temp_id id (typeof e) Delta e) &&
+          P))
+          (Sset id e)
+        (normal_ret_assert
+          (EX old:val, local (`eq (eval_id id) (subst id (`old) (eval_expr e))) &&
+                            subst id (`old) P))
+| semax_ptr_compare: forall P id cmp e1 e2 ty sh1 sh2,
+    sepalg.nonidentity sh1 -> sepalg.nonidentity sh2 ->
+   is_comparison cmp = true  ->
+   eqb_type (typeof e1) int_or_ptr_type = false ->
+   eqb_type (typeof e2) int_or_ptr_type = false ->
+   typecheck_tid_ptr_compare Delta id = true ->
+   @semax CS Espec Delta
+        ( |> ( (tc_expr Delta e1) &&
+              (tc_expr Delta e2)  &&
+
+          local (`(blocks_match cmp) (eval_expr e1) (eval_expr e2)) &&
+          (`(mapsto_ sh1 (typeof e1)) (eval_expr e1) * TT) &&
+          (`(mapsto_ sh2 (typeof e2)) (eval_expr e2) * TT) &&
+          P))
+          (Sset id (Ebinop cmp e1 e2 ty))
+        (normal_ret_assert
+          (EX old:val,
+                 local (`eq (eval_id id)  (subst id `(old)
+                     (eval_expr (Ebinop cmp e1 e2 ty)))) &&
+                            subst id `(old) P))
+| semax_load: forall  sh id P e1 t2 (v2: environ -> val),
+    typeof_temp Delta id = Some t2 ->
+    is_neutral_cast (typeof e1) t2 = true ->
+    readable_share sh ->
+    local (tc_environ Delta) && P |-- `(mapsto sh (typeof e1)) (eval_lvalue e1) v2 * TT ->
+    @semax CS Espec Delta
+       (|> ( (tc_lvalue Delta e1) &&
+       local (`(tc_val (typeof e1)) v2) &&
+          P))
+       (Sset id e1)
+       (normal_ret_assert (EX old:val, local (`eq (eval_id id) (subst id (`old) v2)) &&
+                                          (subst id (`old) P)))
+| semax_cast_load: forall sh id P e1 t1 (v2: environ -> val),
+    typeof_temp Delta id = Some t1 ->
+   cast_pointer_to_bool (typeof e1) t1 = false ->
+    readable_share sh ->
+    local (tc_environ Delta) && P |-- `(mapsto sh (typeof e1)) (eval_lvalue e1) v2 * TT ->
+    @semax CS Espec Delta
+       (|> ( (tc_lvalue Delta e1) &&
+       local (`(tc_val t1) (`(eval_cast (typeof e1) t1) v2)) &&
+          P))
+       (Sset id (Ecast e1 t1))
+       (normal_ret_assert (EX old:val, local (`eq (eval_id id) (subst id (`old) (`(eval_cast (typeof e1) t1) v2))) &&
+                                          (subst id (`old) P)))
+| semax_store: forall e1 e2 sh P,
+   writable_share sh ->
+   @semax CS Espec Delta
+          (|> ( (tc_lvalue Delta e1) &&  (tc_expr Delta (Ecast e2 (typeof e1)))  &&
+             (`(mapsto_ sh (typeof e1)) (eval_lvalue e1) * P)))
+          (Sassign e1 e2)
+          (normal_ret_assert
+             (`(mapsto sh (typeof e1)) (eval_lvalue e1) (`force_val (`(sem_cast (typeof e2) (typeof e1)) (eval_expr e2))) * P))
+| semax_skip: forall P, @semax CS Espec Delta P Sskip (normal_ret_assert P)
+| semax_pre_post_bupd: forall P' (R': ret_assert) P c (R: ret_assert) ,
+    (local (tc_environ Delta) && P |-- |==> P') ->
+    local (tc_environ (update_tycon Delta c)) && RA_normal R' |-- |==> RA_normal R ->
+    local (tc_environ Delta) && RA_break R' |-- |==> RA_break R ->
+    local (tc_environ Delta) && RA_continue R' |-- |==> RA_continue R ->
+    (forall vl, local (tc_environ Delta) && RA_return R' vl |-- |==> RA_return R vl) ->
+   @semax CS Espec Delta P' c R' -> @semax CS Espec Delta P c R.
+
+Lemma semax_seq_inv: forall {Espec: OracleKind}{CS: compspecs} Delta P R h t,
+    @semax CS Espec Delta P (Ssequence h t) R ->
+    exists Q, @semax CS Espec Delta P h (overridePost Q R) /\
+              @semax CS Espec (update_tycon Delta h) Q t R.
+Proof.
+  intros.
+  remember (Ssequence h t) as c eqn:?H.
+  induction H; try solve [inv H0].
+  + inv H0.
+    exists Q; auto.
+  + subst c.
+    pose proof IHsemax eq_refl. clear IHsemax.
+    destruct H0 as [Q [? ?]].
+    exists Q.
+    split.
+    - apply (semax_pre_post_bupd _ P' (overridePost Q R')); auto.
+      * clear.
+        destruct R, R'.
+        apply andp_left2, bupd_intro.
+      * destruct R, R'; auto.
+      * destruct R, R'; auto.
+      * destruct R, R'; auto.
+    - apply (semax_pre_post_bupd _ Q R'); auto.
+      * apply andp_left2, bupd_intro.
+      * admit.
+      * admit.
+      * admit.
+Admitted.
+
+Lemma semax_loop_nocontinue:
+  forall {Espec: OracleKind}{CS: compspecs} ,
+ forall Delta P body incr R,
+ nocontinue body = true ->
+ nojumps incr = true ->
+ @semax CS Espec Delta P (Ssequence body incr) (loop1_ret_assert P R) ->
+ @semax CS Espec Delta P (Sloop body incr) R.
+Proof.
+  intros.
+  apply semax_seq_inv in H1.
+  destruct H1 as [Q [? ?]].
+  eapply (semax_loop _ P Q).
+  +
+Abort.
+
+Axiom semax_if_seq:
+ forall {Espec: OracleKind} {CS: compspecs} Delta P e c1 c2 c Q,
+ semax Delta P (Sifthenelse e (Ssequence c1 c) (Ssequence c2 c)) Q ->
+ semax Delta P (Ssequence (Sifthenelse e c1 c2) c) Q.
+
+(* After this succeeds, remove "weakest_pre" in veric/semax.v. *)
