ccb22ba22ff8e8e8d95de7114f0124dd212223d3
Qinxiang <caoqinxiang@gmail.com>
2018-06-11 17:12:02 -0400

    Finish porting to mailbox.

--- mailbox/verif_mailbox_reader.v
+++ mailbox/verif_mailbox_reader.v
@@ -18,19 +18,18 @@ Proof.
   replace_SEP 0 (data_at Tsh tint (vint r) (force_val (sem_cast_pointer arg))).
   { rewrite sem_cast_neutral_ptr; auto; go_lowerx; cancel. }
   forward.
-  forward_call (r, reading, last_read, reads, lasts, sh1).
+  forward_call (r, reads, lasts, sh1, gv).
 (*  eapply semax_seq'; [|apply semax_ff]. *)
   set (c := Znth r comms).
   set (l := Znth r locks).
   forward_loop (EX b0 : Z, EX h : hist, PROP (0 <= b0 < B; latest_read h (vint b0))
-    LOCAL (temp _r (vint r); temp _arg arg; gvar _reading reading; gvar _last_read last_read; 
-           gvar _lock lock; gvar _comm comm; gvar _bufs buf)
-    SEP (data_at sh1 (tarray (tptr tint) N) reads reading; data_at sh1 (tarray (tptr tint) N) lasts last_read;
+    LOCAL (temp _r (vint r); temp _arg arg; gvars gv)
+    SEP (data_at sh1 (tarray (tptr tint) N) reads (gv _reading); data_at sh1 (tarray (tptr tint) N) lasts (gv _last_read);
          data_at Tsh tint Empty (Znth r reads); data_at Tsh tint (vint b0) (Znth r lasts);
          data_at Tsh tint (vint r) (force_val (sem_cast_pointer arg)); malloc_token Tsh tint arg;
-         data_at sh1 (tarray (tptr tint) N) comms comm;
-         data_at sh1 (tarray (tptr tlock) N) locks lock;
-         data_at sh1 (tarray (tptr tbuffer) B) bufs buf;
+         data_at sh1 (tarray (tptr tint) N) comms (gv _comm);
+         data_at sh1 (tarray (tptr tlock) N) locks (gv _lock);
+         data_at sh1 (tarray (tptr tbuffer) B) bufs (gv _bufs);
          comm_loc sh2 l c g g0 g1 g2 bufs sh gsh2 h;
          EX v : Z, @data_at CompSpecs sh tbuffer (vint v) (Znth b0 bufs);
          ghost_var gsh1 (vint b0) g0))
@@ -39,14 +38,14 @@ Proof.
     unfold latest_read.
     left; split; auto; discriminate. }
   Intros b0 h.
-  subst c l; subst; forward_call (r, reading, last_read, lock, comm, reads, lasts, locks, comms, bufs,
-    sh, sh1, sh2, b0, g, g0, g1, g2, h).
+  subst c l; subst; forward_call (r, reads, lasts, locks, comms, bufs,
+    sh, sh1, sh2, b0, g, g0, g1, g2, h, gv).
   { repeat (split; auto). }
   Intros x; destruct x as (((b, t), e), v); simpl in *.
   rewrite (data_at_isptr _ tbuffer); Intros.
   forward.
   forward.
-  forward_call (r, reading, reads, sh1).
+  forward_call (r, reads, sh1, gv).
   entailer!.
   Exists b (map_upd h t (AE e Empty)) v; entailer!.
 Qed.
