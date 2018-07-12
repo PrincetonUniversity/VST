d4d0a1072577257ec644ccf88d9eb0d9fbc99043
Andrew W. Appel <appel@princeton.edu>
2018-07-12 14:35:03 -0400

    Remove many unused .v files in sepcomp directory; use sepcomp/event_semantics.v from the HybridMachine branch.

--- sepcomp/full_composition.v
+++
@@ -1,9 +0,0 @@
-Require Import compcert.common.Memory.
-Require Import compcert.common.Values.
-Require Import VST.sepcomp.structured_injections.
-
-Definition full_comp (j1 j2: meminj) :=
-  forall b0 b1 delta1, j1 b0 = Some (b1, delta1) -> exists b2 delta2, j2 b1 = Some (b2, delta2).
-
-Definition full_ext (mu1 mu2: SM_Injection) :=
-  full_comp (extern_of mu1) (extern_of mu2).
