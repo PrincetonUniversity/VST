4a1e17da0407e7d258e12fec4fac72be7b082ed3
Qinxiang <caoqinxiang@gmail.com>
2018-07-26 05:51:59 -0400

    More porting. A typical complicated go_lower (line 1090 in drbg_protocal_proofs) needs 4.8 sec instead of original 9.2 sec.

--- hmacdrbg/drbg_protocol_proofs.v
+++ hmacdrbg/drbg_protocol_proofs.v
@@ -1089,7 +1089,6 @@ Proof. start_function.
                        field_address t_struct_hmac256drbg_context_st [StructField _V] ctx, Tsh, gv).
     Time go_lower. (*necessary due to existence of local () && in postcondition of for-rule*)
     idtac "previous timing was for go_lower (goal: 12secs)".
-    apply andp_right; [ apply prop_right; trivial | ].
     apply andp_right; [ apply prop_right; repeat split; trivial |].
     Exists (HMAC256 (V ++ [i] ++ (if na then contents else [])) key).
 
