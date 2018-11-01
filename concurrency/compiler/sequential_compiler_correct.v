731e94fc9ccb7898effd5ce69f5da143ecb46a93
Santiago <scuellar@princeton.edu>
2018-11-01 18:50:37 -0400

    Pushing forward the changes to the simulation (adding strong/relaxed simulations)

--- concurrency/compiler/sequential_compiler_correct.v
+++ concurrency/compiler/sequential_compiler_correct.v
@@ -9,7 +9,7 @@ Parameter CompCert_compiler: Clight.program -> option Asm.program.
 Hypothesis simpl_clight_semantic_preservation:
   forall (p:Clight.program) (tp:Asm.program),
   CompCert_compiler p = Some tp ->
-  fsim_properties_inj (Clight.semantics2 p) (Asm.semantics tp) Clight.get_mem Asm.get_mem.
+  fsim_properties_inj_relaxed (Clight.semantics2 p) (Asm.semantics tp) Clight.get_mem Asm.get_mem.
 
 
 End CompCert_correctness.
