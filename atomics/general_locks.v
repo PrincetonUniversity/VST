dc36a6682a8b1b62b7ac0489bae75406dfe552ad
William Mansky <mansky1@uic.edu>
2020-04-26 06:13:10 -0500

    Added one more view shift in sync_commit

--- atomics/general_locks.v
+++ atomics/general_locks.v
@@ -83,22 +83,22 @@ Proof.
 Qed.
 
 Lemma sync_commit_gen : forall {A B C} {inv_names : invG} a Ei Eo (b : A -> B -> mpred) Q R R' g (x0 : C)
-  (Ha : (forall x, R * a x |-- |==> EX x1, public_half g x1 * (!!(x1 = x0) --> (EX x' : C, my_half g x' * public_half g x' -* |==> (EX y, b x y * R' y))))%I),
+  (Ha : (forall x, R * a x |-- |==> EX x1, public_half g x1 * (!!(x1 = x0) --> |==> (EX x' : C, my_half g x' * public_half g x' -* |==> (EX y, b x y * R' y)))%I)%I),
   (atomic_shift a Ei Eo b Q * my_half g x0 * R |-- |==> EX y, Q y * R' y)%I.
 Proof.
   intros; rewrite sepcon_assoc.
   apply atomic_commit with (R'0 := fun y => R' y).
   intros; iIntros "((my & R) & a)".
   iMod (Ha with "[$]") as (?) "[public a']".
-    iPoseProof (ref_sub(P := discrete_PCM C) with "[$my $public]") as "%".
-    rewrite eq_dec_refl in H0; subst.
-    iDestruct ("a'" with "[%]") as (x') "H"; first done.
+  iPoseProof (ref_sub(P := discrete_PCM C) with "[$my $public]") as "%".
+  rewrite eq_dec_refl in H0; subst.
+  iMod ("a'" with "[%]") as (x') "H"; first done.
   iDestruct (public_update with "[$my $public]") as "[% >[my public]]"; subst.
   iApply ("H" with "[$my $public]").
 Qed.
 
 Lemma sync_commit_gen1 : forall {A B C} {inv_names : invG} a Ei Eo (b : A -> B -> mpred) Q R R' g (x0 : C)
-  (Ha : (forall x, R * a x |-- |==> EX x1, public_half g x1 * (!!(x1 = x0) --> (EX x' : C, my_half g x' * public_half g x' -* |==> (EX y, b x y) * R')%I))%I),
+  (Ha : (forall x, R * a x |-- |==> EX x1, public_half g x1 * (!!(x1 = x0) --> |==> (EX x' : C, my_half g x' * public_half g x' -* |==> (EX y, b x y) * R')%I))%I),
   (atomic_shift a Ei Eo b (fun _ => Q) * my_half g x0 * R |-- |==> Q * R')%I.
 Proof.
   intros; rewrite sepcon_assoc; eapply derives_trans; [apply atomic_commit with (R'0 := fun _ => R')|].
