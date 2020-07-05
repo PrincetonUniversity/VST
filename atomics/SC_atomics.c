82d8e1bc1f3ba1317576be7d6bff1510d721efdd
William Mansky <mansky1@uic.edu>
2020-07-05 10:18:33 -0500

    make_atom_ptr

--- atomics/SC_atomics.c
+++ atomics/SC_atomics.c
@@ -26,10 +26,9 @@ int atom_exchange(atom_int *tgt, int v){
   return atomic_exchange(&tgt->i, v);
 }
 
-/*
-atom_ptr make_atom_ptr(void *v){
-  atom_ptr r;
-  r.p = v;
+atom_ptr *make_atom_ptr(void *v){
+  atom_ptr *r = malloc(sizeof(atom_ptr));
+  r->p = v;
   return r;
 }
 
