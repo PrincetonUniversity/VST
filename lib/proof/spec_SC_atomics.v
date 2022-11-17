6c2629885d4ffaaae64be374fe314d07a4405c3f
Andrew W. Appel <appel@princeton.edu>
2022-11-17 15:26:23 -0500

    Specified SC_atomics VSU, version 1

--- lib/proof/spec_SC_atomics.v
+++ lib/proof/spec_SC_atomics.v
@@ -1,29 +1,26 @@
 (* SC atomics without importing Iris *)
 
+Require Import VST.floyd.proofauto.
+Require Import SC_atomics_extern.
+Local Existing Instance emptyCS.
+
+Import VST.veric.rmaps.
 Require Import Ensembles.
-Require Import VST.veric.rmaps.
-Require Import VST.veric.compcert_rmaps.
-Require Import VST.concurrency.ghosts.
-Require Import VST.concurrency.conclib.
-Require Import VST.floyd.library.
-Require Import VST.zlist.sublist.
+Notation vint z := (Vint (Int.repr z)).
 
-(* Warning: it is UNSOUND to use both this file and acq_rel_atomics.v in the same proof! There is
-   not yet an operational model that can validate the use of both SC and RA atomics. *)
 
-(* At present, due to complexities in the specifications of the C11 atomics (generics, _Atomic types, etc.), these are specs for wrapper functions for common cases.
-   There's probably a more systematic approach possible. *)
+Record AtomicsAPD := {
+   atomic_int : type := Tstruct _atom_int noattr;
+   atomic_int_at: share -> val -> val -> mpred;
+   atomic_int_at__ : forall sh v p, atomic_int_at sh v p |-- atomic_int_at sh Vundef p;
+   atomic_int_conflict : forall sh v v' p, sepalg.nonidentity sh -> atomic_int_at sh v p * atomic_int_at sh v' p |-- FF ;
+   atomic_ptr : type := Tstruct _atom_ptr noattr; 
+   atomic_ptr_at : share -> val -> val -> mpred;
+  atomic_ptr_conflict : forall sh v v' p, sepalg.nonidentity sh -> atomic_ptr_at sh v p * atomic_ptr_at sh v' p |-- FF 
+}.
 
-Class atomic_int_impl := { atomic_int : type; atomic_int_at : share -> val -> val -> mpred;
-  atomic_int_at__ : forall sh v p, atomic_int_at sh v p |-- atomic_int_at sh Vundef p;
-  atomic_int_conflict : forall sh v v' p, sepalg.nonidentity sh -> atomic_int_at sh v p * atomic_int_at sh v' p |-- FF }.
-
-Class atomic_ptr_impl := { atomic_ptr : type; atomic_ptr_at : share -> val -> val -> mpred;
-  atomic_ptr_conflict : forall sh v v' p, sepalg.nonidentity sh -> atomic_ptr_at sh v p * atomic_ptr_at sh v' p |-- FF }.
-
-Section SC_atomics.
-
-Context {CS : compspecs} {AI : atomic_int_impl} {AP : atomic_ptr_impl}.
+Section AtomicsASI.
+Variable M: AtomicsAPD.
 
 Definition make_atomic_spec :=
   WITH v : val
@@ -31,11 +28,11 @@ Definition make_atomic_spec :=
     PROP ()
     PARAMS (v)
     SEP ()
-  POST [ tptr atomic_int ]
+  POST [ tptr M.(atomic_int) ]
    EX p : val,
     PROP ()
     RETURN (p)
-    SEP (atomic_int_at Ews v p).
+    SEP (M.(atomic_int_at) Ews v p).
 
 Definition make_atomic_ptr_spec :=
   WITH v : val
@@ -43,18 +40,18 @@ Definition make_atomic_ptr_spec :=
     PROP ()
     PARAMS (v)
     SEP ()
-  POST [ tptr atomic_ptr ]
+  POST [ tptr M.(atomic_ptr) ]
    EX p : val,
     PROP (is_pointer_or_null p)
     RETURN (p)
-    SEP (atomic_ptr_at Ews v p).
+    SEP (M.(atomic_ptr_at) Ews v p).
 
 Definition free_atomic_ptr_spec :=
   WITH p : val
-  PRE [ tptr atomic_ptr ]
+  PRE [ tptr M.(atomic_ptr) ]
     PROP (is_pointer_or_null p)
     PARAMS (p)
-    SEP (EX v : val, atomic_ptr_at Ews v p)
+    SEP (EX v : val, M.(atomic_ptr_at) Ews v p)
   POST[ tvoid ]
     PROP ()
     LOCAL ()
@@ -62,10 +59,10 @@ Definition free_atomic_ptr_spec :=
 
 Definition free_atomic_int_spec :=
   WITH p : val
-  PRE [ tptr atomic_int ]
+  PRE [ tptr M.(atomic_int) ]
     PROP (is_pointer_or_null p)
     PARAMS (p)
-    SEP (EX v : val, atomic_int_at Ews v p)
+    SEP (EX v : val, M.(atomic_int_at) Ews v p)
   POST[ tvoid ]
     PROP ()
     LOCAL ()
@@ -75,11 +72,11 @@ Definition AL_type := ProdType (ConstType (val * Ensemble nat * Ensemble nat)) (
 
 Program Definition atomic_load_spec := TYPE AL_type
   WITH p : val, Eo : Ensemble nat, Ei : Ensemble nat, Q : val -> mpred
-  PRE [ tptr atomic_int ]
+  PRE [ tptr M.(atomic_int) ]
    PROP (Included Ei Eo)
    PARAMS (p)
    SEP (|={Eo,Ei}=> EX sh : share, EX v : val, !!(readable_share sh) &&
-              atomic_int_at sh v p * (atomic_int_at sh v p -* |={Ei,Eo}=> Q v))
+              M.(atomic_int_at) sh v p * (M.(atomic_int_at) sh v p -* |={Ei,Eo}=> Q v))
   POST [ tint ]
    EX v : val,
    PROP ()
@@ -111,11 +108,11 @@ Definition AS_type := ProdType (ConstType (val * val * Ensemble nat * Ensemble n
 
 Program Definition atomic_store_spec := TYPE AS_type
   WITH p : val, v : val, Eo : Ensemble nat, Ei : Ensemble nat, Q : mpred
-  PRE [ tptr atomic_int, tint ]
+  PRE [ tptr M.(atomic_int), tint ]
    PROP (Included Ei Eo)
    PARAMS (p; v)
-   SEP (|={Eo,Ei}=> EX sh : share, !!(writable_share sh) && atomic_int_at sh Vundef p *
-      (atomic_int_at sh v p -* |={Ei,Eo}=> Q))
+   SEP (|={Eo,Ei}=> EX sh : share, !!(writable_share sh) && M.(atomic_int_at) sh Vundef p *
+      (M.(atomic_int_at) sh v p -* |={Ei,Eo}=> Q))
   POST [ tvoid ]
    PROP ()
    LOCAL ()
@@ -146,12 +143,12 @@ Definition ACAS_type := ProdType (ProdType (ProdType (ConstType (val * share * v
 
 Program Definition atomic_CAS_spec := TYPE ACAS_type
   WITH p : val, shc : share, pc : val, c : val, v : val, Eo : Ensemble nat, Ei : Ensemble nat, Q : val -> mpred
-  PRE [ tptr atomic_int, tptr tint, tint ]
+  PRE [ tptr M.(atomic_int), tptr tint, tint ]
    PROP (readable_share shc; Included Ei Eo)
    PARAMS (p; pc; v)
    SEP (data_at shc tint c pc; |={Eo,Ei}=> EX sh : share, EX v0 : val,
-      !!(writable_share sh) && atomic_int_at sh v0 p *
-           (atomic_int_at sh (if eq_dec v0 c then v else v0) p -* |={Ei,Eo}=> Q v0))
+      !!(writable_share sh) && M.(atomic_int_at) sh v0 p *
+           (M.(atomic_int_at) sh (if eq_dec v0 c then v else v0) p -* |={Ei,Eo}=> Q v0))
   POST [ tint ]
    EX v' : val,
    PROP ()
@@ -219,4 +216,16 @@ Proof.
     rewrite -> !sepcon_emp, ?approx_sepcon, ?approx_idem; auto.
 Qed.
 
-End SC_atomics.
+Definition AtomicsASI:funspecs := [
+  (_make_atomic, make_atomic_spec);
+  (_make_atomic_ptr, make_atomic_ptr_spec);
+  (_free_atomic_ptr, free_atomic_ptr_spec);
+  (_free_atomic, free_atomic_int_spec);
+  (_atom_load, atomic_load_spec);
+  (_atom_store, atomic_store_spec);
+  (_atom_CAS, atomic_CAS_spec);
+  (_atom_exchange, atomic_exchange_spec)
+ ].
+
+End AtomicsASI.
+
