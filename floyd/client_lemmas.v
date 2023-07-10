0beb18adc01ae35a2abec1b7bb56fee24c2e56ab
rinshankaihou <madoka@umich.edu>
2023-07-10 04:07:04 -0500

    fix extract_nth_exists_in_SEP arity

--- floyd/client_lemmas.v
+++ floyd/client_lemmas.v
@@ -1973,7 +1973,7 @@ Ltac extract_exists_in_SEP' PQR :=
    match R with context [(@bi_exist _ ?A ?S) :: ?R'] =>
       let n := constr:((length R - Datatypes.S (length R'))%nat) in
       let n' := eval lazy beta zeta iota delta in n in
-      rewrite (@extract_nth_exists_in_SEP n' P Q R A S (eq_refl _));
+      rewrite (@extract_nth_exists_in_SEP _ _ n' P Q R A S (eq_refl _));
       unfold replace_nth at 1;
       rewrite ?bi.and_exist_l
    end
