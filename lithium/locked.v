23858fe75edd48df19593f7b9b2865a95d9f0bb8
Duc-Than Nguyen <dnguye96@uic.edu>
2024-06-18 17:52:18 -0500

    WIP in porting RefinedC's locked.

--- lithium/locked.v
+++ lithium/locked.v
@@ -3,9 +3,6 @@ From iris.algebra Require Import big_op gset frac agree.
 From VST.lithium Require Import programs.
 From VST.lithium Require Import type_options.
 Require Import iris_ora.algebra.frac_auth.
-Require Import iris_ora.algebra.gmap.
-Require Import iris_ora.logic.own.
-Require Import iris_ora.algebra.ext_order.
 
 Definition lockN : namespace := nroot.@"lockN".
 Definition lock_id := gname.
@@ -35,7 +32,7 @@ Section type.
   Context `{!typeG Σ} `{!lockG Σ} {cs : compspecs}.
 
   Definition lock_token (γ : lock_id) (l : list string) : mpred :=
-    ∃ s : gset string, ⌜l ≡ₚ elements s⌝ ∗ own γ (● GSet s : gset_disjUR_authR).
+    ∃ s : gset string, ⌜l ≡ₚ elements s⌝ ∧ own γ (● GSet s : gset_disjUR_authR).
 
   Global Instance lock_token_timeless γ l : Timeless (lock_token γ l).
   Proof. apply _. Qed.
@@ -46,7 +43,7 @@ Section type.
     iIntros "H1 H2".
     iDestruct "H1" as (?) "[_ H1]".
     iDestruct "H2" as (?) "[_ H2]".
-    iDestruct (own_valid_2 γ (● GSet s) (● GSet s0) with "[$H1] [$H2]") as "Hown".
+    iDestruct (own_valid_2 γ (● GSet (s : gset string)) (● GSet s0) with "[$H1] [$H2]") as "Hown".
   Admitted.
 
   Theorem alloc_lock_token :
@@ -71,10 +68,9 @@ Section type.
     iExists _. iApply inv_alloc. iIntros "!#". iLeft. iExists _. by iFrame.
   Qed.
   Next Obligation. iIntros (A γ n x ty ot mt v ?) "Hl". by iApply ty_aligned. Qed.
-  Next Obligation. iIntros (A γ n x ty ot mt v ?) "Hl". Admitted. (* by iApply ty_size_eq. Qed. *)
-  Next Obligation. iIntros (A γ n x ty ot mt l ? ?) "Hl". by iApply (ty_ref with "[$Hl]").
-  Qed.
-  
+  Next Obligation. iIntros (A γ n x ty ot mt v ?) "Hl". by iApply ty_deref. Qed.
+  Next Obligation. iIntros (A γ n x ty ot mt l ? ?) "Hl". by iApply ty_ref. Qed.
+
   Lemma tylocked_simplify_hyp_place A γ n x (ty : A → type) l T:
     (l ◁ₗ ty x -∗ T)
     ⊢ simplify_hyp (l ◁ₗ tylocked_ex γ n x ty) T.
@@ -100,9 +96,7 @@ Section type.
   Definition tylocked_subsume_inst := [instance tylocked_subsume].
   Global Existing Instance tylocked_subsume_inst | 10.
 
-  (* Will work later *)
-
-  Definition tylocked_ex_token {A} (γ : lock_id) (n : string) (l : loc) (β : own_state) (ty : A → type)  : iProp Σ :=
+  Definition tylocked_ex_token {A} (γ : lock_id) (n : string) (l : address) (β : own_state) (ty : A → type)  : mpred :=
     (∀ E x, ⌜↑lockN ⊆ E⌝ -∗ l ◁ₗ ty x ={E}=∗ l ◁ₗ{β} tylocked_ex γ n x ty ∗ own γ (◯ GSet {[ n ]}))%I.
 
   Lemma locked_open A n s l γ (x : A) ty β E:
@@ -119,9 +113,17 @@ Section type.
     iAssert (lock_token γ (n :: s)) with "[Hown]" as "$". {
       iExists _. iFrame. iPureIntro. rewrite Hperm elements_union_singleton //. set_solver.
     }
-    destruct β. { iIntros "!# !#". iExists _. iFrame. iSplit => //. by iIntros (???) "$". }
+    destruct β.
+    { iIntros "!# !#". iExists _. iFrame. iSplit => //.
+      iIntros (? ?) "H1 Hl". iModIntro.
+      iFrame "Hl". admit.
+    }
+
     iDestruct "Hl" as (γ') "#Hinv".
-    iInv "Hinv" as "[Hl|>Hn]" "Hc". 2: {
+    iInv "Hinv" as "[Hl|>Hn]" "Hc".
+    Admitted.
+  (*
+    2: {
       iDestruct (own_valid_2 with "Hs Hn") as %Hown. exfalso. move: Hown.
       rewrite -auth_frag_op auth_frag_valid gset_disj_valid_op. set_solver.
     }
@@ -138,7 +140,8 @@ Section type.
     iMod ("Hc" with "[Hl Hexcl]") as "_". 2: by iExists _.
     iModIntro. iLeft. iExists _. iFrame.
   Qed.
-
+*)
+  
   Lemma locked_close A n s l γ (x : A) ty β E:
     ↑lockN ⊆ E →
     tylocked_ex_token γ n l β ty -∗ l ◁ₗ ty x -∗ lock_token γ (n :: s) ={E}=∗
@@ -147,17 +150,23 @@ Section type.
     iIntros (HE) "Hlocked Hl Hlock".
     iMod ("Hlocked" with "[//] Hl") as "[$ Hn]".
     iDestruct "Hlock" as (st Hst) "Htok".
-    iExists (st ∖ {[n]}). iSplitR. {
-      iPureIntro. move: (Hst). rewrite {1}(union_difference_L {[n]} st).
-      - rewrite ->elements_union_singleton => ?; last set_solver.
+    iExists (st ∖ {[n]}).
+    iModIntro.
+    iSplit.
+    - iPureIntro.
+      move: (Hst). rewrite {1}(union_difference_L {[n]} st).
+      + rewrite ->elements_union_singleton => ?; last set_solver.
         by apply: Permutation.Permutation_cons_inv.
-      - set_unfold => ??. subst. apply elem_of_elements. rewrite -Hst. set_solver.
-    }
-    iCombine "Htok" "Hn" as "Htok".
-    iMod (own_update with "Htok") as "$" => //.
+      + set_unfold => ??. subst. apply elem_of_elements. rewrite -Hst. set_solver.
+    - iCombine "Htok" "Hn" as "Htok".
+    Admitted.
+      (*
+      iMod (own_update with "[Htok]") as "H".
+      iMod (own_update with "Htok") as "$" => //.
     eapply auth_update_dealloc.
       by apply gset_disj_dealloc_local_update.
   Qed.
+*)
 
   Lemma annot_unlock A l β γ n ty (x : A) T:
     (find_in_context (FindDirect (lock_token γ)) (λ s : list string, ⌜n∉s⌝ ∗ (∀ x',
