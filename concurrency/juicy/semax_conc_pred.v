9461e2aeadc0ed76266f4290d9e5e4a6ba4c62c0
Duc-Than Nguyen <dnguye96@uic.edu>
2024-07-08 09:20:50 -0500

    fix bi_wand_iff notation breakage

--- concurrency/juicy/semax_conc_pred.v
+++ concurrency/juicy/semax_conc_pred.v
@@ -3,7 +3,7 @@ Require Import VST.concurrency.common.lksize.
 
 Section mpred.
 
-Context `{!VSTGS OK_ty Σ}.
+Context `{heapGS Σ}.
 
 Definition exclusive_mpred R : mpred := ((R ∗ R) -∗ False)%I.
 
@@ -14,8 +14,7 @@ Definition lock_inv : share -> val -> mpred -> mpred :=
     (∃ b : block, ∃ ofs : _, ⌜v = Vptr b ofs⌝ ∧
       inv LKN (∃ st, LKspec LKSIZE st sh (b, Ptrofs.unsigned ofs) ∗ if st then emp else R)).
 
-Definition rec_inv sh v (Q R: mpred): mpred :=
-  ((bi_wand_iff R Q) ∗ ▷ lock_inv sh v R).
+Definition rec_inv sh v (Q R: mpred): mpred := (R ∗-∗ Q ∗ ▷ lock_inv sh v R)%I.
 
 Lemma lockinv_isptr sh v R : lock_inv sh v R ⊣⊢ (⌜isptr v⌝ ∧ lock_inv sh v R).
 Proof.
