ccedfb5939cbf7eb4eda17c48b69952858b9cf4c
Duc-Than Nguyen <dnguye96@uic.edu>
2024-07-18 11:49:27 -0500

    more progress on wand

--- lithium/wand.v
+++ lithium/wand.v
@@ -24,7 +24,7 @@ Section wand.
     use all leftover resources. This only works if there is at most
     one wand per block (but this is enough for iterating over linked
     lists). *)
-    (∃ z, T z ∗ (∀ x, P2 z x -∗ ∃ y, P1 y ∗ (l ◁ₗ ty1 y -∗ l ◁ₗ ty2 z x ∗ True)))
+    (∃ z, T z ∗ (∀ x, P2 z x -∗ ∃ y, P1 y ∗ (l ◁ₗ ty1 y -∗ l ◁ₗ ty2 z x ∗ <affine> True)))
     ⊢ subsume (l ◁ₗ wand_ex P1 ty1) (λ z : B, l ◁ₗ wand_ex (P2 z) (ty2 z)) T.
   Proof.
     iIntros "(%&?&Hwand) Hwand2". iExists _. iFrame.
@@ -32,7 +32,7 @@ Section wand.
     iDestruct ("Hwand2" with "HP1") as "Hty1".
     iDestruct ("Hty" with "[$Hty1]") as "H".  
     iDestruct "H" as "(H & ?)". iFrame.
-  Admitted.
+  Qed.
   
   Definition subsume_wand_inst := [instance subsume_wand].
   Global Existing Instance subsume_wand_inst.
@@ -50,8 +50,8 @@ Section wand.
     and:
     | drop_spatial; ∀ x, inhale P x; exhale l ◁ₗ ty x; done
     | return T.
-  Proof. iIntros "[#Hwand $]". iIntros (?) "?".
-         iDestruct ("Hwand" with "[$]") as "H".
+  Proof. iIntros "[#Hwand $]". iIntros (?) "H1".
+         iDestruct ("Hwand" with "[$H1]") as "H".
          Admitted.
   Definition simplify_goal_wand_inst := [instance simplify_goal_wand with 50%N].
   Global Existing Instance simplify_goal_wand_inst | 50.
@@ -68,41 +68,49 @@ Section wand_val.
   Context {A : Type}.
   Implicit Types (P : A → iProp Σ).
 
-  Check field_compatible.
-  Check ty_has_op_type.
-
   Program Definition wand_val_ex ly P (ty : A → type) : type := {|
-    (* ty_has_op_type ot mt := ot = UntypedOp ly; *)
+    ty_has_op_type ot mt := ot = ly; 
     ty_own β l :=
       ∃ v,  <affine> ⌜field_compatible ly [] l⌝ ∗
-                        <affine> ⌜field_compatible ly [] v ⌝ ∗ l ↦_(tptr tvoid)[β] v ∗
+                        <affine> ⌜field_compatible ly [] v ⌝ ∗ l ↦_ly[β] v ∗
         match β return _ with
         | Own => ∀ x, P x -∗ v ◁ᵥ (ty x)
         | Shr => True
         end;
      ty_own_val v := (<affine> ⌜field_compatible ly [] v⌝ ∗ ∀ x, P x -∗ v ◁ᵥ (ty x))%I;
   |}%I.
-  (* Next Obligation.
+  Next Obligation.
     iIntros (??????) "H". iDestruct "H" as (v) "(Hly1&Hly2&Hl&_)".
     iMod (heap_mapsto_own_state_share with "Hl") as "Hl". eauto with iFrame.
-  Qed. *)
-  Next Obligation. Admitted. (* iIntros (??????->) "Hl". iDestruct "Hl" as (?) "[$ _]". Qed. *)
-  Next Obligation. iIntros (??????) "H".
-                   iDestruct "H" as (v) "(Hly1&Hly2&Hl&_)".
-                   iMod (heap_mapsto_own_state_share with "Hl") as "Hl". eauto with iFrame. Qed.
+  Qed. 
+  Next Obligation. iIntros (??????->) "Hl". iDestruct "Hl" as (?) "[$ _]". Qed.
   Next Obligation. iIntros (???????).
                    iIntros "Hl".
-                   iDestruct "Hl" as (v) "(%&%&?&?)".
-                   Check heap_mapsto_own_state_share.
+                   iDestruct "Hl" as (v) "(% & % & Hl1 & Hl2)".
+                   iExists _. iFrame.
+                   iSplit; last first; try done.
+                   iDestruct (heap_mapsto_own_state_to_mt ot l v _ Own with "[Hl1]") as "Hl1"; try done.
+                   { assert (ot = ly) as ->; try done. }
                    
-                   eauto with iFrame. Qed.
+                    Admitted.
     
-    Check heap_mapsto_own_state_share.
-    iIntros (??????->) "[$ _]". Qed.
-  Next Obligation. iIntros (??????->) "Hl". iDestruct "Hl" as (v) "(?&?&?&?)". eauto with iFrame. Qed.
-  Next Obligation. iIntros (?????? v ->) "??[??]". iExists v. iFrame. Qed.
-  Next Obligation. iIntros (????????). apply mem_cast_compat_Untyped. by simplify_eq/=. Qed.
+  Next Obligation. iIntros (????????) "Hl".
+                   iDestruct "Hl" as (v1) "(% & % & % & %)".
+                   iIntros "H1 (H2 & H3)".
+                   iExists v. iFrame.
+                   iSplit; last first.
+                   {
+                     iDestruct (heap_mapsto_own_state_from_mt ot l v _ Own with "[$H1]")
+                       as "H1"; try done.
+                     assert (ot = ly) as ->; try done.
+                     admit.
+                   }
+                   { iPureIntro.
+                     admit.
+                   }
+                   Admitted.
 
+  (*
   Global Instance wand_val_loc_in_bounds P ly β (ty : A → type):
     LocInBounds (wand_val_ex ly P ty) β (ly_size ly).
   Proof.
@@ -110,18 +118,19 @@ Section wand_val.
     iDestruct (heap_mapsto_own_state_loc_in_bounds with "Hl") as "H".
     by iDestruct "Hly" as %->.
   Qed.
-
+  *)
+  
   Lemma subsume_wand_val B v ly1 ly2 P1 (P2 : B → A → iProp Σ) ty1 ty2 T:
     (* The trick is that we prove the wand at the very end so it can
     use all leftover resources. This only works if there is at most
     one wand per block (but this is enough for iterating over linked
     lists). *)
-    (∃ z, ⌜ly1 = ly2 z⌝ ∗ T z ∗ (∀ x, P2 z x -∗ ∃ y, P1 y ∗ (v ◁ᵥ ty1 y -∗ v ◁ᵥ ty2 z x ∗ True)))
+    (∃ z, <affine> ⌜ly1 = ly2 z⌝ ∗ T z ∗ (∀ x, P2 z x -∗ ∃ y, P1 y ∗ (v ◁ᵥ ty1 y -∗ v ◁ᵥ ty2 z x ∗ <affine> True)))
     ⊢ subsume (v ◁ᵥ wand_val_ex ly1 P1 ty1) (λ z : B, v ◁ᵥ wand_val_ex (ly2 z) (P2 z) (ty2 z)) T.
   Proof.
     iIntros "(%&->&?&Hwand) (%&Hty1)". iExists _. iFrame. iSplit; [done|].
     iIntros (x) "HP2". iDestruct ("Hwand" with "HP2") as (y) "[HP1 Hwand]".
-    iDestruct ("Hty1" with "HP1") as "Hty1". iDestruct ("Hwand" with "Hty1") as "[$_]".
+    iDestruct ("Hty1" with "HP1") as "Hty1". iDestruct ("Hwand" with "Hty1") as "[$ _]". 
   Qed.
   Definition subsume_wand_val_inst := [instance subsume_wand_val].
   Global Existing Instance subsume_wand_val_inst.
@@ -141,11 +150,11 @@ Section wand_val.
     simplify_goal (v ◁ᵥ wand_val_ex ly P ty) T :-
     and:
     | drop_spatial; ∀ x, inhale P x; exhale v ◁ᵥ ty x; done
-    | exhale ⌜v `has_layout_val` ly⌝; return T.
+    | exhale (<affine> ⌜field_compatible ly [] v⌝); return T.
   Proof.
     iIntros "[#Hwand [% $]]". iSplit; [done|].
-    iIntros (?) "?". iDestruct ("Hwand" with "[$]") as "[$ _]".
-  Qed.
+    iIntros (?) "?". iDestruct ("Hwand" with "[$]") as "[H1 H2]". iFrame "H1".
+  Admitted.
   Definition simplify_goal_wand_val_inst := [instance simplify_goal_wand_val with 50%N].
   Global Existing Instance simplify_goal_wand_val_inst | 50.
 
