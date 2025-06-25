cfed08019dc1f3c30dfe0731cc22f8d386c1baaf
rinshankaihou <madoka@umich.edu>
2025-06-24 20:54:51 -0500

    delete unused code

--- refinedVST/typing/programs.v
+++ refinedVST/typing/programs.v
@@ -574,11 +574,6 @@ Section proper.
     l ◁ₗ{β} ty2 ∗ T ⊢ simplify_goal (l◁ₗ{β} ty1) T.
   Proof. rewrite Heq. iIntros "$". Qed.
 
-  (* Global Instance ty_own_val_le cty: Proper ((⊑)  ==> eq ==> (⊢)) (ty_own_val_at cty).
-  Proof. intros ?? EQ ??->. apply EQ. Qed.
-  Global Instance ty_own_val_proper cty: Proper ((≡) ==> eq ==> (≡)) (ty_own_val_at cty).
-  Proof. intros ?? EQ ??->. apply EQ. Qed. *)
-
   Lemma simplify_hyp_val_eq cty ty1 ty2 (Heq : ty1 ≡@{type} ty2) v T:
     (v ◁ᵥ|cty| ty2 -∗ T) ⊢ simplify_hyp (v ◁ᵥ|cty| ty1) T.
   Proof. iIntros "HT ?". rewrite Heq. by iApply "HT". Qed.
